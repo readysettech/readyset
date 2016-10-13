480c21ef995da52d496c1b3aac26062396f40d14
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-10-12 22:32:10 -0400

    Add Hyrise test queries and e2e test for them

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,6 +3,26 @@ extern crate nom_sqlp;
 use std::fs::File;
 use std::io::Read;
 
+fn parse_queryset(queries: Vec<&str>) {
+    let mut parsed_ok = Vec::new();
+    let mut parsed_err = 0;
+    for query in queries.iter() {
+        match nom_sqlp::parser::parse_query(query) {
+            Ok(q) => {
+                parsed_ok.push(q);
+            }
+            Err(_) => parsed_err += 1,
+        }
+    }
+
+    println!("Parsing failed: {} queries", parsed_err);
+    println!("Parsed successfully: {} queries", parsed_ok.len());
+    println!("\nSuccessfully parsed queries:");
+    for q in parsed_ok.iter() {
+        println!("{:?}", q);
+    }
+}
+
 #[test]
 fn hotcrp_queries() {
     let mut f = File::open("tests/hotcrp-queries.txt").unwrap();
@@ -11,17 +31,22 @@ fn hotcrp_queries() {
     // Load HotCRP queries
     f.read_to_string(&mut s).unwrap();
     let lines: Vec<&str> = s.split("\n").collect();
-    println!("Loaded {} total queries", lines.len());
+    println!("Loaded {} HotCRP queries", lines.len());
 
-    let mut parsed_ok = 0;
-    let mut parsed_err = 0;
-    for query in lines.iter() {
-        match nom_sqlp::parser::parse_query(query) {
-            Ok(_) => parsed_ok += 1,
-            Err(_) => parsed_err += 1,
-        }
-    }
+    // Try parsing them all
+    parse_queryset(lines);
+}
 
-    println!("Parsed successfully: {} queries", parsed_ok);
-    println!("Parsing failed: {} queries", parsed_err);
+#[test]
+fn hyrise_test_queries() {
+    let mut f = File::open("tests/hyrise-test-queries.txt").unwrap();
+    let mut s = String::new();
+
+    // Load HyRise queries
+    f.read_to_string(&mut s).unwrap();
+    let lines: Vec<&str> = s.split("\n").collect();
+    println!("Loaded {} Hyrise queries", lines.len());
+
+    // Try parsing them all
+    parse_queryset(lines);
 }
