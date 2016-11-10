69ab95aba411a57ae3cf1ee868261fdf2c7ede96
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-11-10 15:33:25 -0500

    Run `cargo update` before building

--- src/taste.rs
+++ src/taste.rs
@@ -20,6 +20,12 @@ pub fn taste_commit(wsl: &Mutex<Workspace>, id: &String) {
     let ws = wsl.lock().unwrap();
     ws.checkout_commit(id);
 
+    // Update dependencies
+    if !update(&ws.path).success() {
+        println!("Failed to update crates for {}", id);
+        return;
+    }
+
     // Try to build the commit
     if !build(&ws.path).success() {
         println!("Failed to build {}", id);
@@ -28,4 +34,12 @@ pub fn taste_commit(wsl: &Mutex<Workspace>, id: &String) {
 
     // Run benchmarks and collect results
     benchmark(&ws.path);
+}
+
+fn update(workdir: &str) -> ExitStatus {
+    Command::new("cargo")
+        .current_dir(workdir)
+        .arg("update")
+        .status()
+        .expect("Failed to execute 'cargo update'!")
 }
