c6ef29aa1cb3b98e302d70a7114cc1903f4e22e0
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-11-21 00:04:19 -0500

    Better test code, and additional query sets
    This refactors the end-to-end test code to use a single code path for
    all files with query sets, and adds two new query sets:
     1) the set of queries in TPC-W, and
     2) the queries used in the SIGMOD 1982 paper on common expression
        analysis in databases by S. Finkelstein.

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,17 +1,23 @@
 extern crate nom_sql;
 
 use std::fs::File;
+use std::path::Path;
 use std::io::Read;
 
-fn parse_queryset(queries: Vec<String>) {
+fn parse_queryset(queries: Vec<String>) -> (i32, i32) {
     let mut parsed_ok = Vec::new();
     let mut parsed_err = 0;
     for query in queries.iter() {
+        println!("Trying to parse '{}': ", &query);
         match nom_sql::parser::parse_query(&query) {
             Ok(_) => {
+                println!("ok");
                 parsed_ok.push(query);
             }
-            Err(_) => parsed_err += 1,
+            Err(_) => {
+                println!("failed");
+                parsed_err += 1;
+            }
         }
     }
 
@@ -21,50 +27,52 @@ fn parse_queryset(queries: Vec<String>) {
     for q in parsed_ok.iter() {
         println!("{:?}", q);
     }
+
+    (parsed_ok.len() as i32, parsed_err)
+}
+
+fn test_queries_from_file(f: &Path, name: &str) -> Result<i32, i32> {
+    let mut f = File::open(f).unwrap();
+    let mut s = String::new();
+
+    // Load queries
+    f.read_to_string(&mut s).unwrap();
+    let lines: Vec<String> = s.lines()
+        .filter(|l| !l.is_empty() && !l.starts_with("#"))
+        .map(|l| {
+            if !(l.ends_with("\n") || l.ends_with(";")) {
+                String::from(l) + "\n"
+            } else {
+                String::from(l)
+            }
+        })
+        .collect();
+    println!("Loaded {} {} queries", lines.len(), name);
+
+    // Try parsing them all
+    let (ok, _) = parse_queryset(lines);
+
+    // For the moment, we're always good
+    Ok(ok)
 }
 
 #[test]
 fn hotcrp_queries() {
-    let mut f = File::open("tests/hotcrp-queries.txt").unwrap();
-    let mut s = String::new();
-
-    // Load HotCRP queries
-    f.read_to_string(&mut s).unwrap();
-    let lines: Vec<String> = s.lines()
-        .filter(|l| !l.starts_with("#"))
-        .map(|l| {
-            if !(l.ends_with("\n") || l.ends_with(";")) {
-                String::from(l) + "\n"
-            } else {
-                String::from(l)
-            }
-        })
-        .collect();
-    println!("Loaded {} HotCRP queries", lines.len());
-
-    // Try parsing them all
-    parse_queryset(lines);
+    assert!(test_queries_from_file(Path::new("tests/hotcrp-queries.txt"), "HotCRP").is_ok());
 }
 
 #[test]
 fn hyrise_test_queries() {
-    let mut f = File::open("tests/hyrise-test-queries.txt").unwrap();
-    let mut s = String::new();
+    assert!(test_queries_from_file(Path::new("tests/hyrise-test-queries.txt"), "HyRise").is_ok());
+}
 
-    // Load HyRise queries
-    f.read_to_string(&mut s).unwrap();
-    let lines: Vec<String> = s.lines()
-        .filter(|l| !l.starts_with("#"))
-        .map(|l| {
-            if !(l.ends_with("\n") || l.ends_with(";")) {
-                String::from(l) + "\n"
-            } else {
-                String::from(l)
-            }
-        })
-        .collect();
-    println!("Loaded {} Hyrise queries", lines.len());
+#[test]
+fn tpcw_test_queries() {
+    assert!(test_queries_from_file(Path::new("tests/tpc-w-queries.txt"), "TPC-W").is_ok());
+}
 
-    // Try parsing them all
-    parse_queryset(lines);
+#[test]
+fn finkelstein82_test_queries() {
+    assert!(test_queries_from_file(Path::new("tests/finkelstein82.txt"), "Finkelstein 1982")
+        .is_ok())
 }
