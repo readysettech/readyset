readysettech/readyset

commit e73c8d90364fa2aa8689f2a3dc467cbcc2b27e02

Expand aliases only in field names
Another alias expansion bug; I *think* I've now restricted them to only
be used as non-functional labels, so they should not break any more
column resolutions.

src/sql/mir.rs
@@ -533,7 +533,17 @@ impl SqlToMirConverter {
         let literal_names: Vec<String> = literals.iter().map(|&(ref n, _)| n.clone()).collect();
         let fields = proj_cols.clone()
             .into_iter()
-            .cloned()
+            .map(|c| match c.alias {
+                     Some(ref a) => {
+                         Column {
+                             name: a.clone(),
+                             table: c.table.clone(),
+                             alias: Some(a.clone()),
+                             function: c.function.clone(),
+                         }
+                     }
+                     None => c.clone(),
+                 })
             .chain(literal_names.into_iter().map(|n| {
                 Column {
                     name: n,
@@ -544,11 +554,25 @@ impl SqlToMirConverter {
             }))
             .collect();
 
+        // remove aliases from emit columns because they are later compared to parent node columns
+        // and need to be equal. Note that `fields`, which holds the column names applied,
+        // preserves the aliases.
+        let emit_cols = proj_cols.into_iter()
+            .cloned()
+            .map(|mut c| {
+                match c.alias {
+                    Some(_) => c.alias = None,
+                    None => (),
+                };
+                c
+            })
+            .collect();
+
         MirNode::new(name,
                      self.schema_version,
                      fields,
                      MirNodeType::Project {
-                         emit: proj_cols.into_iter().cloned().collect(),
+                         emit: emit_cols,
                          literals: literals,
                      },
                      vec![parent_node.clone()],
@@ -904,29 +928,16 @@ impl SqlToMirConverter {
                 .collect();
 
             // 5. Generate leaf views that expose the query result
-            let projected_columns: Vec<Column> = sorted_rels.iter().fold(Vec::new(), |mut v, s| {
-                v.extend(qg.relations[*s].columns.clone().into_iter());
+            let projected_columns: Vec<&Column> = sorted_rels.iter().fold(Vec::new(), |mut v, s| {
+                v.extend(qg.relations[*s].columns.iter());
                 v
             });
 
             // translate aliases on leaf columns only
-            let fields = projected_columns.iter()
-                .map(|c| match c.alias {
-                         Some(ref a) => {
-                             Column {
-                                 name: a.clone(),
-                                 table: c.table.clone(),
-                                 alias: Some(a.clone()),
-                                 function: c.function.clone(),
-                             }
-                         }
-                         None => c.clone(),
-                     })
-                .collect::<Vec<Column>>();
             let leaf_project_node =
                 self.make_project_node(&format!("q_{:x}_n{}", qg.signature().hash, new_node_count),
                                        final_node,
-                                       fields.iter().collect(),
+                                       projected_columns,
                                        vec![]);
             nodes_added.push(leaf_project_node.clone());
 

commit 184e9e835dbb421d2d92bffb1301e5099601ae78

Apply alias on computed columns in grouped nodes
Normally, we only apply aliases at the leaf of a query, but the computed
columns in grouped operators (e.g., aggregations) are an exception:
because these are new columns that are (a) introduced by the operator
and (b) not identical to any parent columns or base table columns, we
can rename them to their aliases immediately. This fixes the tests
broken accidentally by e73c8d90364fa2aa8689f2a3dc467cbcc2b27e02.

src/sql/mir.rs
@@ -399,6 +399,20 @@ impl SqlToMirConverter {
         // Resolve column IDs in parent
         let over_col = over.1;
 
+        // move alias to name in computed column (which needs not to
+        // match against a parent node column, and is often aliased)
+        let computed_col = match computed_col.alias {
+            None => computed_col.clone(),
+            Some(ref a) => {
+                Column {
+                    name: a.clone(),
+                    alias: None,
+                    table: computed_col.table.clone(),
+                    function: computed_col.function.clone(),
+                }
+            }
+        };
+
         // The function node's set of output columns is the group columns plus the function
         // column
         let mut combined_columns = group_by.iter().map(|c| (*c).clone()).collect::<Vec<Column>>();