9ea0a7eda8b929884e685de8e1e098ef55cca474
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-08-18 22:58:58 -0400

    Allow aliases on nested selections in join clauses

--- src/join.rs
+++ src/join.rs
@@ -12,7 +12,8 @@ pub enum JoinRightSide {
     Table(Table),
     /// A comma-separated (and implicitly joined) sequence of tables.
     Tables(Vec<Table>),
-    NestedSelect(Box<SelectStatement>),
+    /// A nested selection, represented as (query, alias).
+    NestedSelect(Box<SelectStatement>, Option<String>),
     /// A nested join clause.
     NestedJoin(Box<JoinClause>),
 }
