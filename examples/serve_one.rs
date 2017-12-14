d64fff8e02a7acaad466bf208d256d4fc1a3f153
Jon Gjengset <jon@thesquareplanet.com>
2017-12-14 14:28:20 -0500

    Add example file

---
+++ examples/serve_one.rs
@@ -0,0 +1,53 @@
+//! After running this, you should be able to run:
+//!
+//! ```console
+//! $ echo "SELECT * FROM foo" | mysql -h 127.0.0.1 --table
+//! $
+//! ```
+
+extern crate msql_srv;
+extern crate mysql;
+
+use std::io;
+use std::net;
+use std::thread;
+use msql_srv::*;
+
+struct Backend;
+impl<W: io::Write> MysqlShim<W> for Backend {
+    fn param_info(&self, _: u32) -> &[Column] {
+        &[]
+    }
+
+    fn on_prepare(&mut self, _: &str, info: StatementMetaWriter<W>) -> io::Result<()> {
+        info.reply(42, &[], &[])
+    }
+    fn on_execute(
+        &mut self,
+        _: u32,
+        _: Vec<Value>,
+        results: QueryResultWriter<W>,
+    ) -> io::Result<()> {
+        results.completed(0, 0)
+    }
+    fn on_close(&mut self, _: u32) {}
+
+    fn on_query(&mut self, _: &str, results: QueryResultWriter<W>) -> io::Result<()> {
+        results.start(&[])?.finish()
+    }
+}
+
+fn main() {
+    let mut threads = Vec::new();
+    let listener = net::TcpListener::bind("127.0.0.1:3306").unwrap();
+
+    while let Ok((s, _)) = listener.accept() {
+        threads.push(thread::spawn(move || {
+            MysqlIntermediary::run_on_tcp(Backend, s).unwrap();
+        }));
+    }
+
+    for t in threads {
+        t.join().unwrap();
+    }
+}
