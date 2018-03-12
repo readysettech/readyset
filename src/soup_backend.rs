c671b77009abaaee32a4e87ce59002b08ef28e95
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2018-03-12 15:11:16 -0400

    Factor out some stuff from soup_backend module

--- src/soup_backend.rs
+++ src/soup_backend.rs
@@ -1,32 +1,12 @@
 use distributary::{ControllerHandle, DataType, Mutator, RemoteGetter, ZookeeperAuthority};
 
-use regex::Regex;
 use msql_srv::{self, *};
 use nom_sql::{self, ColumnConstraint, ColumnSpecification, SqlType};
 use slog;
 use std::io;
 use std::collections::{BTreeMap, HashMap};
 
-lazy_static! {
-    static ref HARD_CODED_REPLIES: Vec<(Regex, Vec<(&'static str, &'static str)>)> = vec![
-        (Regex::new(r"(?i)select version\(\) limit 1").unwrap(),
-         vec![("version()", "10.1.26-MariaDB-0+deb9u1")]),
-        (Regex::new(r"(?i)show engines").unwrap(),
-         vec![("Engine", "InnoDB"),
-              ("Support", "DEFAULT"),
-              ("Comment", ""),
-              ("Transactions", "YES"),
-              ("XA", "YES"),
-              ("Savepoints", "YES")]),
-        (Regex::new(r"SELECT 1 AS ping").unwrap(), vec![("ping", "1")]),
-        (Regex::new(r"(?i)show global variables like 'read_only'").unwrap(),
-         vec![("Variable_name", "read_only"), ("Value", "OFF")]),
-        (Regex::new(r"(?i)select get_lock\(.*\) as lockstatus").unwrap(),
-         vec![("lockstatus", "1")]),
-        (Regex::new(r"(?i)select release_lock\(.*\) as lockstatus").unwrap(),
-         vec![("lockstatus", "1")]),
-    ];
-}
+use utils;
 
 pub struct SoupBackend {
     soup: ControllerHandle<ZookeeperAuthority>,
@@ -298,11 +278,7 @@ impl<W: io::Write> MysqlShim<W> for SoupBackend {
     fn on_query(&mut self, query: &str, results: QueryResultWriter<W>) -> io::Result<()> {
         debug!(self.log, "query: {}", query);
 
-        let query = Regex::new(r"(?s)/\*.*\*/").unwrap().replace_all(query, "");
-        let query = Regex::new(r"--.*\n").unwrap().replace_all(&query, "\n");
-        let query = Regex::new(r" +").unwrap().replace_all(&query, " ");
-        let query = query.replace('"', "'");
-        let query = query.trim();
+        let query = utils::sanitize_query(query);
 
         if query.to_lowercase().contains("show databases")
             || query.to_lowercase().starts_with("begin")
@@ -331,11 +307,12 @@ impl<W: io::Write> MysqlShim<W> for SoupBackend {
                 },
             ];
             let writer = results.start(&cols)?;
+            println!(" -> Ok({} rows)", 0);
             return writer.finish();
         }
 
-        for &(ref pattern, ref columns) in &*HARD_CODED_REPLIES {
-            if pattern.is_match(query) {
+        for &(ref pattern, ref columns) in &*utils::HARD_CODED_REPLIES {
+            if pattern.is_match(&query) {
                 let cols: Vec<_> = columns
                     .iter()
                     .map(|c| Column {
