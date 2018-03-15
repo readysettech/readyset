3a943c6df6a96de52fec357c40e773a8c1e67351
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2018-03-14 23:44:47 -0400

    Make DeleteStatement and UpdateStatement public

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub use self::common::{FieldExpression, Literal, Operator, SqlType, TableKey};
 pub use self::column::{Column, ColumnConstraint, ColumnSpecification, FunctionExpression};
 pub use self::condition::{ConditionBase, ConditionExpression, ConditionTree};
 pub use self::create::CreateTableStatement;
+pub use self::delete::DeleteStatement;
 pub use self::insert::InsertStatement;
 pub use self::join::{JoinConstraint, JoinOperator, JoinRightSide};
 pub use self::parser::*;
@@ -18,6 +19,7 @@ pub use self::select::{GroupByClause, JoinClause, LimitClause, OrderClause, Orde
                        SelectStatement};
 pub use self::table::Table;
 pub use self::set::SetStatement;
+pub use self::update::UpdateStatement;
 
 pub mod parser;
 
