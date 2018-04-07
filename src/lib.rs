fcf74553c8445e44036355d1dab51851946fe1a6
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2018-04-06 22:32:48 -0400

    Revert "Remove unnecessary library target"
    This reverts commit f17d7c789d24101edddd3174ed8db8b5f02d521e.
    
    Tests actually need this library; whoops.

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![feature(box_syntax, box_patterns)]
+#![feature(try_from)]
+
+extern crate arccstr;
+extern crate distributary;
+extern crate msql_srv;
+extern crate nom_sql;
+extern crate regex;
+
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate slog;
+
+mod convert;
+mod rewrite;
+mod schema;
+mod soup_backend;
+mod utils;
+
+pub use soup_backend::SoupBackend;
