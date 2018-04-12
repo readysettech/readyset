5cb645e725d9e3bbb1c7632c6b50846098eb0783
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2018-04-12 19:30:25 -0400

    Handle schemas for columns without a table set
    Computed columns are not associated with a table, but we still need to
    return a MySQL column schema for them. Currently, we return an integer
    column with no flags set, which is probably right for most aggregations.
    However, it doesn't seem like client libraries really check this schema,
    so we should be fine either way.

--- src/schema.rs
+++ src/schema.rs
@@ -73,51 +73,59 @@ pub(crate) fn schema_for_column(
     table_schemas: &HashMap<String, CreateTableStatement>,
     c: &nom_sql::Column,
 ) -> msql_srv::Column {
-    let table = c.table.as_ref().unwrap();
-    let col_schema = table_schemas[table]
-        .fields
-        .iter()
-        .find(|cc| cc.column.name == c.name)
-        .expect(&format!("column {} not found", c.name));
-    assert_eq!(col_schema.column.name, c.name);
+    if let Some(ref table) = c.table {
+        let col_schema = table_schemas[table]
+            .fields
+            .iter()
+            .find(|cc| cc.column.name == c.name)
+            .expect(&format!("column {} not found", c.name));
+        assert_eq!(col_schema.column.name, c.name);
 
-    msql_srv::Column {
-        table: table.clone(),
-        column: c.name.clone(),
-        coltype: match col_schema.sql_type {
-            SqlType::Mediumtext => msql_srv::ColumnType::MYSQL_TYPE_VAR_STRING,
-            SqlType::Longtext => msql_srv::ColumnType::MYSQL_TYPE_BLOB,
-            SqlType::Text => msql_srv::ColumnType::MYSQL_TYPE_STRING,
-            SqlType::Varchar(_) => msql_srv::ColumnType::MYSQL_TYPE_VAR_STRING,
-            SqlType::Int(_) => msql_srv::ColumnType::MYSQL_TYPE_LONG,
-            SqlType::Bigint(_) => msql_srv::ColumnType::MYSQL_TYPE_LONGLONG,
-            SqlType::Tinyint(_) => msql_srv::ColumnType::MYSQL_TYPE_TINY,
-            SqlType::Bool => msql_srv::ColumnType::MYSQL_TYPE_BIT,
-            SqlType::DateTime => msql_srv::ColumnType::MYSQL_TYPE_DATETIME,
-            SqlType::Float => msql_srv::ColumnType::MYSQL_TYPE_DOUBLE,
-            SqlType::Decimal(_, _) => msql_srv::ColumnType::MYSQL_TYPE_DECIMAL,
-            _ => unimplemented!(),
-        },
-        colflags: {
-            let mut flags = msql_srv::ColumnFlags::empty();
-            for c in &col_schema.constraints {
-                match *c {
-                    ColumnConstraint::AutoIncrement => {
-                        flags |= msql_srv::ColumnFlags::AUTO_INCREMENT_FLAG;
+        msql_srv::Column {
+            table: table.clone(),
+            column: c.name.clone(),
+            coltype: match col_schema.sql_type {
+                SqlType::Mediumtext => msql_srv::ColumnType::MYSQL_TYPE_VAR_STRING,
+                SqlType::Longtext => msql_srv::ColumnType::MYSQL_TYPE_BLOB,
+                SqlType::Text => msql_srv::ColumnType::MYSQL_TYPE_STRING,
+                SqlType::Varchar(_) => msql_srv::ColumnType::MYSQL_TYPE_VAR_STRING,
+                SqlType::Int(_) => msql_srv::ColumnType::MYSQL_TYPE_LONG,
+                SqlType::Bigint(_) => msql_srv::ColumnType::MYSQL_TYPE_LONGLONG,
+                SqlType::Tinyint(_) => msql_srv::ColumnType::MYSQL_TYPE_TINY,
+                SqlType::Bool => msql_srv::ColumnType::MYSQL_TYPE_BIT,
+                SqlType::DateTime => msql_srv::ColumnType::MYSQL_TYPE_DATETIME,
+                SqlType::Float => msql_srv::ColumnType::MYSQL_TYPE_DOUBLE,
+                SqlType::Decimal(_, _) => msql_srv::ColumnType::MYSQL_TYPE_DECIMAL,
+                _ => unimplemented!(),
+            },
+            colflags: {
+                let mut flags = msql_srv::ColumnFlags::empty();
+                for c in &col_schema.constraints {
+                    match *c {
+                        ColumnConstraint::AutoIncrement => {
+                            flags |= msql_srv::ColumnFlags::AUTO_INCREMENT_FLAG;
+                        }
+                        ColumnConstraint::NotNull => {
+                            flags |= msql_srv::ColumnFlags::NOT_NULL_FLAG;
+                        }
+                        ColumnConstraint::PrimaryKey => {
+                            flags |= msql_srv::ColumnFlags::PRI_KEY_FLAG;
+                        }
+                        ColumnConstraint::Unique => {
+                            flags |= msql_srv::ColumnFlags::UNIQUE_KEY_FLAG;
+                        }
+                        _ => (),
                     }
-                    ColumnConstraint::NotNull => {
-                        flags |= msql_srv::ColumnFlags::NOT_NULL_FLAG;
-                    }
-                    ColumnConstraint::PrimaryKey => {
-                        flags |= msql_srv::ColumnFlags::PRI_KEY_FLAG;
-                    }
-                    ColumnConstraint::Unique => {
-                        flags |= msql_srv::ColumnFlags::UNIQUE_KEY_FLAG;
-                    }
-                    _ => (),
                 }
-            }
-            flags
-        },
+                flags
+            },
+        }
+    } else {
+        msql_srv::Column {
+            table: "".into(),
+            column: c.name.clone(),
+            coltype: msql_srv::ColumnType::MYSQL_TYPE_LONG,
+            colflags: msql_srv::ColumnFlags::empty(),
+        }
     }
 }
