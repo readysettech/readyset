d6a128179ae4b3a382c11d99f8879a1281c41628
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2018-05-07 13:30:53 -0400

    Clearer error message

--- src/soup_backend.rs
+++ src/soup_backend.rs
@@ -706,9 +706,10 @@ impl SoupBackend {
                         }
 
                         for c in schema {
-                            let coli = cols.iter()
-                                .position(|f| f == &c.column)
-                                .expect("tried to emit column not in getter");
+                            let coli = cols.iter().position(|f| f == &c.column).expect(&format!(
+                                "tried to emit column {:?} not in getter for {:?} with schema {:?}",
+                                c.column, qname, cols
+                            ));
                             write_column(&mut rw, &r[coli], c);
                         }
                         rw.end_row()?;
