3b777cfe2b02f65ea48006f44b89d986ac05c969
Griffin Smith <root@gws.fyi>
2020-12-02 11:29:49 -0500

    Implement all remaining column types
    Implement schema::convert_column for all column types in the
    nom_sql::SqlType enum. A lot of this was based on guesswork on my part,
    as the mysql binary protocol documentation (I was mostly looking at
    https://dev.mysql.com/doc/internals/en/binary-protocol-value.html) is a
    little thin, so more testing is probably in order to make sure all of
    this works. That said, with this change the following query on the
    employees.tiny.noria.sql test db is working:
    
        SELECT first_name, employees.emp_no, dept_no
        FROM employees
        JOIN dept_emp ON employees.emp_no = dept_emp.emp_no

--- src/schema.rs
+++ src/schema.rs
@@ -9,34 +9,73 @@ pub enum Schema {
 
 pub(crate) fn convert_column(cs: &ColumnSpecification) -> msql_srv::Column {
     let mut flags = msql_srv::ColumnFlags::empty();
+    use msql_srv::ColumnType::*;
     msql_srv::Column {
         table: cs.column.table.clone().unwrap_or("".to_owned()),
         column: cs.column.name.clone(),
         coltype: match cs.sql_type {
-            SqlType::Mediumtext => msql_srv::ColumnType::MYSQL_TYPE_VAR_STRING,
-            SqlType::Longtext => msql_srv::ColumnType::MYSQL_TYPE_BLOB,
-            SqlType::Text => msql_srv::ColumnType::MYSQL_TYPE_STRING,
-            SqlType::Varchar(_) => msql_srv::ColumnType::MYSQL_TYPE_VAR_STRING,
-            SqlType::Int(_) => msql_srv::ColumnType::MYSQL_TYPE_LONG,
+            SqlType::Mediumtext => MYSQL_TYPE_VAR_STRING,
+            SqlType::Longtext => MYSQL_TYPE_BLOB,
+            SqlType::Text => MYSQL_TYPE_STRING,
+            SqlType::Varchar(_) => MYSQL_TYPE_VAR_STRING,
+            SqlType::Int(_) => MYSQL_TYPE_LONG,
             SqlType::UnsignedInt(_) => {
                 flags |= msql_srv::ColumnFlags::UNSIGNED_FLAG;
-                msql_srv::ColumnType::MYSQL_TYPE_LONG
-            },
-            SqlType::Bigint(_) => msql_srv::ColumnType::MYSQL_TYPE_LONGLONG,
+                MYSQL_TYPE_LONG
+            }
+            SqlType::Bigint(_) => MYSQL_TYPE_LONGLONG,
             SqlType::UnsignedBigint(_) => {
                 flags |= msql_srv::ColumnFlags::UNSIGNED_FLAG;
-                msql_srv::ColumnType::MYSQL_TYPE_LONGLONG
-            },
-            SqlType::Tinyint(_) => msql_srv::ColumnType::MYSQL_TYPE_TINY,
+                MYSQL_TYPE_LONGLONG
+            }
+            SqlType::Tinyint(_) => MYSQL_TYPE_TINY,
             SqlType::UnsignedTinyint(_) => {
                 flags |= msql_srv::ColumnFlags::UNSIGNED_FLAG;
-                msql_srv::ColumnType::MYSQL_TYPE_TINY
-            },
-            SqlType::Bool => msql_srv::ColumnType::MYSQL_TYPE_BIT,
-            SqlType::DateTime(_) => msql_srv::ColumnType::MYSQL_TYPE_DATETIME,
-            SqlType::Float => msql_srv::ColumnType::MYSQL_TYPE_DOUBLE,
-            SqlType::Decimal(_, _) => msql_srv::ColumnType::MYSQL_TYPE_DECIMAL,
-            _ => unimplemented!(),
+                MYSQL_TYPE_TINY
+            }
+            SqlType::Bool => MYSQL_TYPE_BIT,
+            SqlType::DateTime(_) => MYSQL_TYPE_DATETIME,
+            SqlType::Float => MYSQL_TYPE_DOUBLE,
+            SqlType::Decimal(_, _) => MYSQL_TYPE_DECIMAL,
+            SqlType::Char(_) => {
+                // TODO(grfn): I'm not sure if this is right
+                MYSQL_TYPE_STRING
+            }
+            SqlType::Blob => MYSQL_TYPE_BLOB,
+            SqlType::Longblob => MYSQL_TYPE_LONG_BLOB,
+            SqlType::Mediumblob => MYSQL_TYPE_MEDIUM_BLOB,
+            SqlType::Tinyblob => MYSQL_TYPE_TINY_BLOB,
+            SqlType::Double => MYSQL_TYPE_DOUBLE,
+            SqlType::Real => {
+                // a generous reading of
+                // https://dev.mysql.com/doc/refman/8.0/en/floating-point-types.html seems to
+                // indicate that real is equivalent to float
+                // TODO(grfn): Make sure that's the case
+                MYSQL_TYPE_FLOAT
+            }
+            SqlType::Tinytext => {
+                // TODO(grfn): How does the mysql binary protocol handle
+                // tinytext? is it just an alias for tinyblob or is there a flag
+                // we need?
+                unimplemented!()
+            }
+            SqlType::Date => MYSQL_TYPE_DATE,
+            SqlType::Timestamp => MYSQL_TYPE_TIMESTAMP,
+            SqlType::Binary(_) => {
+                // TODO(grfn): I don't know if this is right
+                flags |= msql_srv::ColumnFlags::BINARY_FLAG;
+                MYSQL_TYPE_STRING
+            }
+            SqlType::Varbinary(_) => {
+                // TODO(grfn): I don't know if this is right
+                flags |= msql_srv::ColumnFlags::BINARY_FLAG;
+                MYSQL_TYPE_VAR_STRING
+            }
+            SqlType::Enum(_) => {
+                // TODO(grfn): I don't know if this is right
+                flags |= msql_srv::ColumnFlags::ENUM_FLAG;
+                MYSQL_TYPE_VAR_STRING
+            }
         },
         colflags: {
             for c in &cs.constraints {
