66b7af98414643540c7bacc2d3c15923e3f78df8
Griffin Smith <root@gws.fyi>
2020-12-02 18:45:22 -0500

    Impl Display for ALTER TABLE
    add a Display impl for the various pieces of the ALTER TABLE AST, which
    print the SQL representation of the statement.

--- src/alter.rs
+++ src/alter.rs
@@ -1,6 +1,8 @@
 /// ALTER TABLE Statement AST and parsing (incomplete)
 ///
 /// See https://dev.mysql.com/doc/refman/8.0/en/alter-table.html
+use std::fmt;
+
 use crate::{ColumnSpecification, Literal, Table};
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
@@ -9,10 +11,30 @@ pub enum AlterColumnOperation {
     DropColumnDefault,
 }
 
+impl fmt::Display for AlterColumnOperation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AlterColumnOperation::SetColumnDefault(val) => {
+                write!(f, "SET DEFAULT {}", val.to_string())
+            }
+            AlterColumnOperation::DropColumnDefault => write!(f, "DROP DEFAULT"),
+        }
+    }
+}
+
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
 pub enum DropBehavior {
     Cascade,
     Restrict,
+}
+
+impl fmt::Display for DropBehavior {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            DropBehavior::Cascade => write!(f, "CASCADE"),
+            DropBehavior::Restrict => write!(f, "RESTRICT"),
+        }
+    }
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
@@ -32,8 +54,77 @@ pub enum AlterTableDefinition {
     // DropTableConstraint(..),
 }
 
+impl fmt::Display for AlterTableDefinition {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AlterTableDefinition::AddColumn(col) => {
+                write!(f, "ADD COLUMN {}", col)
+            }
+            AlterTableDefinition::AlterColumn { name, operation } => {
+                write!(f, "ALTER COLUMN {} {}", name, operation)
+            }
+            AlterTableDefinition::DropColumn { name, behavior } => {
+                write!(f, "DROP COLUMN {}", name)?;
+                if let Some(behavior) = behavior {
+                    write!(f, " {}", behavior)?;
+                }
+                Ok(())
+            }
+        }
+    }
+}
+
 #[derive(Clone, Debug, Default, Eq, Hash, PartialEq, Serialize, Deserialize)]
 pub struct AlterTableStatement {
     pub table: Table,
     pub definitions: Vec<AlterTableDefinition>,
+}
+
+impl fmt::Display for AlterTableStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ALTER TABLE {} ", escape_if_keyword(&self.table.name))?;
+        write!(
+            f,
+            "{}",
+            self.definitions
+                .iter()
+                .map(|def| format!("{}", def))
+                .collect::<Vec<_>>()
+                .join(", ")
+        )?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Column, SqlType};
+
+    use super::*;
+
+    #[test]
+    fn display_add_column() {
+        let stmt = AlterTableStatement {
+            table: "t".into(),
+            definitions: vec![AlterTableDefinition::AddColumn(ColumnSpecification {
+                column: Column {
+                    name: "c".into(),
+                    alias: None,
+                    table: None,
+                    function: None,
+                },
+                sql_type: SqlType::Int(32),
+                comment: None,
+                constraints: vec![],
+            })],
+        };
+
+        let result = format!("{}", stmt);
+        assert_eq!(result, "ALTER TABLE t ADD COLUMN c INT(32)");
+    }
+
+    #[test]
+    fn parse_add_column() {
+        let qstring = "ALTER TABLE `t` ADD COLUMN `c` INT";
+    }
 }
