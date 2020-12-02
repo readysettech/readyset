84506bfd2a94157bff905b78b9fef169acd478ea
Griffin Smith <root@gws.fyi>
2020-12-02 16:22:08 -0500

    Export ItemPlaceholder from the top level
    Yet more AST nodes that aren't exported at the toplevel yet

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,8 @@ pub use self::column::{
     Column, ColumnConstraint, ColumnSpecification, FunctionArgument, FunctionExpression,
 };
 pub use self::common::{
-    FieldDefinitionExpression, FieldValueExpression, Literal, LiteralExpression, Operator, Real,
-    SqlType, TableKey,
+    FieldDefinitionExpression, FieldValueExpression, ItemPlaceholder, Literal, LiteralExpression,
+    Operator, Real, SqlType, TableKey,
 };
 pub use self::compound_select::{CompoundSelectOperator, CompoundSelectStatement};
 pub use self::condition::{ConditionBase, ConditionExpression, ConditionTree};
