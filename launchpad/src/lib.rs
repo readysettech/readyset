849e12d8cb6cd7a34998bbf95008e782539c5245
Griffin Smith <root@gws.fyi>
2021-01-14 10:08:31 -0500

    Init Launchpad crate
    Init a new top-level Launchpad crate, which is intended to provide a
    general grab-bag of utility functions and extensions to the standard
    library for use in *all* crates in the workspace. I'm going to be
    porting the interval utilities and Bound extensions here next.

---
+++ launchpad/src/lib.rs
@@ -0,0 +1,3 @@
+//! This crate provides miscellanious utilities and extensions to the Rust standard library, for use
+//! in all crates in this workspace.
+#![deny(missing_docs, missing_crate_level_docs)]
