16922b7b00b1afb8a2a00cfa51d7d723fa38096f
Francisco Noriega <frannoriega.92@gmail.com>
2021-02-10 15:53:40 -0300

    Created docker stack for easy testing
    This commit adds a docker stack that will allow us to easily bring up the whole
    context for running tests with Debezium.

---
+++ docker-stacks/debezium-stack/mariadb/docker-entrypoint-initdb.d/01-product.sql
@@ -0,0 +1,6 @@
+CREATE TABLE product (
+    id      INT             NOT NULL,
+    name  VARCHAR(14)     NOT NULL,
+    price   DOUBLE     NOT NULL
+--    PRIMARY KEY (id)
+);
---
+++ docker-stacks/debezium-stack/postgres/docker-entrypoint-initdb.d/01-review.sql
@@ -0,0 +1,6 @@
+CREATE TABLE review (
+    id      INT             NOT NULL,
+    prod_id  INT     NOT NULL,
+    rating   INT     NOT NULL
+--    PRIMARY KEY (id)
+);
