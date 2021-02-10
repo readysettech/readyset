41b0b370e74dcf5727f04bcd3a3ceb824171f27f
Francisco Noriega <frannoriega.92@gmail.com>
2021-02-10 15:57:54 -0300

    Minor improvement to the view_checker.rs tool
    This commit adds the capability to specify which view query to run against Noria
     as a command line parameter, thus avoiding having to recompile the test
    each time a new query needs to be tested.
    The new command line parameter is `--query [name of the query]` or `-q [name of
    the query]` for short. If no query is specified, a default value will be used,
    which is `testQuery` as it was hardcoded before, for backwards compatibility.

--- connectors/debezium_connector/test/view_checker.rs
+++ connectors/debezium_connector/test/view_checker.rs
@@ -1,3 +1,4 @@
+use clap::{App, Arg};
 use noria::consensus::ZookeeperAuthority;
 use noria::Builder;
 use std::net::IpAddr;
@@ -8,13 +9,30 @@ use std::sync::Arc;
 // is named `myapp` and that its listening on address 127.0.0.1
 #[tokio::main]
 async fn main() {
+    // Allow for a query to be passed as parameter, to avoid
+    // recompiling each time we want to get a view from a different query.
+    // If no value is provided, then it defaults to "testQuery", for
+    // backwards compatibility.
+    let matches = App::new("view-checker")
+        .version("0.0.1")
+        .arg(
+            Arg::with_name("query")
+                .short("q")
+                .long("query")
+                .takes_value(true)
+                .multiple(false)
+                .required(false)
+                .help("Query to visualize."),
+        )
+        .get_matches();
     let authority = ZookeeperAuthority::new(&"127.0.0.1:2181/myapp");
     let mut builder = Builder::default();
     builder.set_listen_addr(IpAddr::from_str("127.0.0.1").unwrap());
     let (mut noria_authority, _) = builder.start(Arc::new(authority.unwrap())).await.unwrap();
 
     println!("Waiting for noria");
-    let mut getter = noria_authority.view(&"testQuery").await.unwrap();
+    let query = matches.value_of("query").unwrap_or("testQuery");
+    let mut getter = noria_authority.view(query).await.unwrap();
     let results = getter.lookup(&[0.into()], true).await.unwrap();
     println!("Results: {:?}", results);
 }
