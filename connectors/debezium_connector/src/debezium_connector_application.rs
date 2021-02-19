f8311cc30901bfb021abd9aad51e4a2162876e6e
Justin Miron <justinmiron11@gmail.com>
2021-02-19 18:29:09 +0000

    Refactor debezium connector application to support arbitrary topics.
    The debezium connector currently assumes a single message structure from
    each topic. With future work listening to timestamp metadata, the
    connector application will have to a new message structure. This
    refactors the code to support handling messages from  each topic separately.

--- connectors/debezium_connector/src/debezium_connector_application.rs
+++ connectors/debezium_connector/src/debezium_connector_application.rs
@@ -6,18 +6,29 @@ use noria::Builder;
 use noria::Handle;
 use rdkafka::consumer::{CommitMode, Consumer};
 use rdkafka::Message;
+use std::collections::HashMap;
+use std::convert::TryInto;
 use std::net::IpAddr;
 use std::sync::Arc;
 use tokio::stream::StreamExt;
 
 mod debezium_message_parser;
 mod kafka_message_consumer_wrapper;
-use debezium_message_parser::{DataChangePayload, EventKey, EventValue};
+use debezium_message_parser::{DataChange, DataChangePayload, EventKey, EventValue, SchemaChange};
+
+/// Kafka topics the debezium connector reads from.
+enum Topic {
+    /// Schema change events that include all DDL statements applied to a database.
+    SchemaChange,
+    /// Data changes associated with each row-level INSERT, UPDATE, or DELETE.
+    DataChange,
+}
 
 pub struct DebeziumConnector {
     kafka_consumer: kafka_message_consumer_wrapper::KafkaMessageConsumerWrapper,
     noria_ip: Option<IpAddr>,
     zookeeper_conn: String,
+    topics: HashMap<String, Topic>,
 }
 
 impl DebeziumConnector {
@@ -34,13 +45,19 @@ impl DebeziumConnector {
         auto_commit: bool,
     ) -> DebeziumConnector {
         // for each table, we listen to the topic <dbserver>.<dbname>.<tablename>
-        let mut topic_names: Vec<String> = tables
+        let mut topic_names: Vec<String> = Vec::new();
+        let mut topics: HashMap<String, Topic> = HashMap::new();
+        tables
             .iter()
             .map(|table_name| format!("{}.{}.{}", server_name, db_name, table_name))
-            .collect();
+            .for_each(|t| {
+                topic_names.push(t.clone());
+                topics.insert(t, Topic::DataChange);
+            });
 
         // we also listen to the schema change topic, which is just named <dbserver>
-        topic_names.push(server_name);
+        topic_names.push(server_name.clone());
+        topics.insert(server_name, Topic::SchemaChange);
 
         let consumer = kafka_message_consumer_wrapper::KafkaMessageConsumerWrapper::new(
             bootstrap_servers,
@@ -55,53 +72,55 @@ impl DebeziumConnector {
             kafka_consumer: consumer,
             noria_ip,
             zookeeper_conn,
+            topics,
         }
     }
 
-    async fn handle_message(
+    async fn handle_schema_message(
         noria_authority: &mut Handle<ZookeeperAuthority>,
-        key_message: &EventKey,
-        value_message: &EventValue,
+        message: SchemaChange,
     ) -> Result<()> {
-        match value_message {
-            EventValue::SchemaChange(e) => {
-                noria_authority.extend_recipe(&e.payload.ddl).await?;
-            }
-            EventValue::DataChange(e) => {
-                match &e.payload {
-                    DataChangePayload::Create(p) => {
-                        // We know that the payload consist of before, after and source fields
-                        // and that too in that specific order.
-                        if let Some(table_name) = &p.source.table {
-                            let after_field_schema = &e.schema.fields[1];
-                            let create_vector = p.get_create_vector(after_field_schema)?;
-                            let mut table_mutator = noria_authority.table(table_name).await?;
-                            table_mutator.insert(create_vector).await?
-                        }
-                    }
-                    DataChangePayload::Update(p) => {
-                        if let Some(table_name) = &p.source.table {
-                            let pk_datatype = key_message.get_pk_datatype()?;
-                            // We know that the payload consist of before, after and source fields
-                            // and that too in that specific order.
-                            let after_field_schema = &e.schema.fields[1];
-                            let update_vector = p.get_update_vector(after_field_schema)?;
-                            let mut table_mutator = noria_authority.table(table_name).await?;
-                            table_mutator
-                                .update(vec![pk_datatype], update_vector)
-                                .await?
-                        }
-                    }
-                    DataChangePayload::Delete { source: src } => {
-                        if let Some(table_name) = &src.table {
-                            let pk_datatype = key_message.get_pk_datatype()?;
-                            let mut table_mutator = noria_authority.table(table_name).await?;
-                            table_mutator.delete(vec![pk_datatype]).await?
-                        }
-                    }
+        noria_authority.extend_recipe(&message.payload.ddl).await?;
+        Ok(())
+    }
+
+    async fn handle_change_message(
+        noria_authority: &mut Handle<ZookeeperAuthority>,
+        key_message: EventKey,
+        message: DataChange,
+    ) -> Result<()> {
+        match &message.payload {
+            DataChangePayload::Create(p) => {
+                // We know that the payload consist of before, after and source fields
+                // and that too in that specific order.
+                if let Some(table_name) = &p.source.table {
+                    let after_field_schema = &message.schema.fields[1];
+                    let create_vector = p.get_create_vector(after_field_schema)?;
+                    let mut table_mutator = noria_authority.table(table_name).await?;
+                    table_mutator.insert(create_vector).await?
                 }
             }
-        };
+            DataChangePayload::Update(p) => {
+                if let Some(table_name) = &p.source.table {
+                    let pk_datatype = key_message.get_pk_datatype()?;
+                    // We know that the payload consist of before, after and source fields
+                    // and that too in that specific order.
+                    let after_field_schema = &message.schema.fields[1];
+                    let update_vector = p.get_update_vector(after_field_schema)?;
+                    let mut table_mutator = noria_authority.table(table_name).await?;
+                    table_mutator
+                        .update(vec![pk_datatype], update_vector)
+                        .await?
+                }
+            }
+            DataChangePayload::Delete { source: src } => {
+                if let Some(table_name) = &src.table {
+                    let pk_datatype = key_message.get_pk_datatype()?;
+                    let mut table_mutator = noria_authority.table(table_name).await?;
+                    table_mutator.delete(vec![pk_datatype]).await?
+                }
+            }
+        }
         Ok(())
     }
 
@@ -116,26 +135,43 @@ impl DebeziumConnector {
         while let Some(message) = message_stream.next().await {
             if let Ok(m) = message {
                 let owned_message = m.detach();
-                // We have to check existence because on deletes, a tombstone message is sent by the kafka connector.
-                // We really dont use the for anything, so we just ignore them for now.
-                if owned_message.payload().is_some() {
-                    let key_string = std::str::from_utf8(m.key().unwrap())?;
-                    let value_string = std::str::from_utf8(m.payload().unwrap())?;
 
-                    let key_message: EventKey = serde_json::from_str(&key_string)?;
-                    let value_message: EventValue = serde_json::from_str(&value_string)?;
-
-                    DebeziumConnector::handle_message(
-                        &mut noria_authority,
-                        &key_message,
-                        &value_message,
-                    )
-                    .await?;
-
+                if owned_message.payload().is_none() {
                     self.kafka_consumer
                         .kafka_consumer
                         .commit_message(&m, CommitMode::Async)?;
+                    return Ok(());
                 }
+
+                let value_string = std::str::from_utf8(owned_message.payload().unwrap())?;
+                let value_message: EventValue = serde_json::from_str(&value_string)?;
+                let topic = self.topics.get(owned_message.topic()).unwrap();
+
+                match topic {
+                    Topic::SchemaChange => {
+                        DebeziumConnector::handle_schema_message(
+                            &mut noria_authority,
+                            value_message.try_into().unwrap(),
+                        )
+                        .await?;
+                    }
+                    Topic::DataChange => {
+                        // We have to check existence because on deletes, a tombstone message is sent by the kafka connector.
+                        // We really dont use the for anything, so we just ignore them for now.
+                        let key_string = std::str::from_utf8(owned_message.key().unwrap())?;
+                        let key_message = serde_json::from_str(&key_string)?;
+                        DebeziumConnector::handle_change_message(
+                            &mut noria_authority,
+                            key_message,
+                            value_message.try_into().unwrap(),
+                        )
+                        .await?;
+                    }
+                }
+
+                self.kafka_consumer
+                    .kafka_consumer
+                    .commit_message(&m, CommitMode::Async)?;
             }
         }
         Ok(())
