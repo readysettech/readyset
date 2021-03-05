f21d854197851fc5667e9c3df3f06641960717c0
Griffin Smith <root@gws.fyi>
2021-03-05 09:40:08 -0500

    Remove obsolete deployment/ directory
    A lot of the stuff in here was really a proof-of-concept, and it's not
    being used or depended on anywhere at the moment - not to mention that
    we shouldn't have private ssh keys committed to github. I'm removing it
    for now as I start to work on a more robust and long-term deployment
    strategy, but we can bring back pieces of this in the future if we need
    to

--- deployment/pull.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo docker pull 069491470376.dkr.ecr.us-east-2.amazonaws.com/noria-deploy:base
-sudo docker tag 069491470376.dkr.ecr.us-east-2.amazonaws.com/noria-deploy:base readyset-base:latest 
-sudo docker pull 069491470376.dkr.ecr.us-east-2.amazonaws.com/noria-deploy:deploy
-sudo docker tag 069491470376.dkr.ecr.us-east-2.amazonaws.com/noria-deploy:deploy readyset-deploy:latest
