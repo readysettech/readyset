f59131dc77dc2e827e40ff7392fe0ffe7c837b8d
Griffin Smith <root@gws.fyi>
2021-03-10 10:56:14 -0500

    Handle NVME block devices when mounting volumes
    Sometimes - and for reasons I don't really understand - AWS exposes EBS
    volumes as *numbered* nvme block devices instead of the block device
    names they're *explicitly configured with*. If this happens, the only
    way to figure out which block device is which from the perspective of
    the block device names returned from the AWS metadata service is to use
    the nvme CLI to print out the vendor-specific extension data as *binary*
    and index into it at a fixed index. This is absolutely bonkers, but I
    have something that uses the nvme-cli and a cursed pile of shell
    pipelining to successfully search for the name of the nvme block device
    corresponding to the block device given to us by the AWS metadata
    service.
    
    Baking this into the user-data script is getting a *little* out of hand
    - we likely want to change this to just be a script inside of the AMI at
    some point - but this works for now.

--- ops/noria_module/noria_server_init.sh
+++ ops/noria_module/noria_server_init.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+find_nvme_device() {
+    local block_dev="$1"
+    apt-get -y update >&2
+    apt-get -y install nvme-cli jq >&2
+    nvme_devices=$(nvme list -o json | jq -r '.Devices | map(.DevicePath) | .[]')
+    for nvme_dev in $nvme_devices; do
+        ebs_block_dev=$(nvme id-ctrl -vb "$nvme_dev" 2>/dev/null | tr '\0' ' ' | cut -b 3073-3075)
+        if [ "$ebs_block_dev" = "$block_dev" ]; then
+            echo "$nvme_dev"
+            return 0
+        fi
+    done
+    >&2 echo "Could not find nvme device corresponding to block device $block_dev"
+    return 1
+}
+
 sudo tee /etc/default/noria > /dev/null <<EOF
 NORIA_DEPLOYMENT=${deployment}
 ZOOKEEPER_URL=${zookeeper_ip}:2181
@@ -9,10 +25,18 @@ NORIA_QUORUM=${quorum}
 NORIA_SHARDS=${shards}
 EOF
 
-block_device=/dev/$(curl http://169.254.169.254/latest/meta-data/block-device-mapping/ebs1)
-if [ ! -f "$block_device" ]; then
-    block_device="$${block_device/\/s/\/xv}"
+block_device_name=$(curl http://169.254.169.254/latest/meta-data/block-device-mapping/ebs1)
+if [ -f "/dev/$block_device_name" ]; then
+    block_device="/dev/$block_device_name"
+else
+    with_xv="$${block_device_name/s/xv}"
+    if [ -f "/dev/$with_xv" ]; then
+        block_device="/dev/$with_xv"
+    else
+        block_device="$(find_nvme_device "$block_device_name")"
+    fi
 fi
+
 mkfs.ext4 "$block_device"
 mount "$block_device" /var/lib/noria
 echo "$block_device /var/lib/noria ext4 rw,discard,x-systemd.growfs 0 0" >> /etc/fstab
--- ops/noria_module/zookeeper_init.sh
+++ ops/noria_module/zookeeper_init.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-block_device=/dev/$(curl http://169.254.169.254/latest/meta-data/block-device-mapping/ebs1)
-if [ ! -f "$block_device" ]; then
-    block_device="${block_device/\/s/\/xv}"
+find_nvme_device() {
+    local block_dev="$1"
+    apt-get -y update >&2
+    apt-get -y install nvme-cli jq >&2
+    nvme_devices=$(nvme list -o json | jq -r '.Devices | map(.DevicePath) | .[]')
+    for nvme_dev in $nvme_devices; do
+        ebs_block_dev=$(nvme id-ctrl -vb "$nvme_dev" 2>/dev/null | tr '\0' ' ' | cut -b 3073-3075)
+        if [ "$ebs_block_dev" = "$block_dev" ]; then
+            echo "$nvme_dev"
+            return 0
+        fi
+    done
+    >&2 echo "Could not find nvme device corresponding to block device $block_dev"
+    return 1
+}
+
+block_device_name=$(curl http://169.254.169.254/latest/meta-data/block-device-mapping/ebs1)
+if [ -f "/dev/$block_device_name" ]; then
+    block_device="/dev/$block_device_name"
+else
+    with_xv="$${block_device_name/s/xv}"
+    if [ -f "/dev/$with_xv" ]; then
+        block_device="/dev/$with_xv"
+    else
+        block_device="$(find_nvme_device "$block_device_name")"
+    fi
 fi
+
 mkfs.ext4 "$block_device"
 mount "$block_device" /var/lib/zookeeper
 echo "$block_device /var/lib/zookeeper ext4 rw,discard,x-systemd.growfs 0 0" >> /etc/fstab
