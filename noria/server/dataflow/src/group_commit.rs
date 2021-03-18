bff66af837e0585ec16d3fc7cdeaac115f5d2953
eta <eta@theta.eu.org>
2021-03-18 14:38:38 +0000

    error handling: make `GroupCommitQueueSet` return ReadySetResults
    `noria/server/dataflow/src/group_commit.rs` is now error-handling aware,
    and its callsites in `Domain` have been updated accordingly.
    
    META: At this point in the patch series, the `noria-dataflow` crate is
    pretty much completely refactored. Yay!

--- noria/server/dataflow/src/group_commit.rs
+++ noria/server/dataflow/src/group_commit.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
 use core::convert::TryInto;
-use noria::{internal::LocalOrNot, PacketData, TableOperation};
+use noria::errors::{internal_err, ReadySetResult};
+use noria::{internal, internal::LocalOrNot, invariant_eq, PacketData, TableOperation};
 use std::time;
 
 pub struct GroupCommitQueueSet {
@@ -30,7 +31,7 @@ impl GroupCommitQueueSet {
     }
 
     /// Find the first queue that has timed out waiting for more packets, and flush it to disk.
-    pub fn flush_if_necessary(&mut self) -> Option<Box<Packet>> {
+    pub fn flush_if_necessary(&mut self) -> ReadySetResult<Option<Box<Packet>>> {
         let now = time::Instant::now();
         let to = self.params.flush_timeout;
         let node = self
@@ -39,21 +40,21 @@ impl GroupCommitQueueSet {
             .find(|(_, &(first, ref ps))| now.duration_since(first) >= to && !ps.is_empty())
             .map(|(n, _)| n);
 
-        if let Some(node) = node {
-            self.flush_internal(node)
+        Ok(if let Some(node) = node {
+            self.flush_internal(node)?
         } else {
             None
-        }
+        })
     }
 
     /// Merge any pending packets.
-    fn flush_internal(&mut self, node: LocalNodeIndex) -> Option<Box<Packet>> {
+    fn flush_internal(&mut self, node: LocalNodeIndex) -> ReadySetResult<Option<Box<Packet>>> {
         Self::merge_packets(&mut self.pending_packets[node].1)
     }
 
     /// Add a new packet to be persisted, and if this triggered a flush return an iterator over the
     /// packets that were written.
-    pub fn append(&mut self, p: Box<Packet>) -> Option<Box<Packet>> {
+    pub fn append(&mut self, p: Box<Packet>) -> ReadySetResult<Option<Box<Packet>>> {
         let node = p.dst();
         let pp = self
             .pending_packets
@@ -65,11 +66,11 @@ impl GroupCommitQueueSet {
         }
 
         pp.1.push(p);
-        if pp.0.elapsed() >= self.params.flush_timeout {
-            self.flush_internal(node)
+        Ok(if pp.0.elapsed() >= self.params.flush_timeout {
+            self.flush_internal(node)?
         } else {
             None
-        }
+        })
     }
 
     /// Returns how long until a flush should occur.
@@ -86,7 +87,7 @@ impl GroupCommitQueueSet {
             .min()
     }
 
-    fn merge_committed_packets<I>(packets: I) -> Option<Box<Packet>>
+    fn merge_committed_packets<I>(packets: I) -> ReadySetResult<Option<Box<Packet>>>
     where
         I: Iterator<Item = Box<Packet>>,
     {
@@ -94,7 +95,7 @@ impl GroupCommitQueueSet {
         let merged_dst = packets.peek().as_mut().unwrap().dst();
 
         let mut all_senders = vec![];
-        let merged_data = packets.fold(Vec::new(), |mut acc, p| {
+        let merged_data = packets.try_fold(Vec::new(), |mut acc, p| -> ReadySetResult<_> {
             match *p {
                 Packet::Input {
                     inner,
@@ -106,35 +107,35 @@ impl GroupCommitQueueSet {
                     let data: Vec<TableOperation> = p
                         .data
                         .try_into()
-                        .expect("Input packet data was not of Input type.");
-                    assert_eq!(senders.len(), 0);
-                    assert_eq!(merged_dst, p.dst);
+                        .map_err(|_| internal_err("Input packet data was not of Input type."))?;
+                    invariant_eq!(senders.len(), 0);
+                    invariant_eq!(merged_dst, p.dst);
                     acc.extend(data);
 
                     if let Some(src) = src {
                         all_senders.push(src);
                     }
                 }
-                _ => unreachable!(),
+                _ => internal!(),
             }
-            acc
-        });
+            Ok(acc)
+        })?;
 
-        Some(Box::new(Packet::Input {
+        Ok(Some(Box::new(Packet::Input {
             inner: LocalOrNot::new(PacketData {
                 dst: merged_dst,
                 data: PacketPayload::Input(merged_data),
             }),
             src: None,
             senders: all_senders,
-        }))
+        })))
     }
 
     /// Merge the contents of packets into a single packet, emptying packets in the process.
     #[allow(clippy::vec_box)]
-    fn merge_packets(packets: &mut Vec<Box<Packet>>) -> Option<Box<Packet>> {
+    fn merge_packets(packets: &mut Vec<Box<Packet>>) -> ReadySetResult<Option<Box<Packet>>> {
         if packets.is_empty() {
-            return None;
+            return Ok(None);
         }
 
         Self::merge_committed_packets(packets.drain(..))
