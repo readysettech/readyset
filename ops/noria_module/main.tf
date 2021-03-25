cf3eb8ce211a12737e4a0487d7c5328fa7fb9288
Andres Montalban <andres@readyset.io>
2021-03-25 12:18:25 -0300

    chore: Clean up not used variables
    This is based on tflint output.

--- ops/noria_module/main.tf
+++ ops/noria_module/main.tf
@@ -2,13 +2,7 @@ data "aws_subnet_ids" "subnet_ids" {
   vpc_id = var.vpc_id
 }
 
-data "aws_subnet" "subnets" {
-  for_each = data.aws_subnet_ids.subnet_ids.ids
-  id       = each.value
-}
-
 locals {
   subnet_id           = element(tolist(data.aws_subnet_ids.subnet_ids.ids), 0)
-  availability_zone   = element(values(data.aws_subnet.subnets), 0).availability_zone
   readyset_account_id = "069491470376"
 }
