ec696b206085c8f53fb3ac8328b393c1feff049b
Aaron Staple <aaron.staple@gmail.com>
2021-03-25 15:27:39 -0700

    Add an adapter binary to noria-psql.
    This adapter impersonates a PostgreSQL server and forwards requests(responses)
    to(from) Noria itself. Most of the adapter functionality comes from the
    noria-client-adapter crate.

---
+++ noria-psql/src/main.rs
@@ -0,0 +1,28 @@
+use async_trait::async_trait;
+use noria_client::backend as cl;
+use noria_client_adapter::{ConnectionHandler, NoriaAdapter};
+use noria_psql::backend::Backend;
+use psql_srv::run_backend;
+use tokio::net;
+
+#[derive(Clone, Copy)]
+struct PsqlHandler;
+
+#[async_trait]
+impl ConnectionHandler for PsqlHandler {
+    async fn process_connection(&mut self, stream: net::TcpStream, backend: cl::Backend) {
+        let backend = Backend(backend);
+        run_backend(backend, stream).await;
+    }
+}
+
+fn main() {
+    let mut adapter = NoriaAdapter {
+        name: "noria-psql",
+        version: "0.1.0",
+        description: "PostgreSQL adapter for Noria.",
+        default_address: "127.0.0.1:5432",
+        connection_handler: PsqlHandler,
+    };
+    adapter.run()
+}
