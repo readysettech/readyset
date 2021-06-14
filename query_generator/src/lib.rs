f5e1104583a44928d31d4b4c62671d754e035011
Griffin Smith <root@gws.fyi>
2021-06-14 16:47:40 -0400

    Fix assertion in query_generator doctest
    This was broken a bit ago, but this wasn't running in CI so it was
    missed.

--- query_generator/src/lib.rs
+++ query_generator/src/lib.rs
@@ -34,9 +34,9 @@
 //!     vec![],
 //! ));
 //! let query_str = format!("{}", query.statement);
-//! assert_eq!(query_str, "SELECT table_1.column_3, table_2.column_2 \
+//! assert_eq!(query_str, "SELECT table_1.column_2 AS alias_1, table_2.column_2 AS alias_2 \
 //! FROM table_1 \
-//! INNER JOIN table_2 ON (table_1.column_2 = table_2.column_1) \
+//! INNER JOIN table_2 ON (table_1.column_1 = table_2.column_1) \
 //! WHERE (table_1.column_1 = ?)");
 //! ```
 //!
