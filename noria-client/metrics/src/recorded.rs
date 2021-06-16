d78a0db75dca763d3b0fdab805fae5181ff6d140
Francisco Noriega <fran@readyset.io>
2021-06-16 15:08:24 -0400

    Measure time spent on READ queries in the Noria adapters.
    Added two new histogram metrics:
    1. The time spent just parsing a SQL query in the Noria adapters.
    2. The time spent executing a SQL query in the Noria adapters.

--- noria-client/metrics/src/recorded.rs
+++ noria-client/metrics/src/recorded.rs
@@ -1,5 +1,37 @@
 //! Documents the set of metrics that are currently being recorded within
 //! a noria-client.
+use metrics::SharedString;
+
+/// Histogram: The time in microseconds that the Noria adapter spent
+/// parsing a query.
+///
+/// | Tag | Description |
+/// | --- | ----------- |
+/// | query_type | The type of query that was being parsed. Must be a [`SqlQueryType`] |
+pub const QUERY_PARSING_TIME: &str = "noria-client.parsing_time";
+
+/// Histogram: The time in microseconds that the Noria adapter spent
+/// executing a query.
+///
+/// | Tag | Description |
+/// | --- | ----------- |
+/// | query_type | The type of query that was being executed. Must be a [`SqlQueryType`] |
+pub const QUERY_EXECUTION_TIME: &str = "noria-client.execution_time";
+
+/// The type of a SQL query.
+pub enum SqlQueryType {
+    /// Read query.
+    Read,
+}
+
+// Implementing this so it can be used directly as a metric label.
+impl From<SqlQueryType> for SharedString {
+    fn from(query_type: SqlQueryType) -> Self {
+        match query_type {
+            SqlQueryType::Read => SharedString::const_str("read"),
+        }
+    }
+}
 
 /// Identifies the database that is being adapted to
 /// communicate with Noria.
