ac43350570f8c3293a6ac572744dfb43c60e73f0
Griffin Smith <root@gws.fyi>
2021-06-21 09:49:11 -0400

    Don't re-validate packer images when building
    When we unblock the build packer images step on a pipeline that's
    already run the validate-packer-images step, previously the script would
    re-create all the validate steps, causing an error in buildkite due to
    the same step existing multiple times. There's no real need to
    re-validate in the second pipeline, so let's just skip the steps there
    entirely

--- .buildkite/build-packer-images.sh
+++ .buildkite/build-packer-images.sh
@@ -9,32 +9,33 @@ RELEASE_VERSION=$(.buildkite/latest-release-version.sh)
 
 echo "steps:"
 
-# A step to validate each Packer image
-find ${IMAGES_DIR}/* -type f -name '*.pkr.hcl' | while read -r image; do
-  PACKER_IMAGE=$(basename $(dirname $image))
-  echo "  - label: ':packer: Validate Packer image ($PACKER_IMAGE)'"
-  echo "    key: validate-packer-image-${PACKER_IMAGE}"
-  echo "    depends_on:"
-  echo "      - packer-lint"
-  echo "    branches: \"!master !main\""
-  echo "    command:"
-  echo "      - mkdir binaries && ls -lah && pwd"
-  echo "      - export BUILDKITE_BUILD_ID=\"$RELEASE_VERSION\""
-  echo "      - buildkite-agent artifact download target/release/* binaries/"
-  echo "      - packer validate ."
-  echo "    plugins:"
-  echo "      - docker#v3.7.0:"
-  echo "          image: 'hashicorp/packer:latest'"
-  echo "          mount-checkout: false"
-  echo "          entrypoint: \"\""
-  echo "          shell: [\"/bin/bash\", \"-e\", \"-c\"]"
-  echo "          workdir: /workspace"
-  echo "          volumes:"
-  echo "            - './${IMAGES_DIR}/${PACKER_IMAGE}:/workspace'"
-  echo
-done
-
-if [ -n "$ONLY_VALIDATE" ]; then exit 0; fi
+if [ -n "${ONLY_VALIDATE+x}" ]; then
+  # A step to validate each Packer image
+  find ${IMAGES_DIR}/* -type f -name '*.pkr.hcl' | while read -r image; do
+    PACKER_IMAGE=$(basename $(dirname $image))
+    echo "  - label: ':packer: Validate Packer image ($PACKER_IMAGE)'"
+    echo "    key: validate-packer-image-${PACKER_IMAGE}"
+    echo "    depends_on:"
+    echo "      - packer-lint"
+    echo "    branches: \"!master !main\""
+    echo "    command:"
+    echo "      - mkdir binaries && ls -lah && pwd"
+    echo "      - export BUILDKITE_BUILD_ID=\"$RELEASE_VERSION\""
+    echo "      - buildkite-agent artifact download target/release/* binaries/"
+    echo "      - packer validate ."
+    echo "    plugins:"
+    echo "      - docker#v3.7.0:"
+    echo "          image: 'hashicorp/packer:latest'"
+    echo "          mount-checkout: false"
+    echo "          entrypoint: \"\""
+    echo "          shell: [\"/bin/bash\", \"-e\", \"-c\"]"
+    echo "          workdir: /workspace"
+    echo "          volumes:"
+    echo "            - './${IMAGES_DIR}/${PACKER_IMAGE}:/workspace'"
+    echo
+  done
+  exit 0
+fi
 
 # A step to do a dry run build of each Packer image
 find ${IMAGES_DIR}/* -type f -name '*.pkr.hcl' | while read -r image; do
