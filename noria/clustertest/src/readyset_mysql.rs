66e6dd76b26769216aaa2ac9d1276cd0ebc0329d
Dan Wilbanks <dan@readyset.io>
2021-06-23 18:14:11 -0400

    Created clustertests to demonstrate database fallback
    The clustertest framework can deploy a mysql server to host a
    persistent data store. These two clustertests demonstrate that queries
    are rerouted to the mysql server when execution by noria fails.
    
    Future Work:
    The two tests only demonstrate fallback when query parsing fails.
    Additional tests should be implemented to demonstrate fallback when
    queries fail after going to the noria_connector.

--- noria/clustertest/src/readyset_mysql.rs
+++ noria/clustertest/src/readyset_mysql.rs
@@ -1,5 +1,6 @@
 use crate::*;
 use mysql::prelude::Queryable;
+use mysql::Value;
 use serial_test::serial;
 
 #[tokio::test(flavor = "multi_thread")]
@@ -40,8 +41,89 @@ async fn create_table_insert_test() {
 
 #[tokio::test(flavor = "multi_thread")]
 #[serial]
-async fn show_tables_test() {}
+async fn show_tables_test() {
+    let cluster_name = "ct_show_tables";
+    let mut deployment = DeploymentParams::new(
+        cluster_name,
+        NoriaBinarySource::Build(BuildParams {
+            root_project_path: get_project_root(),
+            target_dir: get_project_root().join("test_target"),
+            release: true,
+            rebuild: false,
+        }),
+    );
+    deployment.add_server(ServerParams::default());
+    deployment.add_server(ServerParams::default());
+    deployment.deploy_mysql();
+
+    let mut deployment = start_multi_process(deployment).await.unwrap();
+    let opts = mysql::Opts::from_url(&deployment.mysql_connection_str().unwrap()).unwrap();
+    let mut conn = mysql::Conn::new(opts.clone()).unwrap();
+    let _ = conn
+        .query_drop(r"CREATE TABLE t2a (uid INT NOT NULL, value INT NOT NULL,);")
+        .unwrap();
+    let _ = conn
+        .query_drop(r"CREATE TABLE t2b (uid INT NOT NULL, value INT NOT NULL,);")
+        .unwrap();
+
+    let tables: Vec<String> = conn.query("SHOW TABLES;").unwrap();
+    deployment.teardown().await.unwrap();
+    assert_eq!(tables, vec!["t2a", "t2b"]);
+}
 
 #[tokio::test(flavor = "multi_thread")]
 #[serial]
-async fn describe_table_test() {}
+async fn describe_table_test() {
+    let cluster_name = "ct_describe_table";
+    let mut deployment = DeploymentParams::new(
+        cluster_name,
+        NoriaBinarySource::Build(BuildParams {
+            root_project_path: get_project_root(),
+            target_dir: get_project_root().join("test_target"),
+            release: true,
+            rebuild: false,
+        }),
+    );
+    deployment.add_server(ServerParams::default());
+    deployment.add_server(ServerParams::default());
+    deployment.deploy_mysql();
+
+    let mut deployment = start_multi_process(deployment).await.unwrap();
+    let opts = mysql::Opts::from_url(&deployment.mysql_connection_str().unwrap()).unwrap();
+    let mut conn = mysql::Conn::new(opts.clone()).unwrap();
+    let _ = conn
+        .query_drop(r"CREATE TABLE t3 (uid INT NOT NULL, value INT NOT NULL,);")
+        .unwrap();
+
+    let table: Vec<mysql::Row> = conn.query("DESCRIBE t3;").unwrap();
+    let descriptor = table.get(0).unwrap();
+    let cols = descriptor.columns_ref();
+    let cols = cols
+        .into_iter()
+        .map(|c| c.name_ref())
+        .into_iter()
+        .collect::<Vec<_>>();
+    let vals: Vec<Value> = descriptor.clone().unwrap();
+
+    let cols_truth = vec![
+        "Field".as_bytes(),
+        "Type".as_bytes(),
+        "Null".as_bytes(),
+        "Key".as_bytes(),
+        "Default".as_bytes(),
+        "Extra".as_bytes(),
+    ];
+    let vals_truth = vec![
+        Value::Bytes("uid".as_bytes().iter().cloned().collect()),
+        Value::Bytes("int".as_bytes().iter().cloned().collect()),
+        Value::Bytes("NO".as_bytes().iter().cloned().collect()),
+        Value::Bytes("".as_bytes().iter().cloned().collect()),
+        Value::NULL,
+        Value::Bytes("".as_bytes().iter().cloned().collect()),
+    ];
+
+    deployment.teardown().await.unwrap();
+
+    assert_eq!(vals, vals_truth);
+    assert_eq!(cols, cols_truth);
+}
