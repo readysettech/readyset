826fae6f8ce4110e017a19ddabfd1c15d736fa97
Justin Miron <justinmiron11@gmail.com>
2021-06-28 16:35:02 +0000

    Multi-thread fastly demo read client.
    A new parameter `threads` that allows specifying how many
    threads to use to generate queries from. The `target_qps`
    is split between the specified threads. This will allow
    each reader client to generate higher load and require less
    clients to run our load experiments.

--- demos/fastly/src/bin/reader.rs
+++ demos/fastly/src/bin/reader.rs
@@ -25,6 +25,10 @@ struct Reader {
     /// The target rate at the reader issues queries at.
     #[clap(long, default_value = "10")]
     target_qps: u64,
+
+    /// The number of threads to spawn to issue reader queries.
+    #[clap(long, default_value = "1")]
+    threads: u64,
 }
 
 impl Reader {
@@ -34,33 +38,62 @@ impl Reader {
         uniform.sample(&mut rng)
     }
 
-    pub async fn run(&self) -> anyhow::Result<()> {
-        let fastly_query_file = self.query.clone().unwrap_or_else(|| {
-            PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap() + "/fastly_read_query.sql")
-        });
-        let param_query = fs::read_to_string(fastly_query_file)?;
-        let query_issue_interval = Duration::from_millis(1000 / self.target_qps);
+    async fn generate_queries(
+        &self,
+        query: String,
+        query_interval: Duration,
+    ) -> anyhow::Result<()> {
+        let generate_queries_start = Instant::now();
 
-        // Currently each query is issued on the same thread. If a query execute exceeds
-        // `1/target_qps` seconds queries will be issued slower than the target qps.
-        let mut next_query = Instant::now() + query_issue_interval;
+        let mut next_query = Instant::now() + query_interval;
         loop {
+            // Currently each query is issued on the same thread. If a query execute exceeds
+            // `1/target_qps` seconds queries will be issued slower than the target qps.
             let now = Instant::now();
             if now < next_query {
                 sleep(Duration::from_millis(10));
                 continue;
             }
 
-            next_query = now + query_issue_interval;
+            next_query = now + query_interval;
             let id = self.generate_user_id();
             let mut conn = self.database_url.connect().await?;
-            let _ = conn.execute(param_query.clone(), vec![id as u64]).await?;
+            let query_start = Instant::now();
+            let _ = conn.execute(query.clone(), vec![id as u64]).await?;
+            let query_elapsed = Instant::now() - query_start;
+
+            println!(
+                "{}: {}",
+                (query_start - generate_queries_start).as_secs_f32(),
+                query_elapsed.as_millis()
+            );
         }
+    }
+
+    pub async fn run(&'static self) -> anyhow::Result<()> {
+        println!(
+            "Starting reader with threads {}, target_qps {}",
+            self.threads, self.target_qps
+        );
+
+        let fastly_query_file = self.query.clone().unwrap_or_else(|| {
+            PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap() + "/fastly_read_query.sql")
+        });
+        let param_query = fs::read_to_string(fastly_query_file)?;
+        let query_issue_interval = Duration::from_millis(1000 / self.target_qps / self.threads);
+        let threads = (0..self.threads).map(|_| {
+            let query = param_query.clone();
+            tokio::spawn(async move { self.generate_queries(query, query_issue_interval).await })
+        });
+        futures::future::join_all(threads).await;
+
+        Ok(())
     }
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let reader = Reader::parse();
+    // Reader is set to static as it is referenced by multiple threads.
+    let reader: &'static _ = Box::leak(Box::new(Reader::parse()));
     reader.run().await
 }
