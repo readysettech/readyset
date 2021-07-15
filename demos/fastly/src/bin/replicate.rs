9941532569f1b11173a74543e820a589b29773c6
Justin Miron <justinmiron11@gmail.com>
2021-07-15 16:08:07 +0000

    Demo client to replicate reader's on to other servers.
    Simple client that calls `replicate_reader` to replicate
    specified queries on to other servers. Takes in a vector
    of queries and a vector of server to replicate on to.

---
+++ demos/fastly/src/bin/replicate.rs
@@ -0,0 +1,46 @@
+use clap::Clap;
+use noria::consensus::ZookeeperAuthority;
+use noria::ControllerHandle;
+use std::sync::Arc;
+use url::Url;
+
+#[derive(Clap)]
+#[clap(name = "replicate")]
+struct Replicate {
+    /// ReadySet's zookeeper connection string.
+    #[clap(long)]
+    zookeeper_url: String,
+
+    /// The set of queries to replicate on to the readers.
+    #[clap(long)]
+    queries: Vec<String>,
+
+    /// The reader's to replicate on to.
+    #[clap(long)]
+    reader_addrs: Vec<String>,
+}
+
+impl Replicate {
+    pub async fn run(&'static self) -> anyhow::Result<()> {
+        let authority = Arc::new(ZookeeperAuthority::new(&self.zookeeper_url)?);
+
+        let mut handle: ControllerHandle<ZookeeperAuthority> =
+            ControllerHandle::new(Arc::clone(&authority)).await?;
+        handle.ready().await.unwrap();
+
+        for r in &self.reader_addrs {
+            handle
+                .replicate_readers(self.queries.clone(), Some(Url::parse(r).unwrap()))
+                .await
+                .unwrap();
+        }
+
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let replicate: &'static _ = Box::leak(Box::new(Replicate::parse()));
+    replicate.run().await
+}
