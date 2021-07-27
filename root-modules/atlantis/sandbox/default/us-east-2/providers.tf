c6f7ba5556bff3aff71bd9443c8411649a4dc6ec
harleyk-readyset <87341184+harleyk-readyset@users.noreply.github.com>
2021-07-27 14:22:41 -0700

    Atlantis account (#4)

---
+++ root-modules/atlantis/sandbox/default/us-east-2/providers.tf
@@ -0,0 +1,18 @@
+# managed by Substrate; do not edit by hand
+
+provider "aws" {
+  assume_role {
+    role_arn     = "arn:aws:iam::305232526136:role/Administrator"
+    session_name = "Terraform"
+  }
+  region = "us-east-2"
+}
+
+provider "aws" {
+  alias = "network"
+  assume_role {
+    role_arn     = "arn:aws:iam::911245771907:role/NetworkAdministrator"
+    session_name = "Terraform"
+  }
+  region = "us-east-2"
+}
