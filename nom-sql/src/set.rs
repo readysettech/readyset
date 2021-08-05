1d9455a12f5240fb41301f487e17655baf3e5c7f
Dan Wilbanks <dan@readyset.io>
2021-08-05 14:32:24 -0400

    Error on disallowed set statements
    The mysql adapter now errors on any set statements that are not
    explicitly allowed. This change adds "set {GLOBAL | LOCAL | SESSION}
    time_zone = '+00:00'" as the only explicitly allowed set statement.
    
    nom_sql/src/set.rs is updated to parse variable scope qualifiers. The
    scope is attached to the variable name using the format @@SCOPE.var.
    
    Change-Id: If9318de35f4a9415f998aae66462b617e64a2ee1
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/154
    Reviewed-by: Griffin Smith <griffin@readyset.io>
    Tested-by: Buildkite CI

--- nom-sql/src/set.rs
+++ nom-sql/src/set.rs
@@ -1,9 +1,11 @@
-use nom::bytes::complete::tag_no_case;
+use nom::bytes::complete::{tag, tag_no_case};
 use nom::character::complete::{multispace0, multispace1};
 use std::{fmt, str};
 
 use crate::common::{literal, statement_terminator, Literal};
 use crate::Dialect;
+use nom::branch::alt;
+use nom::combinator::{map, map_res, opt};
 use nom::sequence::tuple;
 use nom::IResult;
 
@@ -21,11 +23,53 @@ impl fmt::Display for SetStatement {
     }
 }
 
+/// check for one of three ways to specify scope and reformat to a single formatting. Returns none
+/// if scope is not specified
+pub fn scope(dialect: Dialect) -> impl Fn(&[u8]) -> IResult<&[u8], Option<String>> {
+    move |i| {
+        // ignore scope parsing if dialect is not MySQL
+        if !matches!(dialect, Dialect::MySQL) {
+            return Ok((i, None));
+        }
+        let (i, scope) = opt(alt((
+            map(
+                map_res(
+                    alt((
+                        tag_no_case("session"),
+                        tag_no_case("local"),
+                        tag_no_case("global"),
+                    )),
+                    str::from_utf8,
+                ),
+                |s: &str| format!("@@{}.", s.to_uppercase()),
+            ),
+            map(
+                map_res(
+                    alt((
+                        tag_no_case("@@session"),
+                        tag_no_case("@@local"),
+                        tag_no_case("@@global"),
+                    )),
+                    str::from_utf8,
+                ),
+                |s: &str| format!("{}.", s.to_uppercase()),
+            ),
+            // check for @@ last
+            map(tag("@@"), |_| "@@SESSION.".into()),
+        )))(i)?;
+
+        //scope may be followed by '.' or multispace
+        let (i, _) = alt((tag("."), multispace0))(i)?;
+        Ok((i, scope))
+    }
+}
+
 pub fn set(dialect: Dialect) -> impl Fn(&[u8]) -> IResult<&[u8], SetStatement> {
     move |i| {
-        let (remaining_input, (_, _, var, _, _, _, value, _)) = tuple((
+        let (remaining_input, (_, _, scope, var, _, _, _, value, _)) = tuple((
             tag_no_case("set"),
             multispace1,
+            scope(dialect),
             dialect.identifier(),
             multispace0,
             tag_no_case("="),
@@ -33,7 +77,13 @@ pub fn set(dialect: Dialect) -> impl Fn(&[u8]) -> IResult<&[u8], SetStatement> {
             literal(dialect),
             statement_terminator,
         ))(i)?;
-        let variable = String::from(var);
+
+        let variable = if let Some(s) = scope {
+            format!("{}{}", s, var)
+        } else {
+            var.into()
+        };
+
         Ok((remaining_input, SetStatement { variable, value }))
     }
 }
@@ -74,5 +124,40 @@ mod tests {
         let expected = "SET autocommit = 1";
         let res = set(Dialect::MySQL)(qstring.as_bytes());
         assert_eq!(format!("{}", res.unwrap().1), expected);
+    }
+
+    #[test]
+    fn global_set() {
+        let qstring1 = "set gloBal var = 2";
+        let qstring2 = "set @@gLobal.var = 2";
+        let expected = "SET @@GLOBAL.var = 2";
+        let res1 = set(Dialect::MySQL)(qstring1.as_bytes());
+        let res2 = set(Dialect::MySQL)(qstring2.as_bytes());
+        assert_eq!(format!("{}", res1.unwrap().1), expected);
+        assert_eq!(format!("{}", res2.unwrap().1), expected);
+    }
+
+    #[test]
+    fn session_set() {
+        let qstring1 = "set @@Session.var = 1";
+        let qstring2 = "set @@var = 1";
+        let qstring3 = "set SeSsion var = 1";
+        let expected = "SET @@SESSION.var = 1";
+        let res1 = set(Dialect::MySQL)(qstring1.as_bytes());
+        let res2 = set(Dialect::MySQL)(qstring2.as_bytes());
+        let res3 = set(Dialect::MySQL)(qstring3.as_bytes());
+        assert_eq!(format!("{}", res1.unwrap().1), expected);
+        assert_eq!(format!("{}", res2.unwrap().1), expected);
+        assert_eq!(format!("{}", res3.unwrap().1), expected);
+    }
+    #[test]
+    fn local_set() {
+        let qstring1 = "set lOcal var = 2";
+        let qstring2 = "set @@local.var = 2";
+        let expected = "SET @@LOCAL.var = 2";
+        let res1 = set(Dialect::MySQL)(qstring1.as_bytes());
+        let res2 = set(Dialect::MySQL)(qstring2.as_bytes());
+        assert_eq!(format!("{}", res1.unwrap().1), expected);
+        assert_eq!(format!("{}", res2.unwrap().1), expected);
     }
 }
