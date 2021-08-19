e271b75656025bd9e0669da31bda615f25196e62
Griffin Smith <griffin@readyset.io>
2021-08-19 13:44:16 -0400

    Update deploy artifacts bucket name
    Update the bucket name that buildkite uses to store the build ID of the
    latest successful release build for the new bucket, and also change the
    name of the environment variable to indicate that this isn't the bucket
    we're using to store deploy artifacts, just metadata about the pipeline
    
    Change-Id: Ie889ea73108fb55045a0adaa9c5d19a9f94eab60
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/186
    Reviewed-by: Fran Noriega <fran@readyset.io>
    Tested-by: Buildkite CI

--- .buildkite/latest-release-version.sh
+++ .buildkite/latest-release-version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-aws s3 cp "s3://$DEPLOY_ARTIFACTS_BUCKET/latest-release-build" /tmp/latest-release-build >&2
+aws s3 cp "s3://$PIPELINE_METADATA_BUCKET/latest-release-build" /tmp/latest-release-build >&2
 cat /tmp/latest-release-build
--- .buildkite/set-release-version.sh
+++ .buildkite/set-release-version.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 echo -n "$BUILDKITE_BUILD_ID" > /tmp/latest-release-build
-aws s3 cp /tmp/latest-release-build "s3://$DEPLOY_ARTIFACTS_BUCKET/latest-release-build"
+aws s3 cp /tmp/latest-release-build "s3://$PIPELINE_METADATA_BUCKET/latest-release-build"
