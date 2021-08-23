26e87faf5cf32b73e60fe88ce45583c08b08241c
Dan Wilbanks <dan@readyset.io>
2021-08-23 15:23:01 -0400

    Remove pool from mysql_connector
    The connection pool is removed from mysql_connector and replaced with a
    single mysql_async connection to the mysql server. This is done because
    the adapter <-> server connection contains state (e.g., a table of
    prepared statements). A single client <-> adapter connection should
    maintain visibility of this state throughout its lifetime. This is not
    guaranteed with a connection pool.
    
    race_reads() now creates its own connection on each call instead of
    grabbing one from the pool used by the existing reader mysql_connection.
    
    Change-Id: I120fabac6b73e97bce042acddcedd2cb0c8a4fdb
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/199
    Tested-by: Buildkite CI
    Reviewed-by: Griffin Smith <griffin@readyset.io>

--- noria-client/src/backend/mysql_connector.rs
+++ noria-client/src/backend/mysql_connector.rs
@@ -10,24 +10,33 @@ use std::collections::HashMap;
 type StatementID = u32;
 
 /// A connector to an underlying mysql store. This is really just a wrapper for the mysql crate.
-#[derive(Clone)]
 pub struct MySqlConnector {
-    pool: Pool,
+    conn: Conn,
     prepared_statements: HashMap<StatementID, String>,
+    url: String,
 }
 
 impl MySqlConnector {
     pub async fn new(url: String) -> Self {
         // CLIENT_SESSION_TRACK is required for GTID information to be sent in OK packets on commits
         // GTID information is used for RYW
-        let pool = Pool::new(
-            OptsBuilder::from_opts(url).add_capability(CapabilityFlags::CLIENT_SESSION_TRACK),
-        );
+        let conn = Conn::new(
+            OptsBuilder::from_opts(url.clone())
+                .add_capability(CapabilityFlags::CLIENT_SESSION_TRACK),
+        )
+        .await
+        .unwrap();
         let prepared_statements = HashMap::new();
         MySqlConnector {
-            pool,
+            conn,
             prepared_statements,
+            url,
         }
+    }
+
+    /// Returns the url used to establish the connection
+    pub fn url(&self) -> &str {
+        &self.url
     }
 
     /// Prepares the given query using the mysql connection. Note, queries are prepared on a
@@ -37,12 +46,11 @@ impl MySqlConnector {
         query: &str,
         statement_id: u32,
     ) -> std::result::Result<u32, Error> {
-        let mut conn = self.pool.get_conn().await?;
         // FIXME: Actually use the prepared statement.
         // This isn't straightforward, because the statements take `self` by value when you try
         // and do anything with them.
         // So, for now, we just prepare it to check that it's valid and drop the result.
-        let _ = conn.prep(query).await?;
+        self.conn.prep(query).await?;
         self.prepared_statements
             .insert(statement_id, query.to_owned());
         debug!("Successfully prepared statement : {}", query);
@@ -58,18 +66,19 @@ impl MySqlConnector {
         id: u32,
         _params: Vec<DataType>,
     ) -> std::result::Result<(u64, u64), Error> {
-        let mut conn = self.pool.get_conn().await?;
         let stmt = self.prepared_statements.get(&id).unwrap().as_str();
         // todo : these params are incorrect. Clubhouse story here : https://app.clubhouse.io/readysettech/story/211/support-prepared-writes-and-executes-to-mysql
         //let mysql_params : Vec<Value> = params.into_iter().map(|p| {p.value}).collect();
-        conn.exec_drop(stmt, Params::Empty).await?;
-        Ok((conn.affected_rows(), conn.last_insert_id().unwrap_or(0)))
+        self.conn.exec_drop(stmt, Params::Empty).await?;
+        Ok((
+            self.conn.affected_rows(),
+            self.conn.last_insert_id().unwrap_or(0),
+        ))
     }
 
     pub async fn handle_select(&mut self, query: &str) -> std::result::Result<QueryResult, Error> {
         let q = query.to_string();
-        let mut conn = self.pool.get_conn().await?;
-        let rows = conn.query(&q).await?;
+        let rows = self.conn.query(&q).await?;
         Ok(QueryResult::MySqlSelect { data: rows })
     }
 
@@ -81,9 +90,8 @@ impl MySqlConnector {
         create_identifier: bool,
     ) -> std::result::Result<(QueryResult, Option<String>), Error> {
         let q = query.to_string();
-        let mut conn = self.pool.get_conn().await?;
         if create_identifier {
-            let mut transaction = conn.start_transaction(TxOpts::default()).await?;
+            let mut transaction = self.conn.start_transaction(TxOpts::default()).await?;
             transaction.query_drop(&q).await.map_err(|e| {
                 error!("Could not execute query in mysql : {:?}", e);
                 e
@@ -107,20 +115,20 @@ impl MySqlConnector {
                 Some(txid),
             ))
         } else {
-            conn.query_drop(&q).await.map_err(|e| {
+            self.conn.query_drop(&q).await.map_err(|e| {
                 error!("Could not execute query in mysql : {:?}", e);
                 e
             })?;
             debug!("Successfully executed query : {}", &q);
             debug!(
                 "results : {:?}, {:?}",
-                conn.affected_rows(),
-                conn.last_insert_id()
+                self.conn.affected_rows(),
+                self.conn.last_insert_id()
             );
             Ok((
                 QueryResult::MySqlWrite {
-                    num_rows_affected: conn.affected_rows(),
-                    last_inserted_id: conn.last_insert_id().unwrap_or(0),
+                    num_rows_affected: self.conn.affected_rows(),
+                    last_inserted_id: self.conn.last_insert_id().unwrap_or(0),
                 },
                 None,
             ))
