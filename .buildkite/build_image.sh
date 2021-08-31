0e8e9ac013a774958439c9bb08ab356c07bd9162
Griffin Smith <griffin@readyset.io>
2021-08-31 11:24:54 -0400

    Generalize docker image building into a script
    As we're making more and more changes to the way docker images are built
    in CI, the number of places we have to make those changes has started to
    become a little prohibitive - especially if we start to think about
    optimizing caching for multi-layer builds etc. This commit generalizes
    the process of building and pushing a docker image to our ECR repository
    into a new build_image.sh script, and updates all places in CI config
    where docker images are built to use that script.
    
    Change-Id: Ib5ce98a0b60b83b667031212a3368e5865b9200d
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/251
    Tested-by: Buildkite CI
    Reviewed-by: Harley Klein <harley@readyset.io>

---
+++ .buildkite/build_image.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+dockerfile="$1"
+image_name="$2"
+
+if [ -z "$dockerfile" ] || [ -z "$image_name" ]; then
+    echo "Usage: $0 <dockerfile> <image_name>" >&2
+    exit 1
+fi
+
+AWS_ACCOUNT=${AWS_ACCOUNT:-305232526136} # build
+AWS_REGION=${AWS_REGION:-us-east-2}
+VERSION=${VERSION:-$BUILDKITE_COMMIT}
+
+docker_repo="$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com"
+image="$docker_repo/$image_name"
+
+echo "--- :docker: Pulling $image:latest"
+docker pull "$image:latest" || echo "Failed to pull previous build of image"
+
+echo "+++ :docker: Building $image:$VERSION"
+docker build \
+    -f "$dockerfile" \
+    --cache-from "$image:latest" \
+    -t "$image:$VERSION" \
+    .
+
+docker tag "$image:$VERSION" "$image:latest"
+
+echo "--- :docker: Pushing $image"
+docker push --all-tags "$image"
