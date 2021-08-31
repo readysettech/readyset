98e916037eba49a5f04f81d0c814402a443f95f0
Griffin Smith <griffin@readyset.io>
2021-08-31 13:29:15 -0400

    Only build release images on main branch
    We need to build docker images in CL builds so that we can run things
    like the adapter deployment test step, but going through the extra time
    to do a full release-stage build is unnecessary unless we're also going
    to be deploying the image to production, which we only do on builds of
    the main branch. This commit introduces a new `release` Docker build arg
    to the three docker images we're deploying which if set will enable a
    release build (but if unset we just default to a debug build), and
    updates the buildkite pipeline to only pass this on builds of the main
    branch
    
    Change-Id: I46bfffb4bdf21a242ae9d01a11630b145feb6baa
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/254
    Tested-by: Buildkite CI
    Reviewed-by: Harley Klein <harley@readyset.io>

--- .buildkite/build_image.sh
+++ .buildkite/build_image.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -xeuo pipefail
 
 dockerfile="$1"
 image_name="$2"
 
 if [ -z "$dockerfile" ] || [ -z "$image_name" ]; then
-    echo "Usage: $0 <dockerfile> <image_name>" >&2
+    echo "Usage: $0 <dockerfile> <image_name> [<docker-build-args>...]" >&2
     exit 1
 fi
+shift 2
 
 AWS_ACCOUNT=${AWS_ACCOUNT:-305232526136} # build
 AWS_REGION=${AWS_REGION:-us-east-2}
@@ -24,6 +25,7 @@ docker build \
     -f "$dockerfile" \
     --cache-from "$image:latest" \
     -t "$image:$VERSION" \
+    "$@" \
     .
 
 if [ "$BUILDKITE_BRANCH" = "refs/heads/main" ]; then
