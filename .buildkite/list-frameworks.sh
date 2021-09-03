5003ddf9576cbe59ce255b0413e3496402e5bc98
Mike Cronce <mc@readyset.io>
2021-09-03 04:33:17 -1000

    .buildkite/list-frameworks.sh:  Remove $BUILDKITE_BRANCH check
    There isn't really any good reason to check the branch.  If $NIGHTLY is
    explicitly set, run the nightly pipeline.
    
    Also, it doesn't work because $BUILDKITE_BRANCH isn't being set :(
    
    Change-Id: I310235a6141b4b7d7b1d3aecf1e5df5dccb29157
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/305
    Reviewed-by: Griffin Smith <griffin@readyset.io>
    Tested-by: Buildkite CI

--- .buildkite/list-frameworks.sh
+++ .buildkite/list-frameworks.sh
@@ -5,7 +5,7 @@ pip3 install pyyaml
 export OUTPUT=plain
 ./readyset-framework-testing/tools/get-frameworks | buildkite-agent meta-data set 'frameworks'
 changed_frameworks="$("${dir}/get-changed-files.sh" | FILTER_FROM_STDIN=1 ./readyset-framework-testing/tools/get-frameworks)"
-if [ "${BUILDKITE_BRANCH}" == 'main' ] && [ "${NIGHTLY}" == 'true' ]; then
+if [ "${NIGHTLY}" == 'true' ]; then
   # In the case of main on nightly, we just test every framework
   ./readyset-framework-testing/tools/get-frameworks | buildkite-agent meta-data set 'changed-frameworks'
 elif [ "${changed_frameworks}" != '' ]; then
