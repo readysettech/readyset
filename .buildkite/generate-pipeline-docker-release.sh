0d25bc422369d48660aeae602534d415773e6fbc
Harley Klein <harley@readyset.io>
2021-09-10 12:52:26 -0700

    Upload Docker Release to s3
    Add the additional steps needed to upload each Docker release into S3
    
    Change-Id: Id9f05c3f8998b1d198a74dc1e8a8aa9f2a155994
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/383
    Reviewed-by: Griffin Smith <griffin@readyset.io>
    Tested-by: Buildkite CI

--- .buildkite/generate-pipeline-docker-release.sh
+++ .buildkite/generate-pipeline-docker-release.sh
@@ -11,11 +11,17 @@ images=(
   readyset-psql
 )
 
+# The step uploading to S3 adds in a to the key to avoid rewriting a release by
+# accident. Commands to deliver to others will find the most recent version
+# by default.
+# TODO: Make bucket for releases actually immutable in some way.
+
 echo "steps:"
 
 for image in "${images[@]}"; do
 cat << EOF
   - name: ":docker: Create Docker archive for $image"
+    key: docker-archive-$image
     commands:
       - docker pull ${ecr_repository_namespace}/${image}:release-${BUILDKITE_COMMIT}
       - docker tag ${ecr_repository_namespace}/${image}:release-${BUILDKITE_COMMIT} ${image}:${release_name}
@@ -24,7 +30,20 @@ cat << EOF
     plugins:
       ecr#v2.2.0:
         login: true
+
+  - name: ":amazon-s3: Upload Docker archive for $image to S3"
+    depends_on:
+      - docker-archive-$image
+    key: s3-upload-$image
+    commands:
+      - buildkite-agent artifact download ${image}-${release_name}.tar.gz
+      - >-
+        aws s3 cp ${image}-${release-name}.tar.gz
+        s3://readysettech-customer-artifacts-us-east-2/docker-release-${release-name}/$(date --rfc-3339=date --utc)/${image}.tar.gz
+    plugins:
+    env:
+      AWS_ASSUME_ROLE_ARN: arn:aws:iam::305232526136:role/DeployCustomerArtifactsWrite
+    plugins:
+      - cultureamp/aws-assume-role#v0.2.0
 EOF
 done
-
-# TODO: Create another step which uploads these artifacts to an S3 bucket we can share.
