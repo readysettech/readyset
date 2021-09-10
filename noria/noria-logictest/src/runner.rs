readysettech/readyset

commit df9fc69adb9cc2fe975b27ef779afa7def6360ea

Abort the process if the important tasks fail
If any of the tokio tasks running the HTTP listener, the controller, or
the worker fails for any reason, whether it's a panic or a regular error
return, we don't want to get in an awkward state where the noria-server
process is still running, but it's not *functional* in any way because
these critical tasks aren't running. In this case, the best thing to do
is to abort the process entirely and let the general orchestration (read:
systemd) restart us automatically.

To this end, there's a new `abort_on_panic` helper function in
launchpad/src/futures which wraps a future in an adapter that causes it
to call `process::abort()` if it panics. We don't want *all* panics to
abort the process, so we're not using panic-as-abort or some kind of
custom panic handler, but this gives us a cheap opt-in on a per-future
basis for that behavior.

Change-Id: Iaa7d586636e386ef4ec48a248bd29a9b15a09bf3
Reviewed-on: https://gerrit.readyset.name/c/readyset/+/144
Tested-by: Buildkite CI
Reviewed-by: Vlad Krasnov <vlad@readyset.io>
Reviewed-by: Justin Miron <justin@readyset.io>

noria/server/src/startup.rs
@@ -53,7 +53,6 @@
 //!
 //! (TODO: write this section)
 
-use std::io;
 use std::net::{IpAddr, SocketAddr};
 use std::sync::{Arc, Mutex};
 use std::time;
@@ -63,10 +62,12 @@ use std::{
     pin::Pin,
     task::{Context, Poll},
 };
+use std::{io, process};
 
 use futures_util::future::TryFutureExt;
 use hyper::{self, header::CONTENT_TYPE, Method, StatusCode};
 use hyper::{service::make_service_fn, Body, Request, Response};
+use launchpad::futures::abort_on_panic;
 use stream_cancel::Valve;
 use tokio::net::TcpListener;
 use tokio::sync::mpsc::Sender;
@@ -134,24 +135,28 @@ pub(super) async fn start_instance<A: Authority + 'static>(
         authority: authority.clone(),
     };
 
-    tokio::spawn(crate::worker::readers::listen(
+    tokio::spawn(abort_on_panic(crate::worker::readers::listen(
         valve.clone(),
         readers_listener,
         readers.clone(),
-    ));
+    )));
 
-    tokio::spawn(
-        hyper::server::Server::builder(hyper::server::accept::from_stream(
-            valve.wrap(TcpListenerStream::new(http_listener)),
-        ))
-        .serve(make_service_fn(move |_| {
-            let s = http_server.clone();
-            async move { io::Result::Ok(s) }
-        }))
-        .map_err(|e| {
-            panic!("HTTP server failed: {}", e);
-        }),
-    );
+    {
+        let log = log.clone();
+        tokio::spawn(abort_on_panic(
+            hyper::server::Server::builder(hyper::server::accept::from_stream(
+                valve.wrap(TcpListenerStream::new(http_listener)),
+            ))
+            .serve(make_service_fn(move |_| {
+                let s = http_server.clone();
+                async move { io::Result::Ok(s) }
+            }))
+            .map_err(move |e| {
+                crit!(log, "HTTP server failed: {}", e);
+                process::abort();
+            }),
+        ));
+    }
 
     let worker = Worker {
         log: log.new(o!("module" => "worker")),
@@ -176,7 +181,7 @@ pub(super) async fn start_instance<A: Authority + 'static>(
         volume_id,
     };
 
-    tokio::spawn(worker.run());
+    tokio::spawn(abort_on_panic(worker.run()));
 
     let our_descriptor = ControllerDescriptor {
         controller_uri: http_uri,
@@ -206,9 +211,10 @@ pub(super) async fn start_instance<A: Authority + 'static>(
         region,
     );
 
-    tokio::spawn(controller.run().map_err(|e| {
-        panic!("ControllerOuter failed: {}", e.to_string());
-    }));
+    tokio::spawn(abort_on_panic(controller.run().map_err(move |e| {
+        crit!(log, "ControllerOuter failed: {}", e.to_string());
+        process::abort()
+    })));
 
     Ok(Handle::new(authority, handle_tx, trigger, our_descriptor))
 }

launchpad/Cargo.toml
@@ -13,3 +13,4 @@ proptest = "1.0.0"
 reqwest = "0.11.1"
 chrono = { version = "0.4.0", features = ["serde"] }
 tokio = { version = "1.8.1", features = ["full"] }
+futures = "0.3.15"

launchpad/src/futures.rs
@@ -1,5 +1,10 @@
 //! Utilities for working with futures, async/await, and tokio
 
+use futures::{FutureExt, TryFutureExt};
+use std::future::Future;
+use std::panic::AssertUnwindSafe;
+use std::process;
+
 /// A version of the [`tokio::select`] macro that also emits an `allow` annotation for
 /// `clippy::unreachable` and `clippy::panic`, since both are internal to the expansion of the macro
 /// and things we don't have control over.
@@ -11,4 +16,15 @@ macro_rules! select {
             tokio::select!($($args)*)
         }
     };
+}
+
+/// Wrap the given future in a handler that will cause the entire process to exit if the future
+/// panics during its execution
+pub fn abort_on_panic<F, A>(f: F) -> impl Future<Output = A> + Send + 'static
+where
+    F: Future<Output = A> + Send + 'static,
+{
+    AssertUnwindSafe(f) // safe because we don't actually use the future when handling errors
+        .catch_unwind()
+        .unwrap_or_else(|_| process::abort())
 }

commit 107586a3b6b39d06b8d4a07574c4527f3a01df23

Don't abort on task failure in tests
Add a new noria_server Config field, `abort_on_task_failure`, which
defaults to true but if set to false will disable the abort-on-panic (or
failure) behavior introduced in df9fc69ad (Abort the process if the
important tasks fail, 2021-08-16), and then set this field to false in
all places we're initializing a noria-server in a background thread as
part of an existing process: integration tests, noria-logictest, and the
query generator benchmark harness. This prevents errors that occur in
the noria server task from causing an unrecoverable full-process abort
in these cases, which is not only difficult to debug, but also
frequently not actually an error we care about - in this particular
case, this was triggered by the cleanup process for integration
tests (specifically the Drop impl for Deployment) removing the zookeeper
node out from under a noria server while it was running, which is
expected behavior and not actually something we need to worry about.

Change-Id: I4995aeb7f7841aa005a71573d8c7dabcd0ac3dc6
Reviewed-on: https://gerrit.readyset.name/c/readyset/+/374
Reviewed-by: Justin Miron <justin@readyset.io>
Tested-by: Buildkite CI

noria/noria-logictest/src/runner.rs
@@ -405,7 +405,7 @@ impl TestScript {
         loop {
             retry += 1;
 
-            let mut builder = Builder::default();
+            let mut builder = Builder::for_tests();
             builder.log_with(run_opts.logger());
 
             if run_opts.enable_reuse {

noria/server/src/builder.rs
@@ -41,6 +41,12 @@ impl Default for Builder {
     }
 }
 impl Builder {
+    /// Construct a new [`Builder`] with configuration setup for running tests
+    pub fn for_tests() -> Self {
+        let mut builder = Self::default();
+        builder.set_abort_on_task_failure(false);
+    }
+
     /// Set the maximum number of concurrent partial replay requests a domain can have outstanding
     /// at any given time.
     ///
@@ -137,6 +143,12 @@ impl Builder {
     /// Configures the volume id associated with this server.
     pub fn set_volume_id(&mut self, volume_id: VolumeId) {
         self.volume_id = Some(volume_id);
+    }
+
+    /// Set the value of [`Config::abort_on_task_failure`]. See the documentation of that field for
+    /// more information.
+    pub fn set_abort_on_task_failure(&mut self, abort_on_task_failure: bool) {
+        self.config.abort_on_task_failure = abort_on_task_failure;
     }
 
     /// Start a server instance and return a handle to it.

noria/server/src/integration.rs
@@ -37,7 +37,7 @@ use vec1::vec1;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn it_completes() {
-    let mut builder = Builder::default();
+    let mut builder = Builder::for_tests();
     builder.set_sharding(Some(DEFAULT_SHARDING));
     builder.log_with(crate::logger_pls());
     builder.set_persistence(get_persistence_params("it_completes"));
@@ -1091,7 +1091,7 @@ async fn it_recovers_persisted_bases() {
     );
 
     {
-        let mut g = Builder::default();
+        let mut g = Builder::for_tests();
         g.log_with(crate::logger_pls());
         g.set_persistence(persistence_params.clone());
         let mut g = g.start(authority.clone()).await.unwrap();
@@ -1122,7 +1122,7 @@ async fn it_recovers_persisted_bases() {
     sleep().await;
 
     let authority = Arc::new(LocalAuthority::new_with_store(authority_store));
-    let mut g = Builder::default();
+    let mut g = Builder::for_tests();
     g.set_persistence(persistence_params);
     g.log_with(crate::logger_pls());
     let mut g = g.start(authority.clone()).await.unwrap();
@@ -1157,7 +1157,7 @@ async fn it_recovers_persisted_bases_with_volume_id() {
     );
 
     {
-        let mut g = Builder::default();
+        let mut g = Builder::for_tests();
         g.log_with(crate::logger_pls());
         g.set_persistence(persistence_params.clone());
         g.set_volume_id("ef731j2".into());
@@ -1189,7 +1189,7 @@ async fn it_recovers_persisted_bases_with_volume_id() {
     sleep().await;
 
     let authority = Arc::new(LocalAuthority::new_with_store(authority_store));
-    let mut g = Builder::default();
+    let mut g = Builder::for_tests();
     g.set_persistence(persistence_params);
     g.set_volume_id("ef731j2".into());
     g.log_with(crate::logger_pls());
@@ -1224,7 +1224,7 @@ async fn it_doesnt_recover_persisted_bases_with_wrong_volume_id() {
     );
 
     {
-        let mut g = Builder::default();
+        let mut g = Builder::for_tests();
         g.log_with(crate::logger_pls());
         g.set_persistence(persistence_params.clone());
         g.set_volume_id("ef731j2".into());
@@ -1255,7 +1255,7 @@ async fn it_doesnt_recover_persisted_bases_with_wrong_volume_id() {
 
     sleep().await;
 
-    let mut g = Builder::default();
+    let mut g = Builder::for_tests();
     let authority = Arc::new(LocalAuthority::new());
     g.set_persistence(persistence_params);
     g.set_volume_id("j3131t8".into());
@@ -1327,7 +1327,7 @@ async fn view_connection_churn() {
     let authority_store = Arc::new(LocalAuthorityStore::new());
     let authority = Arc::new(LocalAuthority::new_with_store(authority_store));
 
-    let mut builder = Builder::default();
+    let mut builder = Builder::for_tests();
     builder.set_sharding(Some(DEFAULT_SHARDING));
     builder.set_persistence(get_persistence_params("connection_churn"));
     let mut g = builder.start(authority.clone()).await.unwrap();
@@ -1348,7 +1348,7 @@ async fn view_connection_churn() {
             let authority = authority.clone();
             let tx = tx.clone();
             tokio::spawn(async move {
-                let mut builder = Builder::default();
+                let mut builder = Builder::for_tests();
                 builder.set_sharding(Some(DEFAULT_SHARDING));
                 builder.set_persistence(get_persistence_params("connection_churn"));
                 let mut g = builder.start(authority.clone()).await.unwrap();
@@ -1378,7 +1378,7 @@ async fn table_connection_churn() {
     let authority_store = Arc::new(LocalAuthorityStore::new());
     let authority = Arc::new(LocalAuthority::new_with_store(authority_store));
 
-    let mut builder = Builder::default();
+    let mut builder = Builder::for_tests();
     builder.set_sharding(Some(DEFAULT_SHARDING));
     builder.set_persistence(get_persistence_params("connection_churn"));
     // builder.log_with(crate::logger_pls());
@@ -1395,7 +1395,7 @@ async fn table_connection_churn() {
             let authority = authority.clone();
             let tx = tx.clone();
             tokio::spawn(async move {
-                let mut builder = Builder::default();
+                let mut builder = Builder::for_tests();
                 builder.set_sharding(Some(DEFAULT_SHARDING));
                 builder.set_persistence(get_persistence_params("connection_churn"));
                 let mut g = builder.start(authority.clone()).await.unwrap();
@@ -1440,7 +1440,7 @@ async fn it_recovers_persisted_bases_w_multiple_nodes() {
     );
 
     {
-        let mut g = Builder::default();
+        let mut g = Builder::for_tests();
         g.set_persistence(persistence_parameters.clone());
         let mut g = g.start(authority.clone()).await.unwrap();
         sleep().await;
@@ -1472,7 +1472,7 @@ async fn it_recovers_persisted_bases_w_multiple_nodes() {
     // Create a new controller with the same authority store, and make sure that it recovers to the same
     // state that the other one had.
     let authority = Arc::new(LocalAuthority::new_with_store(authority_store));
-    let mut g = Builder::default();
+    let mut g = Builder::for_tests();
     g.set_persistence(persistence_parameters);
     let mut g = g.start(authority.clone()).await.unwrap();
     sleep().await;
@@ -1503,7 +1503,7 @@ async fn it_recovers_persisted_bases_w_multiple_nodes_and_volume_id() {
     );
 
     {
-        let mut g = Builder::default();
+        let mut g = Builder::for_tests();
         g.set_persistence(persistence_parameters.clone());
         g.set_volume_id("ef731j2".into());
         let mut g = g.start(authority.clone()).await.unwrap();
@@ -1535,7 +1535,7 @@ async fn it_recovers_persisted_bases_w_multiple_nodes_and_volume_id() {
     // Create a new controller with the same authority store, and make sure that it recovers to the same
     // state that the other one had.
     let authority = Arc::new(LocalAuthority::new_with_store(authority_store));
-    let mut g = Builder::default();
+    let mut g = Builder::for_tests();
     g.set_persistence(persistence_parameters);
     g.set_volume_id("ef731j2".into());
     let mut g = g.start(authority.clone()).await.unwrap();
@@ -2132,7 +2132,7 @@ async fn replay_during_replay() {
     // the join key that does not exist in the view the record was sent from. since joins only do
     // lookups into the origin view during forward processing when it receives things from the
     // right in a left join, that's what we have to construct.
-    let mut g = Builder::default();
+    let mut g = Builder::for_tests();
     g.disable_partial();
     g.set_persistence(get_persistence_params("replay_during_replay"));
     let mut g = g.start_local().await.unwrap();
@@ -3743,7 +3743,7 @@ async fn correct_nested_view_schema() {
                      JOIN votes ON (stories.id = votes.story) \
                      WHERE stories.id = ? GROUP BY votes.story;";
 
-    let mut b = Builder::default();
+    let mut b = Builder::for_tests();
     // need to disable partial due to lack of support for key subsumption (#99)
     b.disable_partial();
     b.set_sharding(None);
@@ -4150,7 +4150,7 @@ async fn self_join_param() {
 #[ignore]
 async fn non_sql_materialized_range_query() {
     let mut g = {
-        let mut builder = Builder::default();
+        let mut builder = Builder::for_tests();
         builder.disable_partial();
         builder.set_sharding(None);
         builder.set_persistence(get_persistence_params("non_sql_materialized_range_query"));
@@ -4191,7 +4191,7 @@ async fn non_sql_materialized_range_query() {
 #[ignore]
 async fn non_sql_range_upquery() {
     let mut g = {
-        let mut builder = Builder::default();
+        let mut builder = Builder::for_tests();
         builder.set_sharding(None);
         builder.set_persistence(get_persistence_params("non_sql_range_upquery"));
         builder.start_local()
@@ -4231,7 +4231,7 @@ async fn non_sql_range_upquery() {
 #[ignore]
 async fn range_upquery_after_point_queries() {
     let mut g = {
-        let mut builder = Builder::default();
+        let mut builder = Builder::for_tests();
         builder.set_sharding(None);
         builder.set_persistence(get_persistence_params("non_sql_range_upquery"));
         builder.start_local()
@@ -4392,7 +4392,7 @@ async fn view_reuse_aliases() {
 #[ignore]
 async fn post_read_ilike() {
     let mut g = {
-        let mut builder = Builder::default();
+        let mut builder = Builder::for_tests();
         builder.disable_partial();
         builder.set_sharding(None);
         builder.set_persistence(get_persistence_params("post_read_ilike"));
@@ -6518,7 +6518,7 @@ async fn replicate_to_unavailable_worker() {
     let w2_authority = Arc::new(LocalAuthority::new_with_store(authority_store));
     let cluster_name = "replicate_to_non_existent_worker";
 
-    let mut builder = Builder::default();
+    let mut builder = Builder::for_tests();
     builder.log_with(logger_pls());
     builder.set_sharding(Some(DEFAULT_SHARDING));
     builder.set_persistence(get_persistence_params(cluster_name));

noria/server/src/integration_serial.rs
@@ -184,7 +184,7 @@ async fn test_metrics_client() {
     // Start a local instance of noria and connect the metrics client to it.
     // We assign it a different port than the rest of the tests to prevent
     // other tests impacting the metrics collected.
-    let builder = Builder::default();
+    let mut builder = Builder::for_tests();
     let mut g = builder.start_local().await.unwrap();
     let mut client = initialize_metrics(&mut g).await;
 

noria/server/src/integration_utils.rs
@@ -76,7 +76,7 @@ pub async fn build_custom(
     reader_only: bool,
 ) -> Handle<LocalAuthority> {
     use crate::logger_pls;
-    let mut builder = Builder::default();
+    let mut builder = Builder::for_tests();
     if log {
         builder.log_with(logger_pls());
     }

noria/server/src/lib.rs
@@ -434,6 +434,8 @@ pub(crate) struct Config {
     pub(crate) quorum: usize,
     pub(crate) reuse: Option<ReuseConfigType>,
     pub(crate) primary_region: Option<String>,
+    /// If set to true (the default), failing tokio tasks will cause a full-process abort.
+    pub(crate) abort_on_task_failure: bool,
 }
 
 impl Default for Config {
@@ -453,6 +455,7 @@ impl Default for Config {
             quorum: 1,
             reuse: None,
             primary_region: None,
+            abort_on_task_failure: true,
         }
     }
 }

noria/server/src/startup.rs
@@ -63,7 +63,7 @@ use std::{
 };
 use std::{io, process};
 
-use futures_util::future::TryFutureExt;
+use futures_util::future::{Either, TryFutureExt};
 use hyper::{self, header::CONTENT_TYPE, Method, StatusCode};
 use hyper::{service::make_service_fn, Body, Request, Response};
 use launchpad::futures::abort_on_panic;
@@ -132,26 +132,42 @@ pub(super) async fn start_instance<A: Authority + 'static>(
         authority: authority.clone(),
     };
 
-    tokio::spawn(abort_on_panic(crate::worker::readers::listen(
+    let Config {
+        abort_on_task_failure,
+        ..
+    } = config;
+
+    macro_rules! maybe_abort_on_panic {
+        ($fut: expr) => {{
+            let fut = $fut;
+            if abort_on_task_failure {
+                Either::Left(abort_on_panic(fut))
+            } else {
+                Either::Right(fut)
+            }
+        }};
+    }
+
+    tokio::spawn(maybe_abort_on_panic!(crate::worker::readers::listen(
         valve.clone(),
         readers_listener,
         readers.clone(),
     )));
 
     {
-        tokio::spawn(abort_on_panic(
-            hyper::server::Server::builder(hyper::server::accept::from_stream(
-                valve.wrap(TcpListenerStream::new(http_listener)),
-            ))
-            .serve(make_service_fn(move |_| {
-                let s = http_server.clone();
-                async move { io::Result::Ok(s) }
-            }))
-            .map_err(|e| {
-                error!(error = %e, "HTTP server failed");
+        tokio::spawn(maybe_abort_on_panic!(hyper::server::Server::builder(
+            hyper::server::accept::from_stream(valve.wrap(TcpListenerStream::new(http_listener)),)
+        )
+        .serve(make_service_fn(move |_| {
+            let s = http_server.clone();
+            async move { io::Result::Ok(s) }
+        }))
+        .map_err(move |e| {
+            error!(error = %e, "HTTP server failed");
+            if abort_on_task_failure {
                 process::abort();
-            }),
-        ));
+            }
+        })));
     }
 
     let worker = Worker {
@@ -169,7 +185,7 @@ pub(super) async fn start_instance<A: Authority + 'static>(
         domains: Default::default(),
     };
 
-    tokio::spawn(abort_on_panic(worker.run()));
+    tokio::spawn(maybe_abort_on_panic!(worker.run()));
 
     let our_descriptor = ControllerDescriptor {
         controller_uri: http_uri.clone(),
@@ -208,9 +224,11 @@ pub(super) async fn start_instance<A: Authority + 'static>(
         region,
     )?;
 
-    tokio::spawn(abort_on_panic(controller.run().map_err(move |e| {
+    tokio::spawn(maybe_abort_on_panic!(controller.run().map_err(move |e| {
         error!(error = %e, "ControllerOuter failed");
-        process::abort()
+        if abort_on_task_failure {
+            process::abort()
+        }
     })));
 
     Ok(Handle::new(authority, handle_tx, trigger, our_descriptor))

query_generator/src/benchmark.rs
@@ -409,7 +409,7 @@ impl Benchmark {
     }
 
     async fn setup_noria(&self) -> anyhow::Result<noria_server::Handle<LocalAuthority>> {
-        let mut builder = noria_server::Builder::default();
+        let mut builder = noria_server::Builder::for_tests();
         builder.set_sharding(self.shards);
         builder.set_persistence(PersistenceParameters {
             mode: DurabilityMode::DeleteOnExit,

noria-client/src/test_helpers.rs
@@ -113,7 +113,7 @@ where
     let b = barrier.clone();
     thread::spawn(move || {
         let authority = ZookeeperAuthority::new(&format!("{}/{}", zk_addr(), n)).unwrap();
-        let mut builder = Builder::default();
+        let mut builder = Builder::for_tests();
         if !partial {
             builder.disable_partial();
         }

replicators/tests/tests.rs
@@ -161,7 +161,7 @@ impl TestHandle {
         url: String,
         authority: Arc<LocalAuthority>,
     ) -> ReadySetResult<TestHandle> {
-        let noria = Builder::default()
+        let noria = Builder::for_tests()
             .start(Arc::clone(&authority))
             .await
             .unwrap();