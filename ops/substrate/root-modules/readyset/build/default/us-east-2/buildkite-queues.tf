a308deb163eff00fbe7b4bc7c2c582082cd469ef
Griffin Smith <root@gws.fyi>
2021-09-21 17:54:46 -0400

    build: Add a t3a.small queue
    Generalize adding multiple different queues for different instance
    types, and add a new queue with the t3a.small instance type, for running
    small, cheap build steps like commit lint.

--- ops/substrate/root-modules/readyset/build/default/us-east-2/buildkite-queues.tf
+++ ops/substrate/root-modules/readyset/build/default/us-east-2/buildkite-queues.tf
@@ -1,20 +1,29 @@
+locals {
+  instance_types = [
+    "t3a.small",
+    "c5a.4xlarge",
+  ]
+}
+
 module "buildkite_queue_shared" {
   source           = "../../../../../modules/buildkite-queue-shared/regional"
   environment      = "build"
-  buildkite_queues = ["c5a_4xlarge"]
+  buildkite_queues = [for it in local.instance_types : replace(it, ".", "-")]
 }
 
-module "buildkite_queue_c5a-4xlarge" {
+module "buildkite_queue" {
+  for_each = toset(local.instance_types)
+
   source      = "../../../../../modules/buildkite-queue/regional"
   environment = "build"
 
-  buildkite_queue = "c5a-4xlarge"
+  buildkite_queue = replace(each.key, ".", "-")
 
   buildkite_agent_token_parameter_store_path = module.buildkite_queue_shared.buildkite_agent_token_parameter_store_path
 
   secrets_bucket   = module.buildkite_queue_shared.secrets_bucket
   artifacts_bucket = module.buildkite_queue_shared.artifacts_bucket
 
-  instance_type = "c5a.4xlarge"
+  instance_type = each.key
   max_size      = 3
 }
