2f537d1cda7a7afc1b3e884451a85a3938712d42
Dan Wilbanks <dan@readyset.io>
2021-09-23 09:23:11 -0400

    Add order_limit_removal pass
    Added a pass to remove the limit and order fields of a query when it can
    be determined that the query will return a single result.
    
    This determination is made by checking whether the query includes an
    equality conditional comparing a primary or unique key to a literal.
    This pass could be expanded to catch other conditionals that would
    result in at most one row returned (e.g., a conditional compary a unique
    key to a select statement that returns a single result).
    
    This pass requires that all columns have explicit table names, and must
    be run after the expand_implied_tables() pass.
    
    Change-Id: I7e46ea87f16a86fa0804b6f92b95672964907d2c
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/506
    Tested-by: Buildkite CI
    Reviewed-by: Griffin Smith <griffin@readyset.io>

---
+++ noria/server/src/controller/sql/passes/order_limit_removal.rs
@@ -0,0 +1,229 @@
+use nom_sql::{
+    BinaryOperator, Column, ColumnConstraint, CreateTableStatement, Expression, SqlQuery,
+};
+use noria::{ReadySetError, ReadySetResult};
+use std::collections::HashMap;
+
+pub trait OrderLimitRemoval: Sized {
+    /// Remove any LIMIT and ORDER statement belonging to a query that is determined to return at
+    /// most one row. Under this condition, the order and limit have no effect on the result.
+    ///
+    /// This past must be run after the expand_implied_tables() pass, because it requires that each
+    /// column have an associated table name.
+    fn order_limit_removal(
+        self,
+        base_schemas: &HashMap<String, CreateTableStatement>,
+    ) -> ReadySetResult<Self>;
+}
+
+fn is_unique_or_primary(
+    col: &Column,
+    base_schemas: &HashMap<String, CreateTableStatement>,
+) -> ReadySetResult<bool> {
+    // This assumes that we will find exactly one table matching col.table and exactly one col
+    // matching col.name. It also assumes that col will always have an associated table.
+    // The last assumption will only hold true if this pass is run after the
+    // expand_implied_tables() pass
+    let table_name = col.table.as_ref().ok_or_else(|| {
+        ReadySetError::Internal(
+            "All columns must have an associated table name at this point".to_string(),
+        )
+    })?;
+    let table = base_schemas.get(table_name).ok_or_else(|| {
+        ReadySetError::Internal("Table name must match table in base schema".to_string())
+    })?;
+    let col_spec = table
+        .fields
+        .iter()
+        .find(|col_spec| col_spec.column.name == col.name)
+        .ok_or_else(|| {
+            ReadySetError::Internal(
+                "Column name must match column in base schema table".to_string(),
+            )
+        })?;
+    Ok(col_spec
+        .constraints
+        .iter()
+        .any(|c| matches!(c, ColumnConstraint::Unique | ColumnConstraint::PrimaryKey)))
+}
+
+fn compares_unique_key_against_literal(
+    expr: &Expression,
+    base_schemas: &HashMap<String, CreateTableStatement>,
+) -> ReadySetResult<bool> {
+    match expr {
+        Expression::BinaryOp {
+            lhs: box Expression::Literal(_),
+            rhs: box Expression::Column(ref c),
+            op: BinaryOperator::Equal | BinaryOperator::Is,
+        }
+        | Expression::BinaryOp {
+            lhs: box Expression::Column(ref c),
+            rhs: box Expression::Literal(_),
+            op: BinaryOperator::Equal | BinaryOperator::Is,
+        } => Ok(is_unique_or_primary(c, base_schemas)?),
+        Expression::BinaryOp {
+            op: BinaryOperator::And,
+            ref lhs,
+            ref rhs,
+        } => Ok(compares_unique_key_against_literal(lhs, base_schemas)?
+            || compares_unique_key_against_literal(rhs, base_schemas)?),
+        // TODO(DAN): it may be possible to determine that a query will return a single (or no)
+        // resut if it has a nested select in the conditional
+        _ => Ok(false),
+    }
+}
+
+impl OrderLimitRemoval for SqlQuery {
+    fn order_limit_removal(
+        mut self,
+        base_schemas: &HashMap<String, CreateTableStatement>,
+    ) -> ReadySetResult<Self> {
+        // If the query uses an equality filter on a column that has a unique or primary key
+        // index, remove order and limit
+        if let SqlQuery::Select(stmt) = &mut self {
+            if stmt.limit.is_none() {
+                return Ok(self);
+            }
+            if let Some(ref expr) = stmt.where_clause {
+                if compares_unique_key_against_literal(expr, base_schemas)? {
+                    stmt.limit = None;
+                    stmt.order = None;
+                    return Ok(self);
+                }
+            }
+        }
+        Ok(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use nom_sql::{parse_query, Dialect, LimitClause, OrderClause, OrderType};
+
+    use super::*;
+
+    fn generate_base_schemas() -> HashMap<String, CreateTableStatement> {
+        use nom_sql::{ColumnSpecification, Table};
+
+        let table = Table {
+            name: "t".to_string(),
+            alias: None,
+            schema: None,
+        };
+
+        let col1 = ColumnSpecification {
+            column: Column {
+                name: "c1".to_string(),
+                table: Some("t".to_string()),
+                function: None,
+            },
+            sql_type: nom_sql::SqlType::Bool,
+            constraints: vec![ColumnConstraint::PrimaryKey],
+            comment: None,
+        };
+        let col2 = ColumnSpecification {
+            column: Column {
+                name: "c2".to_string(),
+                table: Some("t".to_string()),
+                function: None,
+            },
+            sql_type: nom_sql::SqlType::Bool,
+            constraints: vec![ColumnConstraint::Unique],
+            comment: None,
+        };
+        let col3 = ColumnSpecification {
+            column: Column {
+                name: "c3".to_string(),
+                table: Some("t".to_string()),
+                function: None,
+            },
+            sql_type: nom_sql::SqlType::Bool,
+            constraints: vec![],
+            comment: None,
+        };
+
+        let fields = vec![col1, col2, col3];
+        let keys = None;
+        let if_not_exists = false;
+
+        let mut base_schemas = HashMap::new();
+        base_schemas.insert(
+            "t".to_string(),
+            CreateTableStatement {
+                table,
+                fields,
+                keys,
+                if_not_exists,
+            },
+        );
+        base_schemas
+    }
+
+    fn removes_limit_order(input: &str) {
+        let input_query = parse_query(Dialect::MySQL, input).unwrap();
+        let revised_query = input_query
+            .order_limit_removal(&generate_base_schemas())
+            .unwrap();
+        match revised_query {
+            SqlQuery::Select(stmt) => {
+                assert!(stmt.order.is_none());
+                assert!(stmt.limit.is_none());
+            }
+            _ => panic!("Invalid query returned: {:?}", actual),
+        }
+    }
+
+    fn does_not_change_limit_order(input: &str) {
+        let input_query = parse_query(Dialect::MySQL, input).unwrap();
+        assert_eq!(
+            input_query,
+            input_query
+                .clone()
+                .order_limit_removal(&generate_base_schemas())
+                .unwrap()
+        );
+    }
+
+    #[test]
+    fn single_primary_key() {
+        // condition on primary key
+        removes_limit_order("SELECT t.c1 FROM t WHERE t.c1 = 1 ORDER BY c1 ASC LIMIT 10")
+    }
+
+    #[test]
+    fn single_unique_key() {
+        // condition on unique key
+        removes_limit_order("SELECT t.c1 FROM t WHERE t.c2 = 1 ORDER BY c1 ASC LIMIT 10")
+    }
+
+    #[test]
+    fn single_non_unique_key() {
+        // condition on non-unique (or primary) key
+        does_not_change_limit_order("SELECT t.c1 FROM t WHERE t.c3 = 1 ORDER BY c1 ASC LIMIT 10")
+    }
+
+    #[test]
+    fn unique_and_non_unique_key1() {
+        // condition on non-unique AND unique key
+        removes_limit_order(
+            "SELECT t.c1 FROM t WHERE t.c3 = 1 AND t.c2 = 1 ORDER BY c1 ASC LIMIT 10",
+        )
+    }
+
+    #[test]
+    fn unique_and_non_unique_key2() {
+        // condition on primary AND non-unique key
+        removes_limit_order(
+            "SELECT t.c1 FROM t WHERE t.c1 = 1 AND t.c3 = 1 ORDER BY c1 ASC LIMIT 10",
+        )
+    }
+
+    #[test]
+    fn non_unique_and_non_unique_key() {
+        // condition on non-unique AND (the same) non-unique key
+        does_not_change_limit_order(
+            "SELECT t.c1 FROM t WHERE t.c3 = 1 AND t.c3 = 1 ORDER BY c1 ASC LIMIT 10",
+        )
+    }
+}
