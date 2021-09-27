fc57b5ba164431989850b1fe71886285796125b9
Harley Klein <harley@readyset.io>
2021-09-27 16:12:44 -0700

    substrate: Add role for Packer in deploy account
    This new role is what we can assume for specific Packer runs to run
    inside the deploy account to "copy" the image into the right place.
    
    Change-Id: I1c4a9e97b14603a8e9b4228e9d5d44d01393b9e5
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/586
    Tested-by: Buildkite CI
    Reviewed-by: Griffin Smith <griffin@readyset.io>
    Reviewed-by: Michael Cronce <mc@readyset.io>
    Terraform-Plan-Review: Griffin Smith <griffin@readyset.io>

---
+++ ops/substrate/root-modules/deploy/global/packer.tf
@@ -0,0 +1,61 @@
+data "aws_iam_policy_document" "packer_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "AWS"
+      # Should probably be another pipeline of deploy stuff separate from ops.
+      identifiers = ["arn:aws:iam::305232526136:role/buildkite-ops-Role"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "packer" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:AttachVolume",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:CopyImage",
+      "ec2:CreateImage",
+      "ec2:CreateKeypair",
+      "ec2:CreateSecurityGroup",
+      "ec2:CreateSnapshot",
+      "ec2:CreateTags",
+      "ec2:CreateVolume",
+      "ec2:DeleteKeyPair",
+      "ec2:DeleteSecurityGroup",
+      "ec2:DeleteSnapshot",
+      "ec2:DeleteVolume",
+      "ec2:DeregisterImage",
+      "ec2:DescribeImageAttribute",
+      "ec2:DescribeImages",
+      "ec2:DescribeInstances",
+      "ec2:DescribeInstanceStatus",
+      "ec2:DescribeRegions",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSnapshots",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeTags",
+      "ec2:DescribeVolumes",
+      "ec2:DetachVolume",
+      "ec2:GetPasswordData",
+      "ec2:ModifyImageAttribute",
+      "ec2:ModifyInstanceAttribute",
+      "ec2:ModifySnapshotAttribute",
+      "ec2:RegisterImage",
+      "ec2:RunInstances",
+      "ec2:StopInstances",
+      "ec2:TerminateInstances"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "packer" {
+  name               = "Packer"
+  assume_role_policy = data.aws_iam_policy_document.packer_assume_role.json
+  inline_policy {
+    policy = data.aws_iam_policy_document.packer.json
+  }
+}
