b80e34eb22c9bf11850881b37a9ce33761d2f187
Francisco Noriega <fran@readyset.io>
2021-09-27 21:13:04 +0200

    Ignoring `types.rs` tests due to flakiness.
    For now, we are going to ignore these tests until we understand why they are
    failing and how to fix them.
    Follow-up ticket: ENG-620
    
    Change-Id: Ibf39a27a8bf2631bf5d1a9e1ccea6d40ceec2e14
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/578
    Reviewed-by: Harley Klein <harley@readyset.io>
    Tested-by: Buildkite CI

--- noria-psql/tests/types.rs
+++ noria-psql/tests/types.rs
@@ -90,13 +90,13 @@ mod types {
 
     // https://docs.rs/tokio-postgres/0.7.2/tokio_postgres/types/trait.ToSql.html#types
     test_types! {
-        bool_bool("bool", bool);
-        smallint_i16("smallint", i16);
-        int_i32("integer", i32);
+        #[ignore] bool_bool("bool", bool);
+        #[ignore] smallint_i16("smallint", i16);
+        #[ignore] int_i32("integer", i32);
         #[ignore] oid_u32("oid", u32);
-        bigint_i64("bigint", i64);
-        real_f32("real", f32);
-        double_f64("double precision", f64);
-        text_string("text", String);
+        #[ignore] bigint_i64("bigint", i64);
+        #[ignore] real_f32("real", f32);
+        #[ignore] double_f64("double precision", f64);
+        #[ignore] text_string("text", String);
     }
 }
