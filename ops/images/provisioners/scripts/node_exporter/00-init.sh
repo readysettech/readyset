a40ef1f11b4db09e071feeebe281484e3609ff4c
Harley Klein <harley@readyset.io>
2021-09-29 21:52:53 -0700

    ops/images: Create node_exporter user
    The user not existing is appearently not caught by systemd-analyze.
    
    Change-Id: Ie5deded026348d3dea25b3e2d3f92598eba80e8e
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/630
    Tested-by: Buildkite CI
    Reviewed-by: Michael Cronce <mc@readyset.io>
    Reviewed-by: Griffin Smith <griffin@readyset.io>

--- ops/images/provisioners/scripts/node_exporter/00-init.sh
+++ ops/images/provisioners/scripts/node_exporter/00-init.sh
@@ -13,6 +13,8 @@ sudo install -o root -g root -m 755 \
   node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter \
   /usr/local/bin/node_exporter
 
+sudo useradd --system --shell /usr/sbin/nologin node_exporter
+
 sudo install -o root -g root -m 644 \
   /tmp/node_exporter/etc_systemd_system_node_exporter.service \
   /etc/systemd/system/node_exporter.service
