d71cddeda9b0a84fe49b09caea07190366daba97
Justin Miron <justin@readyset.io>
2021-10-07 04:46:23 +0000

    Update noria/tools to use arbitrary authorities and cleanup code.
    This CL updates all the noria/tools to have an arbitrary authority
    passed in through command-line arguements. The code is also updated
    to use clap's structopt format.
    
    Change-Id: Icdbfbb455b84250a64f0cbd0d1f785a9b0c73e90
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/703
    Tested-by: Buildkite CI
    Reviewed-by: Dan Wilbanks <dan@readyset.io>

--- noria/tools/src/query_installer.rs
+++ noria/tools/src/query_installer.rs
@@ -1,11 +1,40 @@
-use noria::consensus::{Authority, ZookeeperAuthority};
+//! Tool to install a test query view in noria. This tool
+//! creates a table in Noria and creates a view for the SELECT
+//! * query for that table. It then performs a lookup of the
+//! view.
+use clap::Clap;
+use noria::consensus::AuthorityType;
 use noria::ControllerHandle;
 
-// This exists for testing purposes. It is an easy script to install a noria recipe. It assumes the deployment
-// is named `myapp` and that its listening on address 127.0.0.1
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let test_sql_string = "
+#[derive(Clap)]
+#[clap(name = "query_installer")]
+struct QueryInstaller {
+    #[clap(short, long, env("AUTHORITY_ADDRESS"), default_value("127.0.0.1:2181"))]
+    authority_address: String,
+
+    #[clap(long, env("AUTHORITY"), default_value("zookeeper"), possible_values = &["consul", "zookeeper"])]
+    authority: AuthorityType,
+
+    #[clap(short, long, env("NORIA_DEPLOYMENT"))]
+    deployment: String,
+
+    /// Name of the view to use in the install_recipe statement.
+    #[clap(short, long)]
+    query: String,
+}
+
+impl QueryInstaller {
+    pub async fn run(self) -> anyhow::Result<()> {
+        let authority = self
+            .authority
+            .to_authority(&self.authority_address, &self.deployment)
+            .await;
+
+        let mut handle: ControllerHandle = ControllerHandle::new(authority).await;
+        handle.ready().await.unwrap();
+
+        let test_sql_string = format!(
+            "
             CREATE TABLE employees (
                 emp_no      INT             NOT NULL,
                 first_name  VARCHAR(14)     NOT NULL,
@@ -13,18 +42,24 @@ async fn main() -> anyhow::Result<()> {
                 gender      VARCHAR(1)  NOT NULL,
                 PRIMARY KEY (emp_no)
             );
-            QUERY testQuery : \
+            QUERY {} : \
                 SELECT * FROM employees;
-        ";
+        ",
+            self.query.clone()
+        );
 
-    let authority = Authority::from(ZookeeperAuthority::new("127.0.0.1:2181/myapp").await?);
-    let mut noria = ControllerHandle::new(authority).await;
+        println!("Waiting for noria");
+        handle.install_recipe(&test_sql_string).await.unwrap();
+        let mut getter = handle.view(&self.query).await.unwrap();
 
-    println!("Waiting for noria");
-    noria.install_recipe(test_sql_string).await.unwrap();
-    let mut getter = noria.view("testQuery").await.unwrap();
+        let results = getter.lookup(&[0.into()], true).await.unwrap();
+        println!("Results: {:?}", results);
+        Ok(())
+    }
+}
 
-    let results = getter.lookup(&[0.into()], true).await.unwrap();
-    println!("Results: {:?}", results);
-    Ok(())
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let query_installer = QueryInstaller::parse();
+    query_installer.run().await
 }
