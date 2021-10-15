ea8f3aeec9485ff96b7c3f4e566b0e738bc65449
Justin Miron <justin@readyset.io>
2021-10-15 19:56:15 +0000

    Support for writing Readyset logs to CloudWatch.
    This CL makes two changes:
      * Creates a new policy that allows writing to cloudwatch from
        vector.
      * Changes the vector config in the aggregator to write to
        cloudwatch.
    
    Change-Id: Ic8a7239cc9115b417c78369b8e6b694f9d4cb572
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/790
    Tested-by: Buildkite CI
    Reviewed-by: Harley Klein <harley@readyset.io>

--- ops/images/provisioners/files/vector-aggregator/aws/usr_local_bin_user-data-init.sh
+++ ops/images/provisioners/files/vector-aggregator/aws/usr_local_bin_user-data-init.sh
@@ -16,4 +16,27 @@ trap 'on_error' ERR
 /usr/local/bin/configure-consul-client.sh
 /usr/local/bin/configure-vector.sh
 
+cat > /etc/vector.d/vector.toml <<EOF
+[sources.in]
+type = "vector"
+address = "0.0.0.0:9000"
+
+[sinks.out]
+inputs = ["in"]
+type = "console"
+target = "stdout"
+encoding.codec = "text"
+
+[sinks.cloudwatch]
+type = "aws_cloudwatch_logs"
+inputs = ["in"]
+create_missing_group = true
+create_missing_stream = true
+group_name = "${AWS_CLOUDFORMATION_STACK}"
+compression = "none"
+region = "${AWS_CLOUDFORMATION_REGION}"
+stream_name = "readyset"
+encoding.codec = "json"
+EOF
+
 usr/local/bin/cfn-signal-wrapper.sh 0
