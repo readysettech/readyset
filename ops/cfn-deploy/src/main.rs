a66c4a64796519a50023e85adbf72947dbc59a25
Harley Klein <harley@readyset.io>
2021-10-15 13:21:29 -0700

    ops/cfn-deploy: initial work for cfn deployment
    Inspired by the Makefile that Buildkite has for their stack, create a
    program that will modify the CloudFormation template using information
    from a Packer manifest and otherwise update other defaults as needed.
    
    As a first step, parse a Packer manifest and create YAML which can be
    copied and pasted into the right location.
    
    Change-Id: I45ff3d661ea260a9c961866ecb7e33ce5e00488d
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/788
    Tested-by: Buildkite CI
    Reviewed-by: Griffin Smith <griffin@readyset.io>

---
+++ ops/cfn-deploy/src/main.rs
@@ -0,0 +1,118 @@
+#![warn(clippy::dbg_macro)]
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+use anyhow::{bail, Result};
+use clap::{AppSettings, Clap};
+use serde::Deserialize;
+use tracing_subscriber::EnvFilter;
+
+#[derive(Clap, Debug)]
+enum Subcommand {
+    BuildCfnTemplate,
+}
+
+#[derive(Clap, Debug)]
+#[clap(setting=AppSettings::SubcommandRequired)]
+struct Opts {
+    #[clap(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(Deserialize, Debug)]
+struct PackerBuild {
+    name: String,
+    builder_type: String,
+    build_time: u64,
+    files: Option<String>,
+    artifact_id: String,
+    packer_run_uuid: String,
+    custom_data: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct PackerManifest {
+    builds: Vec<PackerBuild>,
+    last_run_uuid: String,
+}
+
+fn build_cfn_template() -> Result<()> {
+    let packer_manifest_path = Path::new("ops/image-deploy/packer-manifest.json");
+    let packer_manifest: PackerManifest =
+        serde_json::from_reader(BufReader::new(File::open(&packer_manifest_path)?))?;
+    let last_run_builds: Vec<&PackerBuild> = packer_manifest
+        .builds
+        .iter()
+        .filter(|build| build.packer_run_uuid == packer_manifest.last_run_uuid)
+        .collect();
+    let mut aws_ami_region_maps_for_template: HashMap<&str, HashMap<&str, HashMap<&str, &str>>> =
+        HashMap::new();
+    for build in last_run_builds {
+        let (mapping_name, templates) = match build.name.as_str() {
+            "readyset-mysql-adapter" => {
+                ("READYSETMYSQLADAPTER", vec!["readyset-mysql-template.yaml"])
+            }
+            "readyset-server" => (
+                "READYSETSERVER",
+                vec![
+                    "readyset-mysql-template.yaml",
+                    "readyset-postgresql-template.yaml",
+                ],
+            ),
+            "readyset-vector-aggregator" => (
+                "READYSETMONITOR",
+                vec![
+                    "readyset-mysql-template.yaml",
+                    "readyset-postgresql-template.yaml",
+                ],
+            ),
+            "readyset-authority-consul" => (
+                "AUTHORITYCONSUL",
+                vec!["readyset-authority-consul-template.yaml"],
+            ),
+            unknown_image_name => bail!(
+                "Cannot map image named {} for CloudFormation",
+                unknown_image_name
+            ),
+        };
+
+        for artifact in build.artifact_id.split(',') {
+            if let Some((region, ami_id)) = artifact.split_once(':') {
+                for template in &templates {
+                    let template_map = aws_ami_region_maps_for_template
+                        .entry(template)
+                        .or_default();
+                    let region_map = template_map.entry(region).or_default();
+                    region_map.insert(mapping_name, ami_id);
+                }
+            } else {
+                bail!("Invalid packer manifest, artifact id not correct format")
+            }
+        }
+    }
+
+    for (template_name, template_ami_region_map) in aws_ami_region_maps_for_template.iter() {
+        println!(
+            "For {}:\n {}",
+            template_name,
+            serde_yaml::to_string(&template_ami_region_map)?
+        );
+    }
+
+    Ok(())
+}
+fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .pretty()
+        .without_time()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let opts = Opts::parse();
+
+    match opts.subcommand {
+        Subcommand::BuildCfnTemplate => build_cfn_template(),
+    }
+}
