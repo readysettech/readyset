ddeaed16b33e2ada6130f63bdcf81c2f8bf512ae
Griffin Smith <griffin@readyset.io>
2021-10-20 17:38:18 -0400

    ops/images: Make sure binaries can be run
    Primarily to protect against people like me who have glibc's ld-linux.so
    at a weird path (hello nixos) from building test images that can never
    successfully run, add an extra step to all places where we depend on
    binaries we've compiled (readyset-server and readyset-mysql-adapter,
    specifically) to run the binaries with --help to check that they can
    run, and error out quickly if they can't.
    
    Change-Id: Ia63532958a7da5029f1c09df60047faac226a503
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/863
    Tested-by: Buildkite CI
    Reviewed-by: Justin Miron <justin@readyset.io>

--- ops/images/provisioners/scripts/readyset-mysql-adapter/00-init.sh
+++ ops/images/provisioners/scripts/readyset-mysql-adapter/00-init.sh
@@ -5,6 +5,8 @@ sudo install -o root -g root -m 755 \
   /tmp/readyset-mysql-adapter-binary \
   /usr/local/bin/readyset-mysql-adapter
 
+readyset-mysql-adapter --help # ensure it runs
+
 sudo install -o root -g root -m 644 \
   /tmp/readyset-mysql-adapter/etc_systemd_system_readyset-mysql-adapter.service \
   /etc/systemd/system/readyset-mysql-adapter.service
--- ops/images/provisioners/scripts/readyset-server/00-init.sh
+++ ops/images/provisioners/scripts/readyset-server/00-init.sh
@@ -5,6 +5,8 @@ sudo install -o root -g root -m 755 \
     /tmp/readyset-server-binary \
     /usr/local/bin/readyset-server
 
+readyset-server --help # ensure it runs
+
 sudo install -o root -g root -m 644 \
     /tmp/readyset-server/etc_systemd_system_readyset-server.service \
     /etc/systemd/system/readyset-server.service
