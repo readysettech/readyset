827627f231902ce5e1d4f2718f7f6f09fa58e4b7
Griffin Smith <griffin@readyset.io>
2021-11-02 17:04:25 -0400

    ops: Import nighly and default queues into terraform
    Import the cloudformation stack resources for the buildkite default and
    nighly queues into terraform, leaving the former in place for now but
    replacing the latter with an explicit queue named after the size of the
    instances it provisions rather than just the word "nightly", updating
    the nightly pipeline itself to use this for its agent targeting rules.
    
    I considered briefly doing the same thing with the default queue, but
    decided against it as I think the debugging loop of having
    steps (especially dynamically uploaded ones) never run unless they
    explicitly select an agent targeting rule is too obtuse to make it worth
    it.
    
    Change-Id: Ide36c1aea1901d91ef5d359f2352ada15bfafb9e
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1012
    Reviewed-by: Harley Klein <harley@readyset.io>
    Terraform-Plan-Review: Harley Klein <harley@readyset.io>
    Tested-by: Buildkite CI

--- ops/substrate/root-modules/readyset/build/default/us-east-2/buildkite-queues.tf
+++ ops/substrate/root-modules/readyset/build/default/us-east-2/buildkite-queues.tf
@@ -1,6 +1,7 @@
 locals {
   instance_types = [
     "t3a.small",
+    "r5a.2xlarge",
     "c5a.4xlarge",
   ]
 }
@@ -12,6 +13,7 @@ module "buildkite_queue_shared" {
 
   agent_token_allowed_roles = concat(
     tolist(module.buildkite_ops_queue.iam_roles),
+    tolist(module.buildkite_default_queue.iam_roles),
     flatten(values(module.buildkite_queue)[*].iam_roles),
   )
 }
@@ -71,6 +73,19 @@ resource "aws_s3_bucket_public_access_block" "ops-artifacts" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+# Separate module for the default queue, so we can name the queue "default"
+module "buildkite_default_queue" {
+  source          = "../../../../../modules/buildkite-queue/regional"
+  environment     = "build"
+  buildkite_queue = "default"
+  instance_type   = "c5a.2xlarge"
+
+  buildkite_agent_token_parameter_store_path = module.buildkite_queue_shared.buildkite_agent_token_parameter_store_path
+
+  secrets_bucket   = aws_s3_bucket.ops-secrets.bucket
+  artifacts_bucket = aws_s3_bucket.ops-artifacts.bucket
 }
 
 # Separate queue for running the ops pipelines, so that we can give admin permissions to only that role
