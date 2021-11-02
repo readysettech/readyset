11236a2826e0d37a00e1cf6e095d921f54aced9e
Justin Miron <justin@readyset.io>
2021-11-02 07:41:09 +0000

    metrics: Make NoriaMetricsRecorder thread safe.
    Metrics can be recorded from any of the threads in ReadySet.
    The metrics-rs crate itself does not guarantee that that recorders
    are called in a thread-safe maner. Recorder's themselves must
    guarantee this.
    
    https://gerrit.readyset.name/c/readyset/+/957 removed
    BufferedMetricsRecorder which enforced this thread-safety
    via a RwLock over the recorders. This does not apply to the
    PrometheusRecorder which is already thread-safe.
    
    Change-Id: Ic704335cb3b3a1df235ae3d6f85c8d6c5a288f3e
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/991
    Reviewed-by: Vlad Krasnov <vlad@readyset.io>
    Reviewed-by: Griffin Smith <griffin@readyset.io>
    Tested-by: Buildkite CI

--- noria/server/src/metrics/noria_recorder.rs
+++ noria/server/src/metrics/noria_recorder.rs
@@ -1,8 +1,10 @@
-use std::cell::RefCell;
 use std::collections::HashMap;
+use std::sync::Arc;
 
 use metrics::{GaugeValue, Recorder, Unit};
 use metrics_util::Histogram;
+use parking_lot::lock_api::Mutex;
+use parking_lot::RawMutex;
 
 use noria::metrics::{Key, MetricsDump};
 
@@ -11,9 +13,9 @@ use crate::metrics::{Clear, Render};
 /// A simplistic metrics recorder for Noria, which just stores the different metrics.
 #[derive(Default)]
 pub struct NoriaMetricsRecorder {
-    counters: RefCell<HashMap<Key, u64>>,
-    gauges: RefCell<HashMap<Key, f64>>,
-    histograms: RefCell<HashMap<Key, Histogram>>,
+    counters: Arc<Mutex<RawMutex, HashMap<Key, u64>>>,
+    gauges: Arc<Mutex<RawMutex, HashMap<Key, f64>>>,
+    histograms: Arc<Mutex<RawMutex, HashMap<Key, Histogram>>>,
 }
 
 impl NoriaMetricsRecorder {
@@ -47,13 +49,13 @@ impl Recorder for NoriaMetricsRecorder {
     }
 
     fn increment_counter(&self, key: &Key, value: u64) {
-        let mut counters = self.counters.borrow_mut();
+        let mut counters = self.counters.lock();
         let ent = counters.entry(key.clone()).or_default();
         *ent += value;
     }
 
     fn update_gauge(&self, key: &Key, value: GaugeValue) {
-        let mut gauges = self.gauges.borrow_mut();
+        let mut gauges = self.gauges.lock();
         let ent = gauges.entry(key.clone()).or_default();
         match value {
             GaugeValue::Increment(v) => {
@@ -70,7 +72,7 @@ impl Recorder for NoriaMetricsRecorder {
 
     fn record_histogram(&self, key: &Key, value: f64) {
         // TODO(justin): Support different bounds in histogram metrics.
-        let mut histograms = self.histograms.borrow_mut();
+        let mut histograms = self.histograms.lock();
         let ent = histograms.entry(key.clone()).or_insert_with(|| {
             // Define the buckets for the histogram to be power of 2s up to
             // 2^20.
@@ -84,9 +86,9 @@ impl Recorder for NoriaMetricsRecorder {
 impl Render for NoriaMetricsRecorder {
     fn render(&self) -> String {
         let md = MetricsDump::from_metrics(
-            self.counters.borrow().clone(),
-            self.gauges.borrow().clone(),
-            self.histograms.borrow().clone(),
+            self.counters.lock().clone(),
+            self.gauges.lock().clone(),
+            self.histograms.lock().clone(),
         );
         serde_json::to_string(&md).unwrap()
     }
@@ -94,9 +96,9 @@ impl Render for NoriaMetricsRecorder {
 
 impl Clear for NoriaMetricsRecorder {
     fn clear(&self) -> bool {
-        self.counters.borrow_mut().clear();
-        self.gauges.borrow_mut().clear();
-        self.histograms.borrow_mut().clear();
+        self.counters.lock().clear();
+        self.gauges.lock().clear();
+        self.histograms.lock().clear();
         true
     }
 }
