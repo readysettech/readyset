edf7a87d7465439e66b4ae82a1334c618a66482b
Harley Klein <harley@readyset.io>
2021-11-04 18:02:27 -0700

    ops/cfn-test: Document and fix issues with script
    This script was sitting there but undocumented and generally unused.
    
    Added documentation as a comment in the file and fixed issues with the
    most recent version of the CloudFormation templates.
    
    Change-Id: I3cedaccca6f24171f6e18bdab5512da678cc9a52
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1041
    Tested-by: Buildkite CI
    Reviewed-by: Sasha Fox <sasha@readyset.io>
    Reviewed-by: Peter Farr <peter@readyset.io>

--- ops/cfn-test/create-mysql-super-stack.sh
+++ ops/cfn-test/create-mysql-super-stack.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
 
+# Starts the creation of a ReadySet MySQL super stack using CloudFormation.
+
+# This takes one parameter of the AWS EC2 SSH Key Pair name and uses it for
+# access and as the name of the deployment.
+# You can also override which bucket to fetch the templates from using the
+# CFN_BUCKET environment variable.
+# NOTE: This is currently pointed at Harley's taskcat bucket but should be
+# moved elsewhere by default in the future.
+
 CFN_BUCKET=${CFN_BUCKET:-readysettech-tcat-harley-us-east-2}
 AWS_REGION=${AWS_REGION:-us-east-2}
 key_pair_name=$1
@@ -11,15 +20,16 @@ parameters=(
   "ParameterKey=KeyPairName,ParameterValue=${key_pair_name}"
   "ParameterKey=AvailabilityZones,ParameterValue='us-east-2a,us-east-2b,us-east-2c'"
   "ParameterKey=AccessCIDR,ParameterValue=${external_ip_address}/32"
-  "ParameterKey=ReadysetS3BucketName,ParameterValue=${CFN_BUCKET}"
+  "ParameterKey=ReadySetS3BucketName,ParameterValue=${CFN_BUCKET}"
   "ParameterKey=DatabaseUsername,ParameterValue=readyset"
   "ParameterKey=DatabasePassword,ParameterValue=readyset"
   "ParameterKey=DatabaseName,ParameterValue=readyset"
-  "ParameterKey=DeploymentName,ParameterValue=${key_pair_name}"
+  "ParameterKey=ReadySetDeploymentName,ParameterValue=${key_pair_name}"
 )
 
 aws cloudformation create-stack \
   --stack-name "$stack_name" \
   --template-url "https://${CFN_BUCKET}.s3.${AWS_REGION}.amazonaws.com/readyset/templates/readyset-mysql-super-template.yaml" \
   --parameters "${parameters[@]}" \
+  --disable-rollback \
   --capabilities CAPABILITY_IAM
