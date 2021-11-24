0fd014befa8179ab244b674d095ecb6b55459348
Marcus Bastian <marcus@readyset.io>
2021-11-24 11:53:52 -0500

    ops/tailscale: modify tailscale configuration scripts in internal-base
    Extending functionality of existing init script for the Tailscale subnet
    router so that it consumes the auth key from SSM/Secrets Manager and
    accepts a list of advertisable routes via an environment variable.
    
    Change-Id: I502a2afc577044b3a667d4230e53ba4636e8b3fa
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1226
    Tested-by: Buildkite CI
    Reviewed-by: Harley Klein <harley@readyset.io>

--- ops/images/provisioners/files/internal-base/usr_local_bin_tailscale-autoconnect.sh
+++ ops/images/provisioners/files/internal-base/usr_local_bin_tailscale-autoconnect.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Borrowed from https://tailscale.com/kb/1096/nixos-minecraft/
 
@@ -11,6 +11,27 @@ if [ "$status" = "Running" ]; then # if so, then do nothing
     exit 0
 fi
 
-# TODO: Make tailscale advertise route value configurable
+if [ -z "${AUTH_KEY_SECRETS_MANAGER_ARN}" ]; then
+    echo "Value not supplied to required environment variable: AUTH_KEY_SECRETS_MANAGER_ARN. Exiting with failure."
+    exit 1
+elif [ -z "${TAILSCALE_ROUTES_ADVERTISED}" ]; then
+    echo "Value not supplied to required environment variable: TAILSCALE_ROUTES_ADVERTISED. Exiting with failure."
+    exit 1
+fi
+
+# Retrieve Tailscale auth key from SSM/Secrets Manager
+AUTH_KEY_VALUE=$(aws ssm get-parameter --name ${AUTH_KEY_SECRETS_MANAGER_ARN} --with-decryption --output text --query Parameter.Value)
+
+ec=$?
+if [ $ec -ne 0 ]; then
+    echo "Failed to retrieve auth key from secret: ${AUTH_KEY_SECRETS_MANAGER_ARN}. Exit code: ${ec}"
+    exit 1
+elif [ -z "${AUTH_KEY_VALUE}" ]; then
+    echo "Unexpected value retrieved from secret: ${AUTH_KEY_SECRETS_MANAGER_ARN}. Check the value to be sure it's as expected. Exiting with failure."
+    exit 1
+fi
+
+echo "Starting up Tailscale service."
 /usr/bin/tailscale up \
--authkey "$(aws ssm get-parameter --name /aws/reference/secretsmanager/tailscale/AUTHKEY --with-decryption --output text --query Parameter.Value)"
+--advertise-routes ${TAILSCALE_ROUTES_ADVERTISED} \
+--authkey ${AUTH_KEY_VALUE}
