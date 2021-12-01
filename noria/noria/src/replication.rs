9084a7ba27d1c91c66fdd1c296bc09487406cdb2
Griffin Smith <griffin@readyset.io>
2021-12-01 18:07:09 -0500

    replicators: Allow mixed replication offsets
    If at startup we detect that we've successfully snapshotted some
    tables (aka they have a non-None replication offset) but not others,
    we'd *like* to be able to re-snapshot only the ones that haven't
    successfully been snapshotted yet. The issue is that then we'd have some
    tables that have correct data as of one replication offset (the ones
    that were earlier successfully snapshotted), but other tables that have
    correct data as of a *later* replication offset (the ones we just
    re-snapshotted). If we get in this situation, we can work around this by
    starting replication at the *older* replication offset, and then
    discarding replication events that we receive for either individual
    tables or the DDL that are *before* the recorded replication offset we
    have for that object.
    
    In the case where we've fully snapshot, though, we can do what we used
    to do and just start at the maximum of all replication offsets, since we
    know that we've fully replicated all events up to that point.
    
    Under the hood, we're now querying the `replication_offsets()` RPC
    endpoint rather than the singular `replication_offset()` endpoint, and
    storing the set of all replication offsets as a field of the
    replicators::NoriaAdapter
    
    Refs: ENG-854
    Change-Id: Ia118d9cd538a36ed1546e855a84d1e63bab753fb
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1258
    Tested-by: Buildkite CI
    Reviewed-by: Vlad Krasnov <vlad@readyset.io>

--- noria/noria/src/replication.rs
+++ noria/noria/src/replication.rs
@@ -1,6 +1,6 @@
 //! Data types for implementing snapshot and streaming replication from an upstream database.
 
-use std::cmp::Ordering;
+use std::cmp::{min_by_key, Ordering};
 use std::collections::HashMap;
 
 use noria_errors::{ReadySetError, ReadySetResult};
@@ -81,6 +81,193 @@ impl ReplicationOffsets {
         Self {
             schema,
             tables: HashMap::new(),
+        }
+    }
+
+    /// Returns `true` if self has an offset for the schema
+    pub fn has_schema(&self) -> bool {
+        self.schema.is_some()
+    }
+
+    /// If all replication offsets are present (the schema and all tables), returns the maximum of
+    /// all replication offsets, from which streaming replication can successfully continue.
+    /// Otherwise, returns `Ok(None)`.
+    ///
+    /// If all replication offsets are present but any have mismatched [`replication_log_name`]s,
+    /// returns an error.
+    ///
+    /// [`replication_log_name`]: ReplicationOffset::replication_log_name
+    pub fn max_offset(&self) -> ReadySetResult<Option<&ReplicationOffset>> {
+        let mut res = match &self.schema {
+            Some(schema_offset) => schema_offset,
+            None => return Ok(None),
+        };
+        for offset in self.tables.values() {
+            let offset = match offset {
+                Some(offset) => offset,
+                None => return Ok(None),
+            };
+            if res.replication_log_name != offset.replication_log_name {
+                return Err(ReadySetError::ReplicationOffsetLogDifferent(
+                    res.replication_log_name.clone(),
+                    offset.replication_log_name.clone(),
+                ));
+            }
+            if offset.offset > res.offset {
+                res = offset;
+            }
+        }
+        Ok(Some(res))
+    }
+
+    /// Returns the minimum offset *from those present* within the set of replication offsets.
+    ///
+    /// If no offset is present _at all_, returns [`None`] (but note that unlike [`max_offset`][]
+    /// this function does *not* return [`None`] if an offset is absent).
+    ///
+    /// If any offsets have a different [`replication_log_name`], returns an error.
+    ///
+    /// [`max_offset`]: Self::max_offset
+    /// [`replication_log_name`]: ReplicationOffset::replication_log_name
+    pub fn min_present_offset(&self) -> ReadySetResult<Option<&ReplicationOffset>> {
+        let mut res: Option<&ReplicationOffset> = None;
+        for offset in self.schema.iter().chain(self.tables.values().flatten()) {
+            match (res, offset) {
+                (Some(off1), off2) if off1.replication_log_name != off2.replication_log_name => {
+                    return Err(ReadySetError::ReplicationOffsetLogDifferent(
+                        off1.replication_log_name.clone(),
+                        off2.replication_log_name.clone(),
+                    ));
+                }
+                (Some(off1), off2) => {
+                    res = Some(min_by_key(off1, off2, |off| off.offset));
+                }
+                (None, off) => {
+                    res = Some(off);
+                }
+            }
+        }
+        Ok(res)
+    }
+
+    /// Set the replication offset for the schema and all tables to the given offset
+    pub fn set_offset(&mut self, offset: ReplicationOffset) {
+        for table_offset in self.tables.values_mut() {
+            *table_offset = Some(offset.clone());
+        }
+        self.schema = Some(offset);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod max_offset {
+        use super::*;
+
+        #[test]
+        fn all_present_and_matching() {
+            let offsets = ReplicationOffsets {
+                schema: Some(ReplicationOffset {
+                    offset: 1,
+                    replication_log_name: "test".to_owned(),
+                }),
+                tables: HashMap::from([
+                    (
+                        "t1".to_owned(),
+                        Some(ReplicationOffset {
+                            offset: 2,
+                            replication_log_name: "test".to_owned(),
+                        }),
+                    ),
+                    (
+                        "t2".to_owned(),
+                        Some(ReplicationOffset {
+                            offset: 3,
+                            replication_log_name: "test".to_owned(),
+                        }),
+                    ),
+                ]),
+            };
+            let res = offsets.max_offset().unwrap().unwrap();
+            assert_eq!(res.replication_log_name, "test");
+            assert_eq!(res.offset, 3);
+        }
+
+        #[test]
+        fn all_present_not_matching() {
+            let offsets = ReplicationOffsets {
+                schema: Some(ReplicationOffset {
+                    offset: 1,
+                    replication_log_name: "binlog".to_owned(),
+                }),
+                tables: HashMap::from([
+                    (
+                        "t1".to_owned(),
+                        Some(ReplicationOffset {
+                            offset: 2,
+                            replication_log_name: "test".to_owned(),
+                        }),
+                    ),
+                    (
+                        "t2".to_owned(),
+                        Some(ReplicationOffset {
+                            offset: 3,
+                            replication_log_name: "test".to_owned(),
+                        }),
+                    ),
+                ]),
+            };
+            let res = offsets.max_offset();
+            assert!(res.is_err());
+        }
+
+        #[test]
+        fn schema_missing() {
+            let offsets = ReplicationOffsets {
+                schema: None,
+                tables: HashMap::from([
+                    (
+                        "t1".to_owned(),
+                        Some(ReplicationOffset {
+                            offset: 2,
+                            replication_log_name: "test".to_owned(),
+                        }),
+                    ),
+                    (
+                        "t2".to_owned(),
+                        Some(ReplicationOffset {
+                            offset: 3,
+                            replication_log_name: "test".to_owned(),
+                        }),
+                    ),
+                ]),
+            };
+            let res = offsets.max_offset().unwrap();
+            assert!(res.is_none());
+        }
+
+        #[test]
+        fn table_missing() {
+            let offsets = ReplicationOffsets {
+                schema: Some(ReplicationOffset {
+                    offset: 1,
+                    replication_log_name: "test".to_owned(),
+                }),
+                tables: HashMap::from([
+                    (
+                        "t1".to_owned(),
+                        Some(ReplicationOffset {
+                            offset: 2,
+                            replication_log_name: "test".to_owned(),
+                        }),
+                    ),
+                    ("t2".to_owned(), None),
+                ]),
+            };
+            let res = offsets.max_offset().unwrap();
+            assert!(res.is_none());
         }
     }
 }
