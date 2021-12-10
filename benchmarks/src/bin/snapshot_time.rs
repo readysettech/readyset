597862afe9b39f268046d7f2047f7977a92f2ff6
Vlad Krasnov <vlad@readyset.io>
2021-12-10 09:56:26 -0500

    Add a snapshot benchmark
    The snapshot benchmark measures the time it takes
    to perform a snapshot of a given database.
    
    It reports the time to finish the snapshot and the
    amount of disk space used.
    
    Optionally it can also extend the schema after the
    snapshot and report how long the migration to the
    new schema took, and how much disk space is used
    following the migration.
    
    Example of output:
    
    Snapshot time:     32 s
    Tables replicated: 10
    Disk space used:   246.71 MiB
    Migration time:    4 s
    Disk space used:   349.21 MiB
    
    Change-Id: I59e15c3ec05ca57ba71824bbd2b5d96ad31860d4
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1353
    Tested-by: Buildkite CI
    Reviewed-by: Justin Miron <justin@readyset.io>

---
+++ benchmarks/src/bin/snapshot_time.rs
@@ -0,0 +1,91 @@
+use std::path::PathBuf;
+use std::time::Instant;
+
+use clap::{Parser, ValueHint};
+use noria::metrics::{recorded, MetricsDump};
+use noria_server::metrics::{
+    get_global_recorder, install_global_recorder, CompositeMetricsRecorder, MetricsRecorder,
+    RecorderType,
+};
+use noria_server::NoriaMetricsRecorder;
+
+#[derive(Parser)]
+#[clap(name = "snapshot_time")]
+struct SnapshotBenchmark {
+    /// Sets the MySQL database URL for the snapshot
+    #[clap(short, long, value_hint = ValueHint::Url)]
+    replication_url: String,
+    /// How many times to repeat the benchmark
+    #[clap(short, long, default_value = "1")]
+    iterations: usize,
+    /// An optional SQL file with queries to install and measure the time required
+    /// for this
+    #[clap(short, long)]
+    queries: Option<PathBuf>,
+}
+
+impl SnapshotBenchmark {
+    async fn run(self) -> anyhow::Result<()> {
+        readyset_logging::init_test_logging();
+        init_metrics_recorder();
+
+        for _ in 0..self.iterations {
+            let mut builder = noria_server::Builder::for_tests();
+            let persistence = noria_server::PersistenceParameters {
+                mode: noria_server::DurabilityMode::DeleteOnExit,
+                ..Default::default()
+            };
+            builder.set_persistence(persistence);
+            builder.set_replicator_url(self.replication_url.clone());
+
+            let start = Instant::now();
+            let mut noria = builder.start_local().await?;
+            noria.backend_ready().await;
+
+            println!("Snapshot time:     {} s", start.elapsed().as_secs());
+
+            println!("Tables replicated: {}", noria.inputs().await?.len());
+
+            println!("Disk space used:   {:.2} MiB", tables_size_metric_mib());
+
+            if let Some(queries) = &self.queries {
+                let queries_sql = std::fs::read_to_string(queries)?;
+                let start = Instant::now();
+                noria.extend_recipe(&queries_sql).await?;
+                println!("Migration time:    {} s", start.elapsed().as_secs());
+                println!("Disk space used:   {:.2} MiB", tables_size_metric_mib());
+            }
+
+            noria.shutdown();
+            noria.wait_done().await;
+        }
+
+        Ok(())
+    }
+}
+
+fn init_metrics_recorder() {
+    let rec = CompositeMetricsRecorder::with_recorders(vec![MetricsRecorder::Noria(
+        NoriaMetricsRecorder::new(),
+    )]);
+    unsafe { install_global_recorder(rec).unwrap() };
+}
+
+fn tables_size_metric_mib() -> f64 {
+    let metrics_handle = get_global_recorder();
+
+    let metrics: MetricsDump =
+        serde_json::from_str(&metrics_handle.render(RecorderType::Noria).unwrap()).unwrap();
+
+    let base_sizes = &metrics.metrics[recorded::DOMAIN_ESTIMATED_BASE_TABLE_SIZE_BYTES];
+
+    let byte_size: f64 = base_sizes.iter().map(|s| s.value.value().unwrap()).sum();
+
+    byte_size / 1024. / 1024.
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let bench = SnapshotBenchmark::parse();
+    bench.run().await
+}
