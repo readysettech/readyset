bf9811fc1a5c87190897f2e081771fd58aaf3434
Justin Miron <justin@readyset.io>
2021-12-13 20:55:35 +0000

    deployment test: Add script to build/copy/package binaries and data files.
    Adds `scripts/package-deployment-tests.sh`. This builds the binaries used
    for testing and benchmarking ReadySet in a cloudformation deployment.
    
    Change-Id: I50dea851e1eb865818bc172a3f3443e0b4d1fe53
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1381
    Tested-by: Buildkite CI
    Reviewed-by: Sasha Fox <sasha@readyset.io>

---
+++ scripts/package-deployment-tests.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Packages the set of files and binaires required to test ReadySet on a
+# deployment. Creates deployment_test.tar.gz.
+#
+# ./package-deployment-tests.sh <readyset_root_directory>
+#  readyset_root_directory: The root directory of the monorepo.
+
+# Must be run from the readyset root directory.
+READYSET="./"
+if [ "$#" -eq 1 ]; then
+    READYSET=$1
+fi
+
+# Go to the root directory.
+cd $READYSET
+
+# Set of binaries that we want to be included in the testing package.
+binaries=(benchmarks irl_minimal verify_prometheus_metrics basic_validation_test data_generator)
+
+# Add each of the binaries above to the cargo build cmd. We use docker to build
+# the binaries for our cloudformation deployment's os.
+build_cmd="./build/ubuntu20.04/cargo-ubuntu2004 build --release"
+for binary in "${binaries[@]}"; do
+  build_cmd+=" --bin $binary"
+done
+
+# Run the build command. This creates binaries in ./target-ubuntu2004/release
+$build_cmd
+
+# Create a directory where we will copy relevant files to package.
+TMP_DIR=tmp-package-deployment-tests
+mkdir $TMP_DIR
+
+# Binaries!
+for binary in "${binaries[@]}"; do
+  cp ./target-ubuntu2004/release/$binary $TMP_DIR
+done
+
+# Directory structure needs to match monorepo since .yaml files use the
+# path in the monorepo.
+DATA_DIR_PATH=$TMP_DIR/src/data/irl
+mkdir -p $DATA_DIR_PATH
+cp benchmarks/src/data/irl/* $DATA_DIR_PATH
+cp benchmarks/src/yaml/benchmarks/read_benchmark_irl_small.yaml $TMP_DIR
+cp benchmarks/src/yaml/deployments/example.yaml $TMP_DIR/deployment.example.yaml
+
+tar -cvf deployment_test.tar.gz -C $TMP_DIR .
+rm -r $TMP_DIR
