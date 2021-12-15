e0e9d84aadbfd9311d9f2b65d5cc91cc21fa2a7b
Justin Miron <justin@readyset.io>
2021-12-15 19:54:41 +0000

    failpoints: Macros for setting failpoints.
    This introduces two macros:
      * An attribute macro, `#[failpoint("fail-point-name")]`
        macro which can be used to create a failpoint at the start of a
        function instead.
      * A declarative macro, set_failpoint!("fail-point-name"), that writes
        out both the feature check and the failpoint line.
    
    Both of these replace writing out the following:
    
    ```
    \#[cfg(feature = "failure_injection)]
    fail::fail_point("fail-point-name");
    ```
    
    This should reduce the visual impact of introducing failpoints to
    our code base.
    
    Change-Id: I503d3f99d272162bdae571132aa1fd11c72d9bce
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1412
    Tested-by: Buildkite CI
    Reviewed-by: Griffin Smith <griffin@readyset.io>

---
+++ failpoint-macros/src/lib.rs
@@ -0,0 +1,42 @@
+pub use failpoint_proc_macros::failpoint;
+
+/// Creates a [`fail::fail_point`] which can be used to programatically
+/// introduce failures from tests. This macro wraps [`fail::fail_point`]
+/// with our `failure_injection` feature.
+///
+/// Example: Creating a simple failpoint named `rpc-failed`. This failpoint
+/// can not return values using the `return` action.
+/// ```rust
+/// use failpoint_macros::set_failpoint;
+/// set_failpoint!("rpc-failed");
+/// ```
+///
+/// Example: Creating a failpoint that may return a ReadySet error when
+/// passed the `return` action.
+/// ```rust
+/// use failpoint_macros::set_failpoint;
+/// set_failpoint!("rpc-failed", |_| ReadySetError::Internal("failpoint"));
+/// ```
+///
+/// Example: Creating a failpoint that can return values and be conditioned
+/// on the failpoint enabling + a boolean passed in as an argument.
+/// ```rust
+/// use failpoint_macros::set_failpoint;
+/// let is_leader_rpc = false;
+/// set_failpoint!("rpc-failed", is_leader_rpc, |_| ReadySetError::Internal("thrown from failpoint"));
+/// ```
+#[macro_export]
+macro_rules! set_failpoint {
+    ($name:expr) => {{
+        #[cfg(feature = "failure_injection")]
+        fail::fail_point!($name);
+    }};
+    ($name:expr, $e:expr) => {{
+        #[cfg(feature = "failure_injection")]
+        fail::fail_point!($name, $e);
+    }};
+    ($name:expr, $cond:expr, $e:expr) => {{
+        #[cfg(feature = "failure_injection")]
+        fail::fail_point!($name, $cond, $e);
+    }};
+}
