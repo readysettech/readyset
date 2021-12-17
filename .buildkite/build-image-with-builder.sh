027f145172f82f285ffcd3246c507d927ad94d38
Harley Klein <harley@readyset.io>
2021-12-17 12:00:54 -0800

    build: Use builder stage instead of cargo chef
    cargo-chef does not actually speed up the build in a way that we want as
    it only simplifies downloading dependencies. What actually speeds up
    our builds more is things like using cachepot to cache compilation
    results.
    
    This creates a new image which is a base image for builders to use. We
    could eventually in the future integrate in things like cargo-chef again
    at some point on top of it.
    
    The psql image has also been broken for a bit so this fixes that in the
    process of updating everything else.
    
    Change-Id: I37c1c6edc8772a7956e1cf80518839d2b05edee6
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1435
    Tested-by: Buildkite CI
    Reviewed-by: Ryan Cerniglia <ryan@readyset.io>
    Reviewed-by: Sasha Fox <sasha@readyset.io>

---
+++ .buildkite/build-image-with-builder.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# This script is used by Buildkite to standardize a few things about how we
+# build Docker containers that contain Readyset code.
+# The script ensures the following things:
+# Docker Buildkit is used for better performance and caching
+# Environment variables to help with compile caching are set appropriately
+# Builds are uploaded to our private AWS Elastic Container Registry
+# When builds are done on main, they are tagged as latest to help with caching.
+
+dockerfile="$1"
+image_name="$2"
+context="$3"
+
+if [ -z "$dockerfile" ] || [ -z "$image_name" ]; then
+    echo "Usage: $0 <dockerfile> <tag> <context> [<docker-build-args>...]" >&2
+    exit 1
+fi
+shift 3
+
+BUILDKITE_BRANCH=${BUILDKITE_BRANCH:-}
+BUILDKITE_COMMIT=${BUILDKITE_COMMIT:-}
+AWS_ACCOUNT=${AWS_ACCOUNT:-305232526136} # build
+AWS_REGION=${AWS_REGION:-us-east-2}
+
+CACHEPOT_BUCKET=${CACHEPOT_BUCKET:-readysettech-build-sccache-us-east-2}
+CACHEPOT_REGION=${CACHEPOT_REGION:-${AWS_REGION}}
+
+VERSION=${VERSION:-$BUILDKITE_COMMIT}
+
+docker_repo="$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com"
+image="$docker_repo/$image_name"
+
+echo "--- :docker: Pulling $image:latest"
+if docker pull "$image:latest"; then
+    cache_from="--cache-from=$image:latest"
+else
+    echo "Failed to pull previous build of image"
+    cache_from=""
+fi
+
+if [ "$BUILDKITE_BRANCH" = "refs/heads/main" ]; then
+    release_buildarg="--build-arg release=1"
+else
+    release_buildarg=""
+fi
+
+build_cmd_prefix=(
+    "docker" "build" \
+    "-f" "$dockerfile" \
+    "-t" "$image:$VERSION" \
+    "--build-arg" "BUILDKIT_INLINE_CACHE=1" \
+    "--build-arg" "CACHEPOT_BUCKET"
+    "--build-arg" "CACHEPOT_REGION"
+    "--build-arg" "READYSET_RUST_UBUNTU2004_BUILDER_BASE_TAG=${VERSION}"
+)
+
+build_cmd_suffix=(
+    "$@" \
+    "$context"
+)
+if [ -n "$cache_from" ] &&  [ -n "$release_buildarg" ]; then
+    build_cmd=("${build_cmd_prefix[@]}" "$release_buildarg" "$cache_from" "${build_cmd_suffix[@]}")
+elif [ -n "$cache_from" ]; then
+    build_cmd=("${build_cmd_prefix[@]}" "$cache_from" "${build_cmd_suffix[@]}")
+elif [ -n "$release_buildarg" ]; then
+    build_cmd=("${build_cmd_prefix[@]}" "$release_buildarg" "${build_cmd_suffix[@]}")
+else
+    build_cmd=("${build_cmd_prefix[@]}" "${build_cmd_suffix[@]}")
+fi
+
+echo "+++ :docker: Building $image:$VERSION"
+DOCKER_BUILDKIT=1 "${build_cmd[@]}"
+tags_to_push=("$image:$VERSION")
+
+if [ "$BUILDKITE_BRANCH" = "refs/heads/main" ]; then
+    docker tag "$image:$VERSION" "$image:latest"
+    tags_to_push+=("$image:latest")
+    docker tag "$image:$VERSION" "$image:release-${BUILDKITE_COMMIT}"
+    tags_to_push+=("$image:release-${BUILDKITE_COMMIT}")
+fi
+
+for tag in "${tags_to_push[@]}"; do
+    echo "--- :docker: Pushing $tag"
+    docker push "$tag"
+done
--- .buildkite/build-image.sh
+++ .buildkite/build-image.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# This script is used by Buildkite to standardize a few things about how we
+# build Docker containers that are used for other steps in Buildkite.
+# The script ensures the following things:
+# Docker Buildkit is used for better performance and caching
+# Builds are uploaded to our private AWS Elastic Container Registry
+# When builds are done on main, they are tagged as latest to help with caching.
+
+
 dockerfile="$1"
 image_name="$2"
+context="$3"
 
 if [ -z "$dockerfile" ] || [ -z "$image_name" ]; then
-    echo "Usage: $0 <dockerfile> <image_name> [<docker-build-args>...]" >&2
+    echo "Usage: $0 <dockerfile> <image_name> <context> [<docker-build-args>...]" >&2
     exit 1
 fi
-shift 2
+shift 3
 
 AWS_ACCOUNT=${AWS_ACCOUNT:-305232526136} # build
 AWS_REGION=${AWS_REGION:-us-east-2}
@@ -33,7 +42,7 @@ build_cmd_prefix=(
 )
 build_cmd_suffix=(
     "$@" \
-    "."
+    "$context"
 )
 if [ -n "$cache_from" ]; then
     build_cmd=("${build_cmd_prefix[@]}" "$cache_from" "${build_cmd_suffix[@]}")
@@ -43,11 +52,16 @@ fi
 
 echo "+++ :docker: Building $image:$VERSION"
 DOCKER_BUILDKIT=1 "${build_cmd[@]}"
+tags_to_push=("$image:$VERSION")
 
 if [ "$BUILDKITE_BRANCH" = "refs/heads/main" ]; then
     docker tag "$image:$VERSION" "$image:latest"
+    tags_to_push+=("$image:latest")
     docker tag "$image:$VERSION" "$image:release-${BUILDKITE_COMMIT}"
+    tags_to_push+=("$image:release-${BUILDKITE_COMMIT}")
 fi
 
-echo "--- :docker: Pushing $image"
-docker push --all-tags "$image"
+for tag in "${tags_to_push[@]}"; do
+    echo "--- :docker: Pushing $tag"
+    docker push "$tag"
+done
