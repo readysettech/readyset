6d34f218d9970a7f5e8c681c996bc88a3e8f22d4
Mike Cronce <mc@readyset.io>
2021-12-30 16:12:21 -0500

    + scripts/aba-benchmark.sh and docker-compose overrides for local benchmarking
    This provides a single entrypoint that handles running the benchmark
    suite on two commits (twice on the first, once on the second - so-called
    "A-B-A testing").  It does everything except clone the monorepo.
    
    From start to finish, it ensures the monitoring stack is running; then
    for A, B, and A again, it checks out the commit, builds the codebase,
    starts Zookeeper/Consul/Redis/fallback using docker-compose, starts
    noria-server and noria-mysql, runs the benchmarks, and finally tears
    down Noria and the support stack.
    
    Change-Id: Ia73b0168a51e9cd0be05927b37d992662bb08dc8
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1530
    Tested-by: Buildkite CI
    Reviewed-by: Justin Miron <justin@readyset.io>

---
+++ scripts/aba-benchmark.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -exo pipefail
+
+DIR="$(dirname "${0}")/.."
+
+function ensure_monitoring_stack() {
+    docker-compose -f "${DIR}/docker/monitoring/docker-compose.yml" -f "${DIR}/benchmarks/docker-compose.monitoring.override.yml" up -d
+}
+
+function background() {
+    $@ &>/dev/null &
+    echo $!
+}
+
+function run_noria() {
+    background "${DIR}/target/release/noria-server" --deployment localbench --prometheus-metrics --noria-metrics --replication-url=mysql://root:noria@localhost:3306/noria
+}
+
+function run_adapter() {
+    background "${DIR}/target/release/noria-mysql" --deployment localbench --prometheus-metrics --upstream-db-url=mysql://root:noria@localhost:3306/noria --allow-unauthenticated-connections --address 0.0.0.0:3307
+}
+
+function setup() {
+    docker-compose -f "${DIR}/docker-compose.yml" -f "${DIR}/benchmarks/docker-compose.override.yml" up -d
+    cargo build --release --bin noria-server --bin noria-mysql & sleep 10; wait
+    exec 3< <(run_noria)
+    exec 4< <(run_adapter)
+    read <&3 noria_pid
+    read <&4 adapter_pid
+    echo "$noria_pid $adapter_pid"
+    until mysqladmin -h127.0.0.1 -P3307 ping &>/dev/null; do
+        sleep 1
+    done
+}
+
+function run_benchmarks() {
+    export RDS_HOST=127.0.0.1
+    export RDS_PORT=3306
+    export RDS_USER=root
+    export RDS_PASS=noria
+    export RDS_DATABASE=noria
+    export READYSET_ADAPTER=mysql://noria@localhost:3307/noria
+    export PROMETHEUS_ADDRESS=http://localhost:9091
+    export INSTANCE_LABEL="${1}"
+    "${DIR}/scripts/run-benchmark.sh"
+}
+
+function teardown() {
+    kill "$1" "$2"
+    docker-compose -f "${DIR}/docker-compose.yml" -f "${DIR}/benchmarks/docker-compose.override.yml" down -v
+    rm -Rf localbench-*
+}
+
+if [ "$#" -ne 2 ]; then
+    echo "usage: ${0} a_committish b_committish"
+    echo
+    echo 'Performs A-B-A benchmark comparison using the following workflow:'
+    echo '  1. Ensure monitoring stack is up (Prometheus, etc)'
+    echo '  2. Checkout a_committish'
+    echo '  3. Build the codebase and start a Noria cluster'
+    echo '  4. Run benchmarks, tagging metrics with a_committish'
+    echo '  5. Checkout b_committish'
+    echo '  6. Build the codebase and start a Noria cluster'
+    echo '  7. Run benchmarks, tagging metrics with b_committish'
+    echo '  8. Checkout a_committish'
+    echo '  9. Build the codebase and start a Noria cluster'
+    echo ' 10. Run benchmarks, tagging metrics with a_committish'
+    echo ' 11. Direct you to use your human eyes and brain to view and interpret metrics'
+fi
+
+ensure_monitoring_stack
+
+echo '--- A1'
+git checkout "${1}"
+pids="$(setup)"
+run_benchmarks "${1}-1"
+teardown $pids
+
+echo '--- B'
+git checkout "${2}"
+pids="$(setup)"
+run_benchmarks "${2}"
+teardown $pids
+
+echo '--- A2'
+git checkout "${1}"
+pids="$(setup)"
+run_benchmarks "${1}-2"
+teardown $pids
+
+echo
+echo 'Benchmarks complete.  You can now load up Grafana on http://localhost:3000/ and explore metrics.'
+
