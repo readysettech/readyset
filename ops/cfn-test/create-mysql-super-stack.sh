83c227caf460f75f1585d2f88d1a546346637952
Harley Klein <harley@readyset.io>
2022-01-05 12:29:02 -0800

    cfn: Template Bastion AMI IDs
    This also adds support for fetching the substacks from arbitrary buckets
    and prefixes to make testing easier.
    
    The test script has been updated to use this feature.
    
    Change-Id: I13bfc951140eab3d84890369038d501375045b57
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1560
    Tested-by: Buildkite CI
    Reviewed-by: Marcus Bastian <marcus@readyset.io>

--- ops/cfn-test/create-mysql-super-stack.sh
+++ ops/cfn-test/create-mysql-super-stack.sh
@@ -10,27 +10,29 @@ set -eux -o pipefail
 # NOTE: This is currently pointed at Harley's taskcat bucket but should be
 # moved elsewhere by default in the future.
 
-CFN_BUCKET=${CFN_BUCKET:-readysettech-tcat-harley-us-east-2}
+CFN_BUCKET=${CFN_BUCKET:-readysettech-cfn-internal-us-east-2}
 AWS_REGION=${AWS_REGION:-us-east-2}
-key_pair_name=$1
+commit=$1
+key_pair_name=$2
 external_ip_address=$(curl ifconfig.me)
-stack_name=${2:-${key_pair_name}-super}
+stack_name=${3:-${key_pair_name}-${commit}-super}
 
 parameters=(
   "ParameterKey=KeyPairName,ParameterValue=${key_pair_name}"
   "ParameterKey=AvailabilityZones,ParameterValue='us-east-2a,us-east-2b,us-east-2c'"
   "ParameterKey=AccessCIDR,ParameterValue=${external_ip_address}/32"
   "ParameterKey=ReadySetS3BucketName,ParameterValue=${CFN_BUCKET}"
+  "ParameterKey=ReadySetS3KeyPrefix,ParameterValue=/${commit}/readyset/"
   "ParameterKey=DatabaseUsername,ParameterValue=readyset"
   "ParameterKey=DatabaseName,ParameterValue=readyset"
-  "ParameterKey=ReadySetDeploymentName,ParameterValue=${key_pair_name}"
+  "ParameterKey=ReadySetDeploymentName,ParameterValue=${stack_name}"
   "ParameterKey=SSMParameterKmsKeyArn,ParameterValue=arn:aws:kms:us-east-2:069491470376:key/5cb3afeb-e9dd-40df-98cf-a82bb53ee78b"
   "ParameterKey=SSMPathRDSDatabasePassword,ParameterValue=/readyset/sandbox/dbPassword"
 )
 
 aws cloudformation create-stack \
   --stack-name "$stack_name" \
-  --template-url "https://${CFN_BUCKET}.s3.${AWS_REGION}.amazonaws.com/readyset/templates/readyset-mysql-super-template.yaml" \
+  --template-url "https://${CFN_BUCKET}.s3.${AWS_REGION}.amazonaws.com/${commit}/readyset/templates/readyset-mysql-super-template.yaml" \
   --parameters "${parameters[@]}" \
   --disable-rollback \
   --capabilities CAPABILITY_IAM
