a88715c4f48b8962915d44100ecdb1d1b8a77a78
Marcus Bastian <marcus@readyset.io>
2022-01-06 11:35:30 -0500

    ops/bastion: disable fstab customization logic in bastion packer config
    It looks like we may be clobbering the fstab file, vs appending to it in
    our current Packer Bastion build logic. This appears to be causing a
    plethora of services to no longer start, as the file system is
    read-only.
    
    When EC2 instances init, fstab contains the following:
    
    LABEL=cloudimg-rootfs /  ext4 defaults,discard  0 1
    
    As a follow up CL, we can work on making our additions play well with
    the existing config. I believe we just ported over the previous Bastion
    host's logic from here, but may have missed something while integrating
    it:
    
    https: //bit.ly/3F8449a
    Change-Id: I48eaa0e638ce494d1e4c2de876f1ed5e9e635c86
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1569
    Reviewed-by: Harley Klein <harley@readyset.io>
    Tested-by: Buildkite CI

--- ops/images/provisioners/scripts/readyset-bastion/10-aws.sh
+++ ops/images/provisioners/scripts/readyset-bastion/10-aws.sh
@@ -5,12 +5,11 @@ sudo install -o root -g root -m 755 \
     /tmp/readyset-bastion/aws/usr_local_bin_user-data-init.sh \
     /usr/local/bin/user-data-init.sh
 
-sudo install -o root -g root -m 744 \
-    /tmp/readyset-bastion/aws/etc_fstab \
-    /etc/fstab
+# TODO: uncomment after we mitigate issue w/ initial contents being wiped
+# sudo install -o root -g root -m 744 \
+#     /tmp/readyset-bastion/aws/etc_fstab \
+#     /etc/fstab
 
-
-
-sudo mount -o remount,rw,hidepid=2 /proc
-sudo awk '!/proc/' /etc/fstab | sudo tee temp && sudo mv temp /etc/fstab
-echo "proc /proc proc defaults,hidepid=2 0 0" >> sudo tee -a /etc/fstab
+# sudo mount -o remount,rw,hidepid=2 /proc
+# sudo awk '!/proc/' /etc/fstab | sudo tee temp && sudo mv temp /etc/fstab
+# echo "proc /proc proc defaults,hidepid=2 0 0" >> sudo tee -a /etc/fstab
