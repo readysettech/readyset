07da68c633c77f59233a75fdf6d3b1fc0615413c
Harley Klein <harley@readyset.io>
2022-01-11 08:12:22 -0800

    ops/substrate: More permissions for internal cfn
    This policy gives read only access to the sandbox and build accounts
    for the bucket we store CloudFormation templates generated in CI.
    
    Change-Id: I159892e2cb2b7f631841f39176d3bb53166ecb77
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1616
    Tested-by: Buildkite CI
    Reviewed-by: Marcus Bastian <marcus@readyset.io>
    Terraform-Plan-Review: Marcus Bastian <marcus@readyset.io>

--- ops/substrate/modules/deploy-internal-cfn-template/regional/s3.tf
+++ ops/substrate/modules/deploy-internal-cfn-template/regional/s3.tf
@@ -29,6 +29,29 @@ data "aws_iam_policy_document" "readysettech-cfn-internal-bucket-policy" {
   }
 
   statement {
+    sid    = ""
+    effect = "Allow"
+    resources = [
+      local.readysettech_cfn_internal_bucket_arn,
+      local.readysettech_cfn_internal_objects_arn
+    ]
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket"
+    ]
+    principals {
+      # The principal with the ending of :root means anything in the account has
+      # access if the IAM has access otherwise. This is documented at
+      # https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_principal.html
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::069491470376:root", # Sandbox account
+        "arn:aws:iam::305232526136:root", # Build account
+      ]
+    }
+  }
+
+  statement {
     sid     = "AllowTLSRequestsOnly"
     actions = ["s3:*"]
     effect  = "Deny"
@@ -83,10 +106,10 @@ resource "aws_s3_bucket_public_access_block" "readysettech-cfn-internal" {
 
 output "readysettech_cfn_internal_bucket_arn" {
   value = local.readysettech_cfn_internal_bucket_arn
-#  description = "ARN of the Readyset Internal CFN Bucket in region ${data.aws_region.current.name}"
+  #  description = "ARN of the Readyset Internal CFN Bucket in region ${data.aws_region.current.name}"
 }
 
 output "readysettech_cfn_internal_objects_arn" {
   value = local.readysettech_cfn_internal_objects_arn
-#  description = "ARN of the Readyset Internal CFN Bucket Object in region ${data.aws_region.current.name}"
+  #  description = "ARN of the Readyset Internal CFN Bucket Object in region ${data.aws_region.current.name}"
 }
