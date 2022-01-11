525a53c8b3e0180140f8b7fd34bf06c4e7d74575
Mike Cronce <mc@readyset.io>
2022-01-11 11:15:57 -0500

    scripts/run-benchmark.sh:  Make a handful of changes to support A-B-A script
    * Accept $PROMETHEUS_ADDRESS environment variable
    * `cargo run` the `benchmarks` binary
    * Accept $INSTANCE_LABEL environment variable; default to previously
      hardcoded "local"
    * Document $RDS_DATABASE environment variable
    * Change default $DATA_DIR to /benchmarks/src/yaml/benchmarks in the monorepo,
      and make it an absolute path
    * Run benchmarks from /benchmarks in the monorepo; this makes the paths
      to benchmark YAML files and query/schema specfiles line up
    
    Change-Id: I8b2ee479923c1c842550392e01c023fd6d310b71
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1529
    Tested-by: Buildkite CI
    Reviewed-by: Justin Miron <justin@readyset.io>

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -7,16 +7,20 @@ set -euf -x -o pipefail
 #   RDS_PORT: The port of the RDS instance.
 #   RDS_USER: The username of the RDS database.
 #   RDS_PASS: The password used for the RDS database.
+#   RDS_DATABASE: The database name used for RDS.
 #   READYSET_ADAPTER: The adapter's NLB endpoint.
 #   PROMETHEUS_ADDRESS: The address of the monitor's prometheus server.
+#   INSTANCE_LABEL: Value to apply to "instance" label on Prometheus metrics;
+#                   defaults to "local".
 
-DATA_DIR="./"
+ROOT="$(cd "$(dirname "${0}")/.."; pwd)"
+DATA_DIR="${ROOT}/benchmarks/src/yaml/benchmarks/test"
 if [ "$#" -eq 1 ]; then
-    DATA_DIR=$1
+    DATA_DIR="$1"
 fi
 
 # Go to the directory that has all test scripts.
-cd $DATA_DIR
+cd "${ROOT}/benchmarks"
 
 # MySQL connection string for the RDS.
 READYSET_RDS=mysql://$RDS_USER:$RDS_PASS@$RDS_HOST:$RDS_PORT/$RDS_DATABASE
@@ -26,25 +30,25 @@ DEPLOYMENT_FILE=deployment.yaml
 # Write the deployment YAML for use by benchmarks later.
 cat > $DEPLOYMENT_FILE <<EOF
 ---
-instance_label: local
-prometheus_push_gateway: ~
+instance_label: "${INSTANCE_LABEL:-local}"
+prometheus_push_gateway: "${PROMETHEUS_ADDRESS:-~}"
 target_conn_str: "$READYSET_ADAPTER"
 setup_conn_str: "$READYSET_RDS"
 EOF
 
 echo "Beginning benchmarks"
 
-./benchmarks --benchmark read_benchmark_irl_small.yaml --deployment $DEPLOYMENT_FILE
+cargo run --release --bin benchmarks -- --benchmark "${DATA_DIR}/read_benchmark_irl_small.yaml" --deployment $DEPLOYMENT_FILE
 
 # Binlog replication from this shouldn't bleed over into the next test.
 sleep 120
 
-./benchmarks --benchmark cache_hit_fastly_small.yaml --deployment $DEPLOYMENT_FILE
+cargo run --release --bin benchmarks -- --benchmark "${DATA_DIR}/cache_hit_fastly_small.yaml" --deployment $DEPLOYMENT_FILE
 
-./benchmarks --benchmark read_benchmark_fastly_small.yaml --deployment $DEPLOYMENT_FILE
+cargo run --release --bin benchmarks -- --benchmark "${DATA_DIR}/read_benchmark_fastly_small.yaml" --deployment $DEPLOYMENT_FILE --skip-setup
 
-./benchmarks --benchmark scale_connections_small.yaml --deployment $DEPLOYMENT_FILE
+cargo run --release --bin benchmarks -- --benchmark "${DATA_DIR}/scale_connections_small.yaml" --deployment $DEPLOYMENT_FILE
 
 # This benchmark creates an incredibly number of views and doesn't clean them up.
 # It should be run last.
-./benchmarks --benchmark scale_views_small.yaml --deployment $DEPLOYMENT_FILE
+cargo run --release --bin benchmarks -- --benchmark "${DATA_DIR}/scale_views_small.yaml" --deployment $DEPLOYMENT_FILE
