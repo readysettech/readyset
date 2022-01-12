b32ac1f2710bb835e7353ada145e049844c77e81
Griffin Smith <griffin@readyset.io>
2022-01-11 19:43:54 -0500

    ci: Don't rebuild images if dependent files are unchanged
    The contents of several docker images in our pipeline only depend on the
    Dockerfile itself and, in some cases the rust-toolchain file - but
    notably not any of the code in the repository. Even with docker caches,
    pulling building tagging and pushing this image takes around a minute
    for each step if the agent we're running on is brand new.  If the
    dependent files are unchanged, then, this commit just uses the AWS API
    to re-tag the image using the current commit, which should ideally be
    significantly faster (on the order of up to a couple minutes in the
    worst case).
    
    Change-Id: Icfa4dd7adcc6197b952c057ad253fa53391cb8dd
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1624
    Tested-by: Buildkite CI
    Reviewed-by: Harley Klein <harley@readyset.io>

--- .buildkite/build-image.sh
+++ .buildkite/build-image.sh
@@ -7,6 +7,11 @@ set -euo pipefail
 # Docker Buildkit is used for better performance and caching
 # Builds are uploaded to our private AWS Elastic Container Registry
 # When builds are done on main, they are tagged as latest to help with caching.
+#
+# To optimize rebuilds of images that rarely change, set the SKIP_IF_UNCHANGED
+# environment variable to a comma-separated lists of repository-relative paths
+# to files - if none of these files have changed, the image won't actually be
+# built, it'll just be re-tagged with the current commit via the AWS API.
 
 
 dockerfile="$1"
@@ -25,6 +30,33 @@ VERSION=${VERSION:-$BUILDKITE_COMMIT}
 
 docker_repo="$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com"
 image="$docker_repo/$image_name"
+
+if [ -n "${SKIP_IF_UNCHANGED+1}" ]; then
+    do_skip="1"
+    changed_files="$(git diff --name-only HEAD~ HEAD)"
+    IFS=',' read -ra files <<< "$SKIP_IF_UNCHANGED,$dockerfile"
+    for fname in "${files[@]}"; do
+        if grep "$fname" <<< "$changed_files" > /dev/null; then
+            do_skip=""
+        fi
+    done
+
+    if [ -n "$do_skip" ]; then
+        echo "+++ :docker: Dockerfile unchanged, retagging latest image as ${BUILDKITE_COMMIT}"
+        manifest=$(
+            aws ecr batch-get-image \
+                --repository-name "$image_name" \
+                --image-ids imageTag=latest \
+                --output json \
+                | jq -r '.images[0].imageManifest')
+        aws ecr put-image \
+            --repository-name "$image_name" \
+            --image-tag "$BUILDKITE_COMMIT" \
+            --image-manifest "$manifest" \
+            > /dev/null
+        exit 0
+    fi
+fi
 
 echo "--- :docker: Pulling $image:latest"
 if docker pull "$image:latest"; then
