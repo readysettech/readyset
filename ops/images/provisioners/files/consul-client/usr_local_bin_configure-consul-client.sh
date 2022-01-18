0f2618899035b0cf98581e3a3752522521c80d34
Marcus Bastian <marcus@readyset.io>
2022-01-18 17:37:32 -0500

    consul/agent: discontinue use of consul generated node ids
    During redeployments to IRL's environment, we've encountered issues with
    agents receiving the following error message when attempting to link to
    Consul server.
    
    2022-01-14T20:00:09.258Z [WARN] agent.client.memberlist.lan: memberlist:
    ignoring alive message for 'ip-10-1-2-224': Member 'ip-10-1-2-224' has
    conflicting node ID '3c533a1c-f446-6a6b-996f-318265bb73b0' with this
    agent's ID
    
    There are quite a few threads out there wrt folks running into this
    error, and the resounding answer is to disable their host-id generation
    processes when dealing with ephemeral infrastructure.
    
    Bitnami, for instance, defaults to setting this key's value to `true`.
    
    https://github.com/bitnami/bitnami-docker-consul/pull/5/files
    
    Therefore, I think we should give this a shot and see how it fairs.
    
    Change-Id: I4b81aa87d8800bcd39028c2b8d781f9bd5ccb6a9
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1669
    Tested-by: Buildkite CI
    Reviewed-by: Harley Klein <harley@readyset.io>

--- ops/images/provisioners/files/consul-client/usr_local_bin_configure-consul-client.sh
+++ ops/images/provisioners/files/consul-client/usr_local_bin_configure-consul-client.sh
@@ -5,6 +5,8 @@ cat > /etc/consul.d/consul.hcl <<EOF
 data_dir = "/opt/consul"
 client_addr = "127.0.0.1"
 server = false
+# https://www.consul.io/docs/agent/options#_disable_host_node_id
+disable_host_node_id = true
 retry_join = ["provider=aws tag_key=${CONSUL_TAG_KEY:-consul-server} tag_value=${CONSUL_TAG_VALUE}"]
 EOF
 
