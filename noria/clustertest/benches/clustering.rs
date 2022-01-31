68305170b98c12043056fbaf6fd592be3286470b
Sif <sif@readyset.io>
2022-01-31 13:35:59 -0800

    clustertest: Worker failover/add benchmarks
    This adds two new clustertest benchmarks:
    * worker_failover
    * add_worker
    
    These tests are hidden behind the "slow_bench" feature flag, the
    module's documentation describes how to run them.
    
    Refs: ENG-944
    Change-Id: I1b00ceef45a2aff232feeb6f7ec0f5e1d414aec1
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1800
    Tested-by: Buildkite CI
    Reviewed-by: Justin Miron <justin@readyset.io>

--- noria/clustertest/benches/clustering.rs
+++ noria/clustertest/benches/clustering.rs
@@ -18,7 +18,7 @@ criterion::criterion_group! {
     // As these tests are potentially quite slow to begin with, set the sample
     // size to the lowest allowed by criterion
     config = criterion::Criterion::default().sample_size(10);
-    targets = slow_bench::leader_failover
+    targets = slow_bench::leader_failover, slow_bench::worker_failover, slow_bench::add_worker
 }
 
 #[cfg(feature = "slow_bench")]
@@ -58,6 +58,64 @@ mod slow_bench {
                     .kill_server(&leader, true)
                     .await
                     .unwrap();
+            });
+        });
+
+        rt.block_on(async {
+            deployment.borrow_mut().teardown().await.unwrap();
+        });
+    }
+
+    pub fn worker_failover(c: &mut Criterion) {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+
+        let deployment = Arc::new(RefCell::new(rt.block_on(async {
+            DeploymentBuilder::new("ct_bench_worker_failover")
+                .add_server(ServerParams::default())
+                .start()
+                .await
+                .unwrap()
+        })));
+        c.bench_function("worker_failover", |b| {
+            let worker = rt.block_on(async {
+                deployment
+                    .borrow_mut()
+                    .start_server(ServerParams::default(), true)
+                    .await
+                    .unwrap()
+            });
+
+            b.to_async(&rt).iter(|| async {
+                deployment
+                    .borrow_mut()
+                    .kill_server(&worker, true)
+                    .await
+                    .unwrap();
+            });
+        });
+
+        rt.block_on(async {
+            deployment.borrow_mut().teardown().await.unwrap();
+        });
+    }
+
+    pub fn add_worker(c: &mut Criterion) {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+
+        let deployment = Arc::new(RefCell::new(rt.block_on(async {
+            DeploymentBuilder::new("ct_bench_add_worker")
+                .add_server(ServerParams::default())
+                .start()
+                .await
+                .unwrap()
+        })));
+        c.bench_function("add_worker", |b| {
+            b.to_async(&rt).iter(|| async {
+                deployment
+                    .borrow_mut()
+                    .start_server(ServerParams::default(), true)
+                    .await
+                    .unwrap()
             });
         });
 
