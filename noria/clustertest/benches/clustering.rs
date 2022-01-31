c490835610437b6ca2eb9b9b2b1c37942ccde7fe
Sif <sif@readyset.io>
2022-01-31 10:32:05 -0800

    clustertest: Add leader failover benchmark
    This adds a benchmark to clustertest which measures the time it takes
    for cluster leadership to fail over to a new server. As the benchmark
    is fairly slow (~40s per iteration on my laptop, >6m for the shortest
    run criterion supports, >1h for default), it's hidden behind the
    "slow_bench" feature flag to keep it off by defaut.
    
    Change-Id: If2733133a534deb33b93a19c950bf9fd78d24ce1
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1795
    Reviewed-by: Justin Miron <justin@readyset.io>
    Tested-by: Buildkite CI

---
+++ noria/clustertest/benches/clustering.rs
@@ -0,0 +1,68 @@
+//! Slow cluster-manipulating benchmarks
+//!
+//! The benchmarks in this module are potentially quite slow (seconds to
+//! minutes per iteration), and so are disabled by default. To run these tests,
+//! the `--features` flag can be provided to cargo bench to enable them, for
+//! example:
+//!
+//! ```ignore
+//! cargo bench -p clustertest --features slow_bench
+//! ```
+
+#[cfg(not(feature = "slow_bench"))]
+fn main() {}
+
+#[cfg(feature = "slow_bench")]
+criterion::criterion_group! {
+    name = benches;
+    // As these tests are potentially quite slow to begin with, set the sample
+    // size to the lowest allowed by criterion
+    config = criterion::Criterion::default().sample_size(10);
+    targets = slow_bench::leader_failover
+}
+
+#[cfg(feature = "slow_bench")]
+criterion::criterion_main!(benches);
+
+#[cfg(feature = "slow_bench")]
+mod slow_bench {
+    use clustertest::{DeploymentBuilder, ServerParams};
+    use criterion::Criterion;
+    use std::cell::RefCell;
+    use std::sync::Arc;
+
+    pub fn leader_failover(c: &mut Criterion) {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+
+        let deployment = Arc::new(RefCell::new(rt.block_on(async {
+            DeploymentBuilder::new("ct_bench_leader_failover")
+                .add_server(ServerParams::default())
+                .start()
+                .await
+                .unwrap()
+        })));
+        c.bench_function("leader_failover", |b| {
+            let leader = &deployment.borrow().server_addrs()[0];
+
+            rt.block_on(async {
+                deployment
+                    .borrow_mut()
+                    .start_server(ServerParams::default(), true)
+                    .await
+                    .unwrap();
+            });
+
+            b.to_async(&rt).iter(|| async {
+                deployment
+                    .borrow_mut()
+                    .kill_server(&leader, true)
+                    .await
+                    .unwrap();
+            });
+        });
+
+        rt.block_on(async {
+            deployment.borrow_mut().teardown().await.unwrap();
+        });
+    }
+}
