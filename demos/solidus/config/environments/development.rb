9fcc0efee3e2d195b9dc180d411800e966073c47
Jay Bensal <jay@readyset.io>
2022-02-07 22:50:40 +0000

    demos/solidus: push current demo with monitoring
    This commit includes a few different modifications
    as we spin up a demo application in AWS.
    
    1) Add the Vector / Prometheus / Grafana stack
    2) Flip a few switches to make solidus faster
    3) Use a release build of Readyset
    
    Change-Id: I2ae6169f25502e0fde404031353b186fa6e8c5fa
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1858
    Tested-by: Buildkite CI
    Reviewed-by: Justin Miron <justin@readyset.io>

--- demos/solidus/config/environments/development.rb
+++ demos/solidus/config/environments/development.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  # config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
--- demos/solidus/config/environments/production.rb
+++ demos/solidus/config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
