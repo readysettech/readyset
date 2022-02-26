2fd975eafa0d99b9e42e0edd05634fa8cf1ced98
Justin Miron <justin@readyset.io>
2022-02-26 08:36:50 +0000

    clustertests: Simplify the duplicated clustertest querying code
    This CL deduplicates code in the clustertest utilities and supports
    specifying whether a read is done through prepare and execute or via
    query. In addition some refactoring is done to prevent having a separate
    function for querying against Noria.
    
    Change-Id: I2d7f9059613fd9e2a17302fa2092c20a7458e07d
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/1987
    Tested-by: Buildkite CI
    Reviewed-by: Peter Farr <peter@readyset.io>

--- noria/clustertest/src/utils.rs
+++ noria/clustertest/src/utils.rs
@@ -17,11 +17,36 @@ where
 
 /// Tracks the set of allowed values after a set of writes with eventual
 /// consistency. This is intended to be used with [`query_until_expected`]
-/// and [`query_until_expected_from_noria`] via [`as_expected_results`].
+/// and [`query_until_expected_from_noria`].
 ///
 /// Writes indicated via the [`write`] function, will populate the
 /// intermediate and expected results to meet eventual consistency
-/// correctness accordingly.
+/// correctness accordingly. This is done by tracking the set of intermediate
+/// values that a read *may* return, and the final value that a read *must*
+/// return. If a read returns a value not in the intermediate set or match
+/// the final value, we have violated correctness and `query_until_expected`
+/// will fail.
+///
+/// Example: Imagine that we want to check that executing the query
+/// `SELECT * FROM t1 where c1 = ?`returns eventually consistent results under
+/// some conditions, while we are performing writes.
+///
+/// Say we update the database twice:
+/// `INSERT INTO t1 VALUES (4,5);`
+/// `UPDATE t1 SET c2 = 4 where c1 = 4`
+/// `UPDATE t1 SET c2 = 3 where c1 = 4`
+///
+/// For this set of writes we would expect an eventually consistent database
+/// to return the following for `SELECT * FROM t1 where c1 = ?`:
+/// 1. (), an empty set before any value with c1 = 4 is inserted.
+/// 2. (4,5)
+/// 3. (4,4)
+/// 4. (4,3)
+///
+/// From a reader's perspective we may only see a subset of these values, however,
+/// we can *never* see a value that is not one of these four. This can be guaranteed
+/// by using [`EventuallyConsistentResults`] by mirroring the set of writes to the
+/// result set with `write`.
 pub struct EventuallyConsistentResults<T>
 where
     T: Clone,
@@ -62,20 +87,29 @@ where
     }
 }
 
-/// Returns true when a prepare and execute returns the expected results,
-/// [`EventuallyConsistentResults::expected`]. This function returns false if:
-///   - The `timeout` is reached without the expected result being yielded.
-///   - The query returns a value that is neither [`EventuallyConsistentResults::expected`] nor in
-///     the [`EventuallyConsistentResults::intermediate`] list.
-///   - A query to the connection returns an error.
-///
-/// This function should be used in place of sleeping and executing a query after
-/// the write propagation delay. It can also be used to assert that ReadySet
-/// returns eventually consistent results, while waiting for an expected result.
+/// How to execute a query against a MySQL backend.
+pub enum QueryExecution<S, P>
+where
+    S: StatementLike + Clone,
+    P: Into<Params> + Clone + std::marker::Send,
+{
+    /// Use preparing and executing a query.
+    PrepareExecute(S, P),
+    /// Query via the query text.
+    Query(&'static str),
+}
+
+/// Where the expected result should come from in `query_until_expected_inner`.
+pub enum ResultSource<'a> {
+    /// Results must be retrieved from Noria, verified via the use of the MetricsClient.
+    FromNoria(&'a mut MetricsClient),
+    /// Results can come from either Noria or MySQL.
+    FromAnywhere,
+}
+
 pub async fn query_until_expected<S, T, P>(
     conn: &mut Conn,
-    query: S,
-    params: P,
+    query: QueryExecution<S, P>,
     results: &EventuallyConsistentResults<T>,
     timeout: Duration,
 ) -> bool
@@ -84,76 +118,97 @@ where
     P: Into<Params> + Clone + std::marker::Send,
     T: FromRow + std::cmp::PartialEq + std::marker::Send + std::fmt::Debug + Clone + 'static,
 {
-    let mut last: Option<Vec<T>> = None;
-    let start = Instant::now();
-    loop {
-        if start.elapsed() > timeout {
-            println!("query_until_expected timed out, last: {:?}", last);
-            return false;
-        }
-        let remaining = std::cmp::min(Duration::from_secs(5), timeout - start.elapsed());
-        let result =
-            tokio::time::timeout(remaining, conn.exec(query.clone(), params.clone())).await;
-        match result {
-            Ok(Ok(r)) => {
-                if equal_rows(&r, &results.expected) {
-                    return true;
-                }
-                if !results
-                    .intermediate
-                    .iter()
-                    .any(|intermediate| equal_rows(&r, intermediate))
-                {
-                    println!("Query results did not match accepted intermediate results. Results: {:?}, Accepted: {:?}", r, results.intermediate);
-                    return false;
-                }
-                last = Some(r.clone());
-            }
-            Ok(Err(e)) => {
-                println!("Returned an error when querying for results, {:?}", e);
-                return false;
-            }
-            Err(_) => {
-                println!("Timed out when querying conn.");
-            }
-        }
-
-        sleep(Duration::from_millis(100)).await;
-    }
+    query_until_expected_inner(conn, query, ResultSource::FromAnywhere, results, timeout).await
 }
 
-/// The same as query_until_expected but takes a string slice for the query and no params. Allows
-/// us to test our own custom commands that may take time to resolve, and aren't undertood as being
-/// StatementLike.
-pub async fn query_until_expected_str<T>(
+/// Like [`query_until_expected`], except requires that the expected result was
+/// queried from Noria. The intermediate results, and the expected result may be
+/// returned via fallback without failing.
+pub async fn query_until_expected_from_noria<S, T, P>(
     conn: &mut Conn,
-    query: &str,
+    metrics: &mut MetricsClient,
+    query: QueryExecution<S, P>,
     results: &EventuallyConsistentResults<T>,
     timeout: Duration,
 ) -> bool
 where
+    S: StatementLike + Clone,
+    P: Into<Params> + Clone + std::marker::Send,
     T: FromRow + std::cmp::PartialEq + std::marker::Send + std::fmt::Debug + Clone + 'static,
 {
-    let mut last: Option<Vec<T>> = None;
+    query_until_expected_inner(
+        conn,
+        query,
+        ResultSource::FromNoria(metrics),
+        results,
+        timeout,
+    )
+    .await
+}
+
+pub async fn query_until_expected_inner<S, T, P>(
+    conn: &mut Conn,
+    query: QueryExecution<S, P>,
+    mut source: ResultSource<'_>,
+    results: &EventuallyConsistentResults<T>,
+    timeout: Duration,
+) -> bool
+where
+    S: StatementLike + Clone,
+    P: Into<Params> + Clone + std::marker::Send,
+    T: FromRow + std::cmp::PartialEq + std::marker::Send + std::fmt::Debug + Clone + 'static,
+{
+    let mut last: Option<(Vec<T>, bool)> = None;
     let start = Instant::now();
     loop {
+        // Check if we have timed out, if we have not execute the query against the connection
+        // with up to as long as we have left in the timeout. We timeout any query after 5
+        // seconds to quickly retry due to failures.
         if start.elapsed() > timeout {
             println!("query_until_expected timed out, last: {:?}", last);
             return false;
         }
         let remaining = std::cmp::min(Duration::from_secs(5), timeout - start.elapsed());
-        let result = tokio::time::timeout(remaining, conn.query(query)).await;
+        let num_noria_queries_before = if let ResultSource::FromNoria(metrics) = &mut source {
+            get_num_view_queries(metrics).await
+        } else {
+            0
+        };
+
+        let result = match &query {
+            QueryExecution::PrepareExecute(query, params) => {
+                tokio::time::timeout(remaining, conn.exec(query.clone(), params.clone())).await
+            }
+            QueryExecution::Query(q) => tokio::time::timeout(remaining, conn.query(q)).await,
+        };
+
+        //
         match result {
             Ok(Ok(r)) => {
-                if equal_rows(&r, &results.expected) {
+                let correct_source = match &mut source {
+                    ResultSource::FromAnywhere => true,
+                    ResultSource::FromNoria(metrics) => {
+                        get_num_view_queries(metrics).await > num_noria_queries_before
+                    }
+                };
+                let has_expected_result = equal_rows(&r, &results.expected);
+                if has_expected_result && correct_source {
                     return true;
                 }
-                last = Some(r.clone());
+                if !has_expected_result
+                    && !results
+                        .intermediate
+                        .iter()
+                        .any(|intermediate| equal_rows(&r, intermediate))
+                {
+                    println!("Query results did not match accepted intermediate results. Results: {:?}, Accepted: {:?}", r, results.intermediate);
+                    return false;
+                }
+                last = Some((r.clone(), correct_source));
             }
-            Ok(Err(_)) => {
-                // Returned an error when querying for results, which may be intermittent. We'll
-                // try again.
-                continue;
+            Ok(Err(e)) => {
+                println!("Returned an error when querying for results, {:?}", e);
+                return false;
             }
             Err(_) => {
                 println!("Timed out when querying conn.");
@@ -181,70 +236,5 @@ async fn get_num_view_queries(metrics: &mut MetricsClient) -> u32 {
         // If we cannot reach the metrics client we return 0 view queries, this will always fail a
         // check that we executed the query against Noria.
         Err(_) => 0,
-    }
-}
-
-/// Like [`query_until_expected`], except requires that the expected result was
-/// queried from Noria. The intermediate results, and the expected result may be
-/// returned via fallback without failing.
-pub async fn query_until_expected_from_noria<S, T, P>(
-    conn: &mut Conn,
-    metrics: &mut MetricsClient,
-    query: S,
-    params: P,
-    results: &EventuallyConsistentResults<T>,
-    timeout: Duration,
-) -> bool
-where
-    S: StatementLike + Clone,
-    P: Into<Params> + Clone + std::marker::Send,
-    T: FromRow + std::cmp::PartialEq + std::marker::Send + std::fmt::Debug + Clone + 'static,
-{
-    let mut last: Option<Vec<T>> = None;
-    let start = Instant::now();
-    loop {
-        if start.elapsed() > timeout {
-            println!("query_until_expected timed out, last: {:?}", last);
-            return false;
-        }
-        let remaining = std::cmp::min(Duration::from_secs(5), timeout - start.elapsed());
-
-        let num_view_queries_before = get_num_view_queries(metrics).await;
-        let result =
-            tokio::time::timeout(remaining, conn.exec(query.clone(), params.clone())).await;
-        match result {
-            Ok(Ok(r)) => {
-                let noria_queried = get_num_view_queries(metrics).await > num_view_queries_before;
-                let has_expected_result = equal_rows(&r, &results.expected);
-                if has_expected_result && noria_queried {
-                    return true;
-                }
-                if !has_expected_result
-                    && !results
-                        .intermediate
-                        .iter()
-                        .any(|intermediate| equal_rows(&r, intermediate))
-                {
-                    println!("Query results did not match accepted intermediate results or the results did not come from Noria.");
-                    println!(
-                        "Results: {:?}, Accepted: {:?}, Expected: {:?}",
-                        r, results.intermediate, results.expected
-                    );
-                    return false;
-                }
-                if noria_queried {
-                    last = Some(r.clone());
-                }
-            }
-            Ok(Err(e)) => {
-                println!("Returned an error when querying for results, {:?}", e);
-                return false;
-            }
-            Err(_) => {
-                println!("Timed out when querying conn.");
-            }
-        }
-
-        sleep(Duration::from_millis(100)).await;
     }
 }
