232dae626a465f2e656d70f7c8acc1ada6bdd42f
Griffin Smith <griffin@readyset.io>
2022-03-24 17:23:22 -0400

    noria-client: Make set-statement-allowed a method of QueryHandler
    Whether or not a SET statement should be allowed is a different question
    based on the type of upstream database we're using, but previously the
    logic we had for whether or not to allow SET statements was basically
    entirely mysql-specific. To enable making that logic different for
    postgres vs mysql, this introduces a new is_set_allowed method on the
    QueryHandler trait, and migrates all the existing logic to the impl of
    that method on the mysql QueryHandler. The postgres version of this
    method is currently stubbed out to always return false, but soon will be
    updated to have postgres-specific logic.
    
    Change-Id: Id10299822fb8c8008256ec3e7d01919f69239e20
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/2125
    Tested-by: Buildkite CI
    Reviewed-by: Peter Farr <peter@readyset.io>

--- noria-mysql/src/query_handler.rs
+++ noria-mysql/src/query_handler.rs
@@ -1,20 +1,74 @@
 use std::borrow::Cow;
+use std::str::FromStr;
 use std::sync::Arc;
 
 use nom_sql::{
-    Column, ColumnSpecification, Expression, FieldDefinitionExpression, SqlIdentifier, SqlQuery,
-    SqlType,
+    Column, ColumnSpecification, Expression, FieldDefinitionExpression, Literal, SqlIdentifier,
+    SqlQuery, SqlType,
 };
 use noria::results::Results;
-use noria::ColumnSchema;
+use noria::{ColumnSchema, ReadySetError};
 use noria_client::backend::noria_connector::QueryResult;
 use noria_client::backend::{noria_connector, SelectSchema};
 use noria_client::QueryHandler;
 use noria_data::DataType;
 use noria_errors::ReadySetResult;
+use tracing::warn;
 
 const MAX_ALLOWED_PACKET_VARIABLE_NAME: &str = "max_allowed_packet";
 const MAX_ALLOWED_PACKET_DEFAULT: DataType = DataType::UnsignedInt(67108864);
+
+const ALLOWED_SQL_MODES: [SqlMode; 7] = [
+    SqlMode::OnlyFullGroupBy,
+    SqlMode::StrictTransTables,
+    SqlMode::NoZeroInDate,
+    SqlMode::NoZeroDate,
+    SqlMode::ErrorForDivisionByZero,
+    SqlMode::NoAutoCreateUser,
+    SqlMode::NoEngineSubstitution,
+];
+
+// SqlMode holds the current list of known sql modes that we care to deal with.
+// TODO(peter): expand this later to include ALL sql modes.
+#[derive(PartialEq, Eq, Hash)]
+enum SqlMode {
+    OnlyFullGroupBy,
+    StrictTransTables,
+    NoZeroInDate,
+    NoZeroDate,
+    ErrorForDivisionByZero,
+    NoAutoCreateUser,
+    NoEngineSubstitution,
+}
+
+// TODO(vlad): replace with strum
+impl FromStr for SqlMode {
+    type Err = ReadySetError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let res = match &s.trim().to_ascii_lowercase()[..] {
+            "only_full_group_by" => SqlMode::OnlyFullGroupBy,
+            "strict_trans_tables" => SqlMode::StrictTransTables,
+            "no_zero_in_date" => SqlMode::NoZeroInDate,
+            "no_zero_date" => SqlMode::NoZeroDate,
+            "error_for_division_by_zero" => SqlMode::ErrorForDivisionByZero,
+            "no_auto_create_user" => SqlMode::NoAutoCreateUser,
+            "no_engine_substitution" => SqlMode::NoEngineSubstitution,
+            _ => {
+                return Err(ReadySetError::SqlModeParseFailed(s.to_string()));
+            }
+        };
+        Ok(res)
+    }
+}
+
+fn raw_sql_modes_to_list(sql_modes: &str) -> Result<Vec<SqlMode>, ReadySetError> {
+    sql_modes
+        .split(',')
+        .into_iter()
+        .map(SqlMode::from_str)
+        .collect::<Result<Vec<SqlMode>, ReadySetError>>()
+}
 
 /// MySQL flavor of [`QueryHandler`].
 pub struct MySqlQueryHandler;
@@ -87,5 +141,48 @@ impl QueryHandler for MySqlQueryHandler {
             data,
             select_schema: schema,
         })
+    }
+
+    fn is_set_allowed(stmt: &nom_sql::SetStatement) -> bool {
+        match stmt {
+            nom_sql::SetStatement::Variable(set) => set.variables.iter().all(|(variable, value)| {
+                match variable.as_non_user_var() {
+                    Some("time_zone") => {
+                        matches!(value, Expression::Literal(Literal::String(ref s)) if s == "+00:00")
+                    }
+                    Some("autocommit") => {
+                        matches!(value, Expression::Literal(Literal::Integer(i)) if *i == 1)
+                    }
+                    Some("sql_mode") => {
+                        if let Expression::Literal(Literal::String(ref s)) = value {
+                            match raw_sql_modes_to_list(&s[..]) {
+                                Ok(sql_modes) => {
+                                    sql_modes.iter().all(|sql_mode| ALLOWED_SQL_MODES.contains(sql_mode))
+                                }
+                                Err(e) => {
+                                    warn!(%e, "unknown sql modes in set");
+                                    false
+                                }
+                            }
+                        } else {
+                            false
+                        }
+                    }
+                    Some("names") => {
+                        if let Expression::Literal(Literal::String(ref s)) = value {
+                            matches!(&s[..], "latin1" | "utf8" | "utf8mb4")
+                        } else {
+                            false
+                        }
+                    }
+                    Some("foreign_key_checks") => true,
+                    _ => false,
+                }
+            }),
+            nom_sql::SetStatement::Names(names) => {
+                names.collation.is_none()
+                    && matches!(&names.charset[..], "latin1" | "utf8" | "utf8mb4")
+            }
+        }
     }
 }
