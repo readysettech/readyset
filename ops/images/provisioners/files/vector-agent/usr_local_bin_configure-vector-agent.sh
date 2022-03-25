000c89e2d266538855e2f5f2531c640f20ba261f
Marcus Volhontseff <marcus.v@readyset.io>
2022-03-24 22:08:29 -0700

    ENG-840: Creating new stack for monitoring resources
    - moved resources into new template file, readyset-monitoring-template.ytt.yaml
    - moved over relevant params
    - removed unneeded params within the mysql and postresql stacks
    - added a new AWS::CloudFormation::Stack resource type for monitoring stack to
    the 'super stacks'
    - removed uneeded k/v pairs in AWSAMIRegionMap
    - created a dependency on the new monitoring stack for other mysql and
    postgresql nested stacks declared in related super stacks
    - used rain and cfn-lint on mysql super stack to ensure formatting standards
    - fixing export of monitoring instance IP
    - adding new vector config for direct to cloudwatch sink
    - introducing new CFN param to specify vector config file
    - updating userdata and bash to pass param as env var for vector config
    - enabling mysql adapter IAM profile to write to CW
    - resolved merge conflicts
    - updating provisioner script to install /etc/vector/cloudwatch.toml
    - adding Param for vector config to smoke test script
    - refactored vector files; split into atomic snippets; updated 00-init.sh
    - added new params to CFN to enable Cloudwatch and/or 3rd party agg
    - updated vector config script to evaluate strings and copy vector config
    - removed unneeded param from smoketest (default is to enable CW logging only)
    - added whitespace to shell test brackets for visual consistency
    - adding CFN Params to postgresql for CW/AGG control
    - adding env vars to UserData for server
    - updating file paths for linux install cmd in vector-agent/00-init.sh
    - used rain fmt to fix issues with linting reported by the pipeline
    - adding changes (forgot to stage changes prior to last amend)
    - fixing typos in script; missing double quote and end of if statement
    - adding missing triple quote to vector config snippet
    - adding new bash var LOOKUP_AGGREGATOR to vector config script
    - adding missing vars AWS_CLOUDFORMATION_STACK and AWS_CLOUDFORMATION_REGION
    - adding CW managed policy to Server role
    
    Change-Id: I4db226d380a671821dbf8fc26608faa7e53601e5
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/2127
    Tested-by: Buildkite CI
    Reviewed-by: Harley Klein <harley@readyset.io>
    Reviewed-by: Sasha Fox <sasha@readyset.io>

--- ops/images/provisioners/files/vector-agent/usr_local_bin_configure-vector-agent.sh
+++ ops/images/provisioners/files/vector-agent/usr_local_bin_configure-vector-agent.sh
@@ -4,10 +4,33 @@ set -euo pipefail
 IMDS_TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 SERVER_ADDRESS=$(curl -H "X-aws-ec2-metadata-token: $IMDS_TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
 
+OPT_PATH="/opt/readyset/vector.d"
+CONFIG_DIR="/etc/vector"
+BASE="base.toml"
+METADATA_CW="metadata_to_cw.toml"
+METRICS_CW="metrics_to_cw.toml"
+METADATA_AGG="metadata_to_agg.toml"
+METRICS_AGG="metrics_to_agg.toml"
+LOOKUP_AGGREGATOR=false
+
 MAX_RETRIES=8
 failures=0
 LOG_AGGREGATOR_ADDRESS=""
-while [ -z "${LOG_AGGREGATOR_ADDRESS}" ] && [ $failures -lt $MAX_RETRIES ]; do
+
+cp ${OPT_PATH}/${BASE} ${CONFIG_DIR}/${BASE}
+
+if [ ${ENABLE_CW} = true ]; then
+  cp ${OPT_PATH}/${METADATA_CW} ${CONFIG_DIR}/${METADATA_CW}
+  cp ${OPT_PATH}/${METRICS_CW} ${CONFIG_DIR}/${METRICS_CW}
+fi
+
+if [ ${ENABLE_AGG} = true ]; then
+  cp ${OPT_PATH}/${METADATA_AGG} ${CONFIG_DIR}/${METADATA_AGG}
+  cp ${OPT_PATH}/${METRICS_AGG} ${CONFIG_DIR}/${METRICS_AGG}
+  LOOKUP_AGGREGATOR=true
+fi
+
+while [ ${LOOKUP_AGGREGATOR} = true ] && [ -z ${LOG_AGGREGATOR_ADDRESS} ] && [ ${failures} -lt ${MAX_RETRIES} ]; do
   if [ $failures -gt 1 ]; then
     sleep $((2 ** failures))
   fi
@@ -16,11 +39,14 @@ while [ -z "${LOG_AGGREGATOR_ADDRESS}" ] && [ $failures -lt $MAX_RETRIES ]; do
 done
 
 cat > /etc/default/vector <<EOF
+AWS_CLOUDFORMATION_STACK=${AWS_CLOUDFORMATION_STACK}
+AWS_CLOUDFORMATION_REGION=${AWS_CLOUDFORMATION_REGION}
 PROMETHEUS_PORT=${PROMETHEUS_PORT}
 NORIA_DEPLOYMENT=${DEPLOYMENT}
 NORIA_TYPE=${NORIA_TYPE}
 SERVER_ADDRESS=${SERVER_ADDRESS}
 LOG_AGGREGATOR_ADDRESS=${LOG_AGGREGATOR_ADDRESS}
+VECTOR_CONFIG_DIR=${CONFIG_DIR}
 EOF
 
 systemctl reset-failed
--- ops/images/provisioners/scripts/vector-agent/00-init.sh
+++ ops/images/provisioners/scripts/vector-agent/00-init.sh
@@ -2,8 +2,24 @@
 set -eux
 
 sudo install -D -o vector -g vector -m 644 \
-  /tmp/vector-agent/etc_vector_vector.toml \
-  /etc/vector/vector.toml
+  /tmp/vector-agent/etc_vector_base.toml \
+  /opt/readyset/vector.d/base.toml
+
+sudo install -D -o vector -g vector -m 644 \
+  /tmp/vector-agent/etc_vector_metadata_to_cw.toml \
+  /opt/readyset/vector.d/metadata_to_cw.toml
+
+sudo install -D -o vector -g vector -m 644 \
+  /tmp/vector-agent/etc_vector_metrics_to_cw.toml \
+  /opt/readyset/vector.d/metrics_to_cw.toml
+
+sudo install -D -o vector -g vector -m 644 \
+  /tmp/vector-agent/etc_vector_metadata_to_agg.toml \
+  /opt/readyset/vector.d/metadata_to_agg.toml
+
+sudo install -D -o vector -g vector -m 644 \
+  /tmp/vector-agent/etc_vector_metrics_to_agg.toml \
+  /opt/readyset/vector.d/metrics_to_agg.toml
 
 sudo install -D -o root -g root -m 755 \
   /tmp/vector-agent/usr_local_bin_configure-vector-agent.sh \
