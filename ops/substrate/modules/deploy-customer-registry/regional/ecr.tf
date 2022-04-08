cf12340891469da46b79e9376e5c022af6211a7e
Саша <sasha@readyset.io>
2022-04-08 08:05:02 -0700

    Authenticated ECR Repo
    Overview:
    Add customer facing ECR repo w/ AWS account authentication
    
    Changes
    - Added ECR policy role that allows conditional access based on IAM account
    - Added YAML file of allowed account IDs
    
    Tech Debt
    - YAML file of accounts is not validated or checked in any way
    
    Change-Id: Ie9ba08443cc92c5e4ff39a6d592635aef146c78b
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/2201
    Tested-by: Buildkite CI
    Reviewed-by: Harley Klein <harley@readyset.io>
    Terraform-Plan-Review: Harley Klein <harley@readyset.io>

--- ops/substrate/modules/deploy-customer-registry/regional/ecr.tf
+++ ops/substrate/modules/deploy-customer-registry/regional/ecr.tf
@@ -9,5 +9,41 @@ locals {
 resource "aws_ecr_repository" "release_primary" {
   # We set up the primary if we are in the primary region and set up the repository replicas in others.
   for_each = data.aws_region.current.name == "us-east-2" ? local.release_repositories : toset([])
-  name     = "${each.key}"
+  name     = each.key
+}
+
+locals {
+  iam_ids          = yamldecode(file("${path.module}/data/allowed_accounts.yaml"))
+  iam_users        = local.iam_ids.users
+  allowed_accounts = [for user_id in local.iam_users : "arn:aws:iam::${user_id}:root"]
+}
+
+data "aws_iam_policy_document" "release_primary_read_perms" {
+  statement {
+    sid = "ECRPublicRead"
+
+    effect = "Allow"
+
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetAuthorizationToken",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+    ]
+
+    principals {
+      identifiers = local.allowed_accounts
+      type        = "AWS"
+    }
+  }
+}
+
+resource "aws_ecr_repository_policy" "release_primary_policy" {
+  for_each   = aws_ecr_repository.release_primary
+  repository = each.value.name
+  policy     = data.aws_iam_policy_document.release_primary_read_perms.json
 }
