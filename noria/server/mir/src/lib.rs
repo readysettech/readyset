e916cc62f6d7451de4010b16608a569cd9504855
Griffin Smith <griffin@readyset.io>
2022-04-26 11:24:35 -0400

    mir: Add crate-level docstring
    Add a crate-level docstring to the mir crate, explaining what it is, why
    it exists, and what its goals are.
    
    Change-Id: I8cabe68802143c0f42705c0a59c90b272a2720e8
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/2277
    Tested-by: Buildkite CI
    Reviewed-by: Fran Noriega <fran@readyset.io>

--- noria/server/mir/src/lib.rs
+++ noria/server/mir/src/lib.rs
@@ -1,3 +1,44 @@
+//! The **M**id-level **I**ntermediate **R**epresentation
+//!
+//! This crate contains the data structure definitions for `MIR`, the Mid-level Intermediate
+//! Representation for the compiler from SQL to Noria. MIR is structured as a directed acyclic
+//! graph, where (similarly to dataflow) nodes in the graph each represent some abstract computation
+//! on rows produced by their parents. MIR sits in between the SQL [AST] and the final dataflow
+//! graph in the compilation process. When converting from MIR to dataflow, each [node] in the MIR
+//! graph is converted to zero or more nodes in the dataflow graph.
+//!
+//! # Why MIR?
+//!
+//! As a directed acyclic graph, MIR is structured in a way that is similar to the dataflow graph
+//! (the final representation we use for a query), but has several differences in its design that
+//! make it better suited for use as an intermediate representation. At a high level, while the
+//! structure of the dataflow graph is primarily optimized for the most efficient *execution* of a
+//! query, the structure of the MIR graph is primarily optmized for making it easiest for
+//! *developers* to deal with the graph. Most notably, while dataflow uses numeric column *indices*
+//! throughout to refer to columns in its parents, MIR uses *named* [column references]. This allows
+//! us to much more easily perform [rewrite passes] which may add new columns, remove columns, or
+//! reorder columns, without ever having to worry about keeping numeric references up-to-date. In
+//! addition, where convenient, node types in MIR do not have to correspond one-to-one with node
+//! types in dataflow - for example:
+//!
+//! * The [`TableAlias`] node is skipped entirely when converting to dataflow since its only purpose
+//!   is to change table names in column references, something which doesn't exist in dataflow
+//! * The [`Distinct`] node, which corresponds in the most obvious way to the SQL `DISTINCT`
+//!   keyword, is actually converted to a `Count` node when lowering to dataflow, since due to the
+//!   intricacies of partial stateful dataflow the best way to implement distinct is to calculate a
+//!   count grouped by all columns followed by omitting the actual count value in the result set
+//! * The [`DependentJoin`] node will throw an *error* if encountered when converting to dataflow -
+//!   this node type is created as part of compiling correlated subqueries, and the expectation is
+//!   that it will be removed entirely as part of a [rewrite pass][decorrelate]
+//!
+//! [AST]: nom_sql
+//! [node]: crate::node::MirNode
+//! [column references]: crate::Column
+//! [rewrite passes]: crate::rewrite
+//! [`TableAlias`]: crate::node::MirNodeInner::TableAlias
+//! [`Distinct`]: crate::node::MirNodeInner::Distinct
+//! [decorrelate]: crate::rewrite::decorrelate::eliminate_dependent_joins
+
 #![warn(clippy::panic)]
 #![deny(unused_extern_crates, macro_use_extern_crate)]
 #![feature(stmt_expr_attributes, box_patterns)]
