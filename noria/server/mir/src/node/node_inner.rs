588480d93e15c6812424325a0645aba410b47bfb
Griffin Smith <griffin@readyset.io>
2022-04-26 10:48:07 -0400

    mir: Add docstrings for all MIR node types
    Add a docstring to all MIR node types, and all fields within those MIR
    node types, explaining what those nodes do in a (more) consistent style
    and linking to the dataflow nodes that the MIR nodes get converted to
    when lowering to dataflow.
    
    Change-Id: Ic0a6a6b5558e007e919336fe10efacc934d721d2
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/2276
    Reviewed-by: Fran Noriega <fran@readyset.io>
    Tested-by: Buildkite CI

--- noria/server/mir/src/node/node_inner.rs
+++ noria/server/mir/src/node/node_inner.rs
@@ -16,40 +16,91 @@ use crate::{Column, MirNodeRef};
 
 #[derive(Clone, Serialize, Deserialize)]
 pub enum MirNodeInner {
-    /// over column, group_by columns
+    /// Node that computes an aggregate function on a column grouped by another set of columns,
+    /// outputting its result as an additional column.
+    ///
+    /// Converted to [`Aggregator`] when lowering to dataflow.
+    ///
+    /// [`Aggregator`]: dataflow::ops::grouped::aggregate::Aggregator
     Aggregation {
+        /// Column to compute the aggregate function over
         on: Column,
+        /// List of columns to group by
         group_by: Vec<Column>,
         /// The column name to use for the result of the aggregate, which will always be the last
         /// column
         output_column: Column,
+        /// Which aggregate function we are computing
         kind: Aggregation,
     },
-    /// column specifications, unique-keys (non-compound), tx flag, adapted base
+    /// Base node in the graph, corresponding to a snapshot of a full table in the upstream
+    /// database.
+    ///
+    /// Converted to [`Base`] when lowering to dataflow.
+    ///
+    /// [`Aggregator`]: dataflow::node::special::Base
     Base {
         column_specs: Vec<(ColumnSpecification, Option<usize>)>,
         primary_key: Option<Box<[Column]>>,
         unique_keys: Box<[Box<[Column]>]>,
         adapted_over: Option<BaseNodeAdaptation>,
     },
-    /// over column, group_by columns
+    /// Node that computes the extreme value (minimum or maximum) of a column grouped by another
+    /// set of columns, outputting its result as an additional column.
+    ///
+    /// Converted to [`ExtremumOperator`] when lowering to dataflow
+    ///
+    /// [`ExtremumOperator`]: dataflow::ops::grouped::extremum::ExtremumOperator
     Extremum {
+        /// Column to compute the extremum of
         on: Column,
+        /// List of columns to group by
         group_by: Vec<Column>,
         /// The column name to use for the extreme value, which will always be the last column
         output_column: Column,
+        /// Which kind of extreme value to compute (minimum or maximum).
         kind: Extremum,
     },
-    /// filter conditions
+    /// Node that filters its input to only rows where a particular expression evaluates to a
+    /// truthy value.
+    ///
+    /// Converted to [`Filter`] when lowering to dataflow.
+    ///
+    /// [`Filter`]: dataflow::ops::filter::Filter
     Filter {
+        /// Condition to filter on.
+        ///
+        /// Note that at this point this is still just the raw AST, so column references use only
+        /// name and table (and don't support aliases).
         conditions: Expression,
     },
-    /// no extra info required
+    /// Node which makes no changes to its input
+    ///
+    /// Converted to [`Identity`] when lowering to dataflow.
+    ///
+    /// [`Identity`]: dataflow::ops::identity::Identity
     Identity,
-    /// left node, right node, on left columns, on right columns, emit columns
+    /// Node which computes a join on its two parents by finding all rows in the left where the
+    /// values in `on_left` are equal to the values of `on_right` on the right
+    ///
+    /// Converted to [`Join`] with [`JoinType::Inner`] when lowering to dataflow.
+    ///
+    /// [`Join`]: dataflow::ops::join::Join
+    /// [`JoinType::Inner`]: dataflow::ops::join::JoinType::Inner
     Join {
+        /// Columns in the first parent to use as the join key.
+        ///
+        /// # Invariants
+        ///
+        /// * This must have the same length as `on_right`
         on_left: Vec<Column>,
+        /// Columns in the second parent to use as the join key.
+        ///
+        /// # Invariants
+        ///
+        /// * This must have the same length as `on_left`
         on_right: Vec<Column>,
+        /// Columns (from both parents) to project in the output.
         project: Vec<Column>,
     },
     /// JoinAggregates is a special type of join for joining two aggregates together. This is
@@ -64,10 +115,27 @@ pub enum MirNodeInner {
     /// unique column (the actual aggregate columns) from each aggregate node, and a single
     /// version of each group_by column in the final join.
     JoinAggregates,
-    /// on left column, on right column, emit columns
+    /// Node which computes a *left* join on its two parents by finding all rows in the right where
+    /// the values in `on_right` are equal to the values of `on_left` on the left
+    ///
+    /// Converted to [`Join`] with [`JoinType::Left`] when lowering to dataflow.
+    ///
+    /// [`Join`]: dataflow::ops::join::Join
+    /// [`JoinType::Left`]: dataflow::ops::join::JoinType::Left
     LeftJoin {
+        /// Columns in the first parent to use as the join key.
+        ///
+        /// # Invariants
+        ///
+        /// * This must have the same length as `on_right`
         on_left: Vec<Column>,
+        /// Columns in the second parent to use as the join key.
+        ///
+        /// # Invariants
+        ///
+        /// * This must have the same length as `on_left`
         on_right: Vec<Column>,
+        /// Columns (from both parents) to project in the output.
         project: Vec<Column>,
     },
     /// Join where nodes in the right-hand side depend on columns in the left-hand side
@@ -81,22 +149,50 @@ pub enum MirNodeInner {
     ///
     /// [hyper-joins]: http://btw2017.informatik.uni-stuttgart.de/slidesandpapers/F1-10-37/paper_web.pdf
     DependentJoin {
+        /// Columns in the first parent to use as the join key.
+        ///
+        /// # Invariants
+        ///
+        /// * This must have the same length as `on_right`
         on_left: Vec<Column>,
+        /// Columns in the second parent to use as the join key.
+        ///
+        /// # Invariants
+        ///
+        /// * This must have the same length as `on_left`
         on_right: Vec<Column>,
+        /// Columns (from both parents) to project in the output.
         project: Vec<Column>,
     },
     /// group columns
     // currently unused
     #[allow(dead_code)]
-    Latest {
-        group_by: Vec<Column>,
-    },
-    /// emit columns
+    Latest { group_by: Vec<Column> },
+    /// Node which outputs a subset of columns from its parent in any order, and can evaluate
+    /// expressions.
+    ///
+    /// Project nodes always emit columns first, then expressions, then literals.
+    ///
+    /// Converted to [`Project`] when lowering to dataflow.
+    ///
+    /// [`Project`]: dataflow::ops::project::Project
     Project {
+        /// List of columns, in order, to emit verbatim from the parent
         emit: Vec<Column>,
+        /// List of pairs of `(alias, expr)`, giving expressions to evaluate and the names for the
+        /// columns for the results of those expressions.
+        ///
+        /// Note that at this point these expressions are still just raw AST, so column references
+        /// use only name and table (and don't support aliases).
         expressions: Vec<(SqlIdentifier, Expression)>,
+        /// List of pairs of `(alias, value)`, giving literal values to emit in the output
         literals: Vec<(SqlIdentifier, DataType)>,
     },
+    /// Node which computes a union of all of its (two or more) parents.
+    ///
+    /// Converted to [`Union`] when lowering to dataflow
+    ///
+    /// [`Union`]: dataflow::ops::union::Union
     Union {
         /// Columns to emit from each parent
         ///
@@ -104,19 +200,31 @@ pub enum MirNodeInner {
         ///
         /// * This will always have the same length as the number of parents
         emit: Vec<Vec<Column>>,
+        /// Specification for how the union operator should operate with respect to rows that exist
+        /// in all parents.
         duplicate_mode: union::DuplicateMode,
     },
-    /// MirNodeInner used to create a Paginate dataflow Node, based on whether the node has
-    /// an offset.
+    /// Node which orders its input rows within a group, then emits an extra page number column
+    /// (which will always have a name given by [`PAGE_NUMBER_COL`]) for the page number of the
+    /// rows within that group, with page size given by `limit`.
+    ///
+    /// Converted to [`Paginate`] when lowering to dataflow.
+    ///
+    /// [`PAGE_NUMBER_COL`]: crate::PAGE_NUMBER_COL
+    /// [`Paginate`]: dataflow::ops::paginate::Paginate
     Paginate {
         /// Set of columns used for ordering the results
         order: Option<Vec<(Column, OrderType)>>,
         /// Set of columns that are indexed to form a unique grouping of results
         group_by: Vec<Column>,
-        /// Numeric literal that determines the number of results stored per group
+        /// How many rows per page
         limit: usize,
     },
-    /// MirNodeInner used to create a TopK dataflow Node
+    /// Node which emits only the top `limit` records per group, ordered by a set of columns
+    ///
+    /// Converted to [`TopK`] when lowering to dataflow.
+    ///
+    /// [`TopK`]: dataflow::ops::topk::TopK
     TopK {
         /// Set of columns used for ordering the results
         order: Option<Vec<(Column, OrderType)>>,
@@ -126,21 +234,31 @@ pub enum MirNodeInner {
         /// LIMIT clause
         limit: usize,
     },
-    // Get the distinct element sorted by a specific column
-    Distinct {
-        group_by: Vec<Column>,
-    },
-    /// reuse another node
+    /// Node which emits only distinct rows per some group.
+    ///
+    /// Converted to [`Aggregator`] with [`Aggregation::Count`] when lowering to dataflow.
+    ///
+    /// [`Aggregator`]: dataflow::ops::grouped::aggregate::Aggregator
+    /// [`Aggregation::Count`]: dataflow::ops::grouped::aggregate::Aggregation::Count
+    Distinct { group_by: Vec<Column> },
+    /// Reuse a node that's already in the graph.
+    ///
+    /// Note that this node is used even when query graph reuse is disabled, for the base nodes in
+    /// newly created query graphs (either querying from a base table or an explicit named `VIEW`)
     Reuse {
+        /// Reference to the node to reuse
         node: MirNodeRef,
     },
     /// Alias all columns in the query to change their table
     ///
     /// This node will not be converted into a dataflow node when lowering MIR to dataflow.
-    AliasTable {
-        table: SqlIdentifier,
-    },
-    /// leaf (reader) node, keys
+    AliasTable { table: SqlIdentifier },
+    /// Leaf node of a query, which specifies the columns to index on, and an optional set of
+    /// operations to perform post-lookup.
+    ///
+    /// Converted to a [`Reader`] node when lowering to dataflow.
+    ///
+    /// [`Reader`]: dataflow::node::special::reader::Reader
     Leaf {
         /// Keys is a tuple of the key column, and if the column was derived from a SQL
         /// placeholder, the index of the placeholder in the SQL query.
