26fb97f6d960819311e59cb394def0c9e5a297f9
Griffin Smith <griffin@readyset.io>
2022-05-06 11:40:12 -0400

    ci: Group output of darwin installer build
    Since we're running two large, expensive builds here, it's nice to have
    the output grouped by each build so it's easier to tell at a glance how
    far along the step is.
    
    Change-Id: I3c982a734eb3287ff5ea36584136cca05e18b52d
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/2352
    Tested-by: Buildkite CI
    Reviewed-by: Harley Klein <harley@readyset.io>

--- .buildkite/build-apple-installer.sh
+++ .buildkite/build-apple-installer.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-set -eux
+set -eu
 
 rustup target add x86_64-apple-darwin
+echo "+++ Running build for x86_64-apple-darwin"
 RUSTFLAGS='-D warnings' cargo build --release --bin readyset-installer --target=x86_64-apple-darwin
+echo "+++ Running build for aarch64-apple-darwin"
 RUSTFLAGS='-D warnings' cargo build --release --bin readyset-installer --target=aarch64-apple-darwin
+echo "+++ Stripping binaries"
 strip target/x86_64-apple-darwin/release/readyset-installer
 strip target/aarch64-apple-darwin/release/readyset-installer
