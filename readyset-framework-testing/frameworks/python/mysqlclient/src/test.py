0b67882c70643af76f34b045e5147bf9ab1a8a00
Peter Farr <peter@readyset.io>
2022-05-23 16:54:53 -0700

    Fix python/mysqlclient test
    This fixes the python/mysqlclient test. I had thought that I had
    previously fixed the test. However, on top of us returning a different
    order than MySQL would if there is no ORDER BY clause, it turned out
    that after fixing that there was an eventual consistency bug, whereby
    the python test checked for a SELECT before we caught up. This oddly
    enough only presents itself after and UPDATE or DELETE (perhaps these
    take longer to replicate?).
    
    This updates the framework test by including a new method that
    continually checks for results up to a given time limit until we
    succeed.
    
    Change-Id: I791789f009631e85d5e9ce408cd775bb46d28134
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/2415
    Tested-by: Buildkite CI
    Reviewed-by: Griffin Smith <griffin@readyset.io>

--- readyset-framework-testing/frameworks/python/mysqlclient/src/test.py
+++ readyset-framework-testing/frameworks/python/mysqlclient/src/test.py
@@ -1,84 +1,107 @@
 import os
+import time
+import timeit
 import urllib.parse
 
 import MySQLdb
 
 args = {}
 
-args['host'] = os.environ.get('RS_HOST')
-if(args['host'] is None):
-	print('RS_HOST must be set')
-	os.Exit(1)
+args["host"] = os.environ.get("RS_HOST")
+if args["host"] is None:
+    print("RS_HOST must be set")
+    os.Exit(1)
 
-port = os.environ.get('RS_PORT')
-if(port is None):
-	args['port'] = 3306
+port = os.environ.get("RS_PORT")
+if port is None:
+    args["port"] = 3306
 else:
-	args['port'] = int(port)
+    args["port"] = int(port)
 
-args['user'] = os.environ.get('RS_USERNAME')
-if(args['user'] is None):
-	print('RS_USERNAME must be set')
-	os.Exit(2)
+args["user"] = os.environ.get("RS_USERNAME")
+if args["user"] is None:
+    print("RS_USERNAME must be set")
+    os.Exit(2)
 
-password = os.environ.get('RS_PASSWORD')
-if(password is not None):
-	args['passwd'] = password
+password = os.environ.get("RS_PASSWORD")
+if password is not None:
+    args["passwd"] = password
 
-args['db'] = os.environ.get('RS_DATABASE')
-if(args['db'] is None):
-	print('RS_DATABASE must be set')
-	os.Exit(3)
+args["db"] = os.environ.get("RS_DATABASE")
+if args["db"] is None:
+    print("RS_DATABASE must be set")
+    os.Exit(3)
 
 connection = MySQLdb.connect(**args)
 
+
+def check_people(expected_results):
+    with connection.cursor() as cursor:
+        query = "SELECT * FROM people ORDER BY id"
+        cursor.execute(query)
+        for result in expected_results:
+            if cursor.fetchone() != result:
+                return False
+        if cursor.fetchone() is not None:
+            return False
+        return True
+
+
+def eventually_consistent_people(expected_results, seconds):
+    start = timeit.default_timer()
+    while not check_people(expected_results):
+        now = timeit.default_timer()
+        if now - start > seconds:
+            return False
+        time.sleep(0.1)
+    return True
+
+
 with connection.cursor() as cursor:
-	query = 'CREATE TABLE people (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL)'
-	cursor.execute(query)
+    query = "CREATE TABLE people (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL)"
+    cursor.execute(query)
 connection.commit()
 
+
 with connection.cursor() as cursor:
-	query = 'INSERT INTO people VALUES (DEFAULT, %s)'
-	cursor.execute(query, ('ReadySet User 1',))
-	cursor.execute(query, ('ReadySet User 2',))
-	cursor.execute(query, ('ReadySet User 3',))
+    query = "INSERT INTO people VALUES (DEFAULT, %s)"
+    cursor.execute(query, ("ReadySet User 1",))
+    cursor.execute(query, ("ReadySet User 2",))
+    cursor.execute(query, ("ReadySet User 3",))
 connection.commit()
 
-with connection.cursor() as cursor:
-	query = 'SELECT * FROM people ORDER BY id'
-	cursor.execute(query)
-	assert(cursor.fetchone() == (1, 'ReadySet User 1'))
-	assert(cursor.fetchone() == (2, 'ReadySet User 2'))
-	assert(cursor.fetchone() == (3, 'ReadySet User 3'))
-	assert(cursor.fetchone() is None)
+
+success = eventually_consistent_people(
+    [(1, "ReadySet User 1"), (2, "ReadySet User 2"), (3, "ReadySet User 3")], 5
+)
+assert success
+
 
 with connection.cursor() as cursor:
-	query = 'UPDATE people SET name = %s WHERE id = %s'
-	cursor.execute(query, ('NotReadySet User', 2))
+    query = "UPDATE people SET name = %s WHERE id = %s"
+    cursor.execute(query, ("NotReadySet User", 2))
 connection.commit()
 
-with connection.cursor() as cursor:
-	query = 'SELECT * FROM people ORDER BY id'
-	cursor.execute(query)
-	assert(cursor.fetchone() == (1, 'ReadySet User 1'))
-	assert(cursor.fetchone() == (2, 'NotReadySet User'))
-	assert(cursor.fetchone() == (3, 'ReadySet User 3'))
-	assert(cursor.fetchone() is None)
+
+success = eventually_consistent_people(
+    [(1, "ReadySet User 1"), (2, "NotReadySet User"), (3, "ReadySet User 3")], 5
+)
+assert success
+
 
 with connection.cursor() as cursor:
-	query = 'DELETE FROM people WHERE id = %s'
-	cursor.execute(query, (2,))
+    query = "DELETE FROM people WHERE id = %s"
+    cursor.execute(query, (2,))
 connection.commit()
 
-with connection.cursor() as cursor:
-	query = 'SELECT * FROM people ORDER BY id'
-	cursor.execute(query)
-	assert(cursor.fetchone() == (1, 'ReadySet User 1'))
-	assert(cursor.fetchone() == (3, 'ReadySet User 3'))
-	assert(cursor.fetchone() is None)
+
+success = eventually_consistent_people(
+    [(1, "ReadySet User 1"), (3, "ReadySet User 3")], 5
+)
+assert success
+
 
 with connection.cursor() as cursor:
-	query = 'DROP TABLE people'
-	cursor.execute(query)
+    query = "DROP TABLE people"
+    cursor.execute(query)
 connection.commit()
-
