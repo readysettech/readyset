bc6a0adad5fbea3e5b0ba68bc16b4b4621dc0566
Griffin Smith <griffin@readyset.io>
2022-06-09 10:16:04 -0400

    noria-client-metrics: Fix doc link in recorded
    `DatabaseType` was using the wrong kinds of quotes for a link, plus the
    link wasn't resolving since the identifier wasn't in scope.
    
    Also, fixes some indentation, typos and wrapping while I'm here.
    
    Change-Id: Ie7524449839b95ecf63016cd1e1bec6be6b66eb8
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/2526
    Tested-by: Buildkite CI
    Reviewed-by: Corbin Hall <sif@readyset.io>

--- noria-client/metrics/src/recorded.rs
+++ noria-client/metrics/src/recorded.rs
@@ -1,15 +1,16 @@
 //! Documents the set of metrics that are currently being recorded within
 //! a noria-client.
 
-/// Histogram: The time in seconds that the database spent
-/// executing a query.
+/// Histogram: The time in seconds that the database spent executing a query.
 ///
 /// | Tag | Description |
 /// | --- | ----------- |
 /// | query | The query text being executed. |
-/// | database_type | The database type being executed. Must be a ['DatabaseType'] |
+/// | database_type | The database type being executed. Must be a [`DatabaseType`] |
 /// | query_type | SqlQueryType, whether the query was a read or write. |
 /// | event_type | EventType, whether the query was a prepare, execute, or query.  |
+///
+/// [`DatabaseType`]: crate::DatabaseType
 pub const QUERY_LOG_EXECUTION_TIME: &str = "query-log.execution_time";
 
 /// Histogram: The time in seconds that the database spent executing a
@@ -36,15 +37,14 @@ pub const QUERY_LOG_TOTAL_CACHE_MISSES: &str = "query-log.total_cache_misses";
 /// | query | The query text being executed. |
 pub const QUERY_LOG_QUERY_CACHE_MISSED: &str = "query-log.query_cache_missed";
 
-/// Counter: The total number of queries processing by the migration handler.
-/// Incremented on each loop of the migration handler.
+/// Counter: The total number of queries processing by the migration handler.  Incremented on each
+/// loop of the migration handler.
 pub const MIGRATION_HANDLER_PROCESSED: &str = "migration-handler.processed";
 
-/// Counter: The number of queries themigration handler has set to allowed.
-/// Incremented on each loop of the migration handler.
-/// TODO(justin): In the future it would be good to support gauges for the
-/// counts of each query status in the query status cache. Requires
-/// optimization of locking.
+/// Counter: The number of queries the migration handler has set to allowed.  Incremented on each
+/// loop of the migration handler.
+/// TODO(justin): In the future it would be good to support gauges for the counts of each query
+/// status in the query status cache. Requires optimization of locking.
 pub const MIGRATION_HANDLER_ALLOWED: &str = "migration-handler.allowed";
 
 /// Counter: The number of HTTP requests received at the noria-client.
