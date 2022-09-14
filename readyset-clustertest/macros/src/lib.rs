96105444f581a1a651bbda8d275351fbe8999f92
Luke Osborne <luke.o@readyset.io>
2022-09-14 13:05:45 -0400

    clustertest: Add tracing support
    This commit adds readyset-tracing and tracing as dependencies to
    readyset-clustertests so that they can be used to instrument the
    clustertest with helpful log messages.
    
    It also extends the #[clustertest] macro to always the test logging and
    print out the name of the test being run as a DEBUG log when the tests
    start.
    
    Change-Id: Ib36f96e1c50337d88d8c66b20cf2e64c797fdc7a
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/3110
    Tested-by: Buildkite CI
    Reviewed-by: Griffin Smith <griffin@readyset.io>

--- readyset-clustertest/macros/src/lib.rs
+++ readyset-clustertest/macros/src/lib.rs
@@ -1,16 +1,32 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, parse_quote, ItemFn};
 
 #[proc_macro_attribute]
 pub fn clustertest(_attr: TokenStream, input: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(input as ItemFn);
 
+    let fn_block = *input_fn.block;
+    let fn_name = input_fn.sig.ident.to_string();
+
+    // Enable readyset_tracing test logging for all #[clustertest]s and log the test name as a debug
+    // log
+    let fn_block_with_tracing = parse_quote! {{
+        ::readyset_tracing::init_test_logging();
+        tracing::debug!("Starting {}", #fn_name);
+        #fn_block
+    }};
+
+    let test_with_tracing = ItemFn {
+        block: Box::new(fn_block_with_tracing),
+        ..input_fn
+    };
+
     let result = quote! {
         #[tokio::test(flavor = "multi_thread")]
         #[serial]
-        #input_fn
+        #test_with_tracing
     };
     result.into()
 }
