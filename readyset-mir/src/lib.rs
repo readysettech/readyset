272b9662d5f21395800a28a00b1bc2a2152269f2
Francisco Noriega <fran@readyset.io>
2022-11-16 00:51:38 -0300

    mir: Remove `MirNodeRef` and `MirNodeWeakRef`.
    Those types are not used anymore. Goodbye!
    
    Refs: ENG-1638
    Change-Id: I40ff96ceeb9974554b8982fc4e731ce7291612a7
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/3764
    Tested-by: Buildkite CI
    Reviewed-by: Griffin Smith <griffin@readyset.io>

--- readyset-mir/src/lib.rs
+++ readyset-mir/src/lib.rs
@@ -43,9 +43,6 @@
 #![deny(unused_extern_crates, macro_use_extern_crate)]
 #![feature(stmt_expr_attributes, box_patterns)]
 
-use std::cell::RefCell;
-use std::rc::{Rc, Weak};
-
 pub use column::Column;
 use lazy_static::lazy_static;
 use nom_sql::SqlIdentifier;
@@ -58,9 +55,6 @@ pub mod node;
 pub mod query;
 pub(crate) mod rewrite;
 pub mod visualize;
-
-pub type MirNodeRef = Rc<RefCell<node::MirNode>>;
-pub type MirNodeWeakRef = Weak<RefCell<node::MirNode>>;
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 pub enum FlowNode {
