848f563f033127fd386d0ad98e1e1004c23c8c64
Nikolai Vazquez <nikolai@readyset.io>
2022-12-12 12:20:43 -0500

    dataflow-expression: Fix caller tracking for `[try_]eval_expr`
    Change Idc5909708f81a428d6fa1c25a547f4dc28f38cc2 made it such that we
    get better error messages on failure. However, because of the panic
    happening in a closure, `#[track_caller]` did not get propagated.
    
    This commit fixes the error reporting to track the function caller by
    having the panic be invoked in a `match` of the `Result` values.
    
    Change-Id: I850aad70532b7d3d2ffd23e1920317e0c71786df
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/3993
    Reviewed-by: Griffin Smith <griffin@readyset.io>
    Tested-by: Buildkite CI

--- dataflow-expression/src/eval.rs
+++ dataflow-expression/src/eval.rs
@@ -329,27 +329,29 @@ mod tests {
     /// Note that parsing is expected to succeed, since this is strictly meant to test evaluation.
     #[track_caller]
     pub(crate) fn try_eval_expr(expr: &str, dialect: nom_sql::Dialect) -> ReadySetResult<DfValue> {
-        let ast = parse_expr(dialect, expr).unwrap_or_else(|error| eval_expr_fail(expr, error));
+        let ast = expr_unwrap(parse_expr(dialect, expr), expr);
 
         let expr_dialect = match dialect {
             PostgreSQL => crate::Dialect::DEFAULT_POSTGRESQL,
             MySQL => crate::Dialect::DEFAULT_MYSQL,
         };
 
-        Expr::lower(ast, expr_dialect, no_op_lower_context())
-            .unwrap_or_else(|error| eval_expr_fail(expr, error))
+        expr_unwrap(Expr::lower(ast, expr_dialect, no_op_lower_context()), expr)
             .eval::<DfValue>(&[])
     }
 
     /// Returns the value from evaluating an expression, panicking if parsing or evaluation fails.
     #[track_caller]
     pub(crate) fn eval_expr(expr: &str, dialect: nom_sql::Dialect) -> DfValue {
-        try_eval_expr(expr, dialect).unwrap_or_else(|error| eval_expr_fail(expr, error))
+        expr_unwrap(try_eval_expr(expr, dialect), expr)
     }
 
     #[track_caller]
-    fn eval_expr_fail(expr: &str, error: impl std::fmt::Display) -> ! {
-        panic!("Failed to evaluate `{expr}`: {error}")
+    fn expr_unwrap<T>(result: Result<T, impl std::fmt::Display>, expr: &str) -> T {
+        match result {
+            Ok(value) => value,
+            Err(error) => panic!("Failed to evaluate `{expr}`: {error}"),
+        }
     }
 
     #[test]
