7ce6f7dc4fb8ef7c30c8488077309d8cdc32a77b
Dan Wilbanks <dan@readyset.io>
2022-12-14 17:08:56 +0000

    Inlining: Create InlineLiterals sql pass
    Creates a SQL pass to inline a set of literals into a query, given a
    mapping of dollar number placeholder indices and the literal to inline.
    
    Change-Id: Ic868419d21703d1cb3f1e252329486816a1e1b9c
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/4127
    Tested-by: Buildkite CI
    Reviewed-by: Griffin Smith <griffin@readyset.io>
    Reviewed-by: Fran Noriega <fran@readyset.io>

---
+++ readyset-sql-passes/src/inline_literals.rs
@@ -0,0 +1,64 @@
+use std::collections::HashMap;
+
+use nom_sql::analysis::visit_mut::VisitorMut;
+use nom_sql::{ItemPlaceholder, Literal, SelectStatement};
+
+struct InlineLiteralsVisitor<'a> {
+    placeholder_literal_map: &'a HashMap<u32, Literal>,
+}
+
+pub trait InlineLiterals {
+    fn inline_literals(self, placeholder_literal_map: &HashMap<u32, Literal>) -> Self;
+}
+
+impl<'ast, 'a> VisitorMut<'ast> for InlineLiteralsVisitor<'a> {
+    type Error = !;
+    fn visit_literal(&mut self, literal: &'ast mut Literal) -> Result<(), Self::Error> {
+        if let Literal::Placeholder(ItemPlaceholder::DollarNumber(p)) = literal {
+            if let Some(inlined_literal) = self.placeholder_literal_map.get(p) {
+                *literal = inlined_literal.clone();
+            }
+        }
+        Ok(())
+    }
+}
+
+impl InlineLiterals for SelectStatement {
+    fn inline_literals(mut self, placeholder_literal_map: &HashMap<u32, Literal>) -> Self {
+        let mut visitor = InlineLiteralsVisitor {
+            placeholder_literal_map,
+        };
+        let Ok(()) = visitor.visit_select_statement(&mut self);
+        self
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use nom_sql::Literal;
+
+    use super::InlineLiterals;
+    use crate::util::parse_select_statement;
+
+    #[test]
+    fn replaces_literals() {
+        let select = parse_select_statement(
+            "SELECT $2, a FROM t WHERE t.a = $3 GROUP BY t.b HAVING sum(t.b) = $1",
+        );
+        let literals = vec![
+            (1, Literal::String("one".into())),
+            (2, Literal::UnsignedInteger(2)),
+            (3, Literal::UnsignedInteger(3)),
+        ]
+        .into_iter()
+        .collect::<HashMap<_, _>>();
+        let expected = parse_select_statement(
+            "SELECT 2, a FROM t WHERE t.a = 3 GROUP BY t.b HAVING sum(t.b) = 'one'",
+        );
+
+        let select = select.inline_literals(&literals);
+        assert_eq!(select, expected);
+    }
+}
