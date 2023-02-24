03755323b8911289a51578a7cfab8a92ddafda7e
Griffin Smith <griffin@readyset.io>
2023-02-24 09:38:42 -0500

    benchmarks/query_slammer: Support postgres
    Use the database_utils crate in the query_slammer binary, to allow
    slamming a postgres server with queries in addition to a mysql server.
    Since database_utils doesn't support connection pools, this replaces the
    connection pooling with just having each task we spawn open its own
    connection (which is likely faster anyway, since it avoids
    synchronization).
    
    Change-Id: I82cd5ac5e3a5a1c31529c1a1ad38fe8394ed6bca
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/4369
    Tested-by: Buildkite CI
    Reviewed-by: Dan Wilbanks <dan@readyset.io>

--- benchmarks/src/bin/query_slammer.rs
+++ benchmarks/src/bin/query_slammer.rs
@@ -3,14 +3,15 @@ use std::time::Duration;
 use atomic_counter::{AtomicCounter, RelaxedCounter};
 use chrono::Utc;
 use clap::Parser;
+use database_utils::DatabaseURL;
 use futures::future::select_all;
-use mysql_async::prelude::*;
+use readyset_data::DfValue;
 
 #[derive(Parser, Debug)]
 struct Opts {
-    /// MySQL endpoint to connect to; you probably want a noria-mysql adapter
-    #[clap(long, env = "DATABASE_URL")]
-    database_url: String,
+    /// SQL endpoint to connect to
+    #[clap(env = "DATABASE_URL", parse(try_from_str))]
+    database_url: DatabaseURL,
 
     /// Number of greenthreads to run hitting the database
     #[clap(long, env = "PARALLELISM", short = 'j', default_value = "16")]
@@ -18,23 +19,21 @@ struct Opts {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
-    let pool = mysql_async::Pool::new(opts.database_url.as_ref());
-
     let counter: &'static RelaxedCounter = Box::leak(Box::new(RelaxedCounter::new(0)));
 
     let mut tasks = Vec::with_capacity(opts.parallelism + 2);
     for _i in 0..opts.parallelism {
-        let mut conn = pool.get_conn().await.unwrap();
+        let mut conn = opts.database_url.connect().await?;
         let counter = counter;
         let handle = tokio::task::spawn(async move {
             let mut i = 600000;
             loop {
-                let _invites: Result<Vec<mysql_async::Row>, _> = "SELECT * FROM invites_users WHERE invites_users.invite_id IN (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) AND invites_users.user_id = ?"
-                    .with((i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9, i + 10))
-                    .fetch(&mut conn)
-                    .await;
+                let _ = conn
+                    .execute_str::<Vec<u32>, DfValue>("SELECT * FROM t", vec![])
+                    .await
+                    .unwrap();
                 counter.inc();
                 i += 1;
                 if i > 7000000 {
@@ -71,4 +70,6 @@ async fn main() {
         "--- QPS:  {}",
         counter.get() / duration.num_seconds() as usize
     );
+
+    Ok(())
 }
