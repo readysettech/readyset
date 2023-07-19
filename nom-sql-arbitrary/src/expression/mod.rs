83bd54a1d5127b994bf3ab3aa870ae0b13d94e57
Francisco Noriega <fran@readyset.io>
2023-07-19 10:21:24 -0300

    genexpr: Boilerplate code for recursive `Expr` generation
    This commit introduces the `ExprStrategy` struct, which holds different
    proptest `Strategy`s for `Expr`, for each distinct type.
    This kind of structure is needed to generate recursive `Expr`s, since
    recursion can't be done over a single type alone.
    For example, it is not possible to generate boolean `Expr`s using only
    boolean `Expr`s, since `(1 + 1) = 1` would require int (recursive
    as well) `Expr`s.
    
    So recursion happens at the `ExprStrategy` level, where the struct
    itself provides access to the different `Expr` types it generates during
     recursion.
    
    For now, the recursion step doesn't alter the `Expr` strategies at all,
    but it will on future commits.
    
    Refs: REA-2603
    Change-Id: I31dbd98ea551c8824999b878d6e4889d31b33401
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/5439
    Tested-by: Buildkite CI
    Reviewed-by: Nick Marino <nick@readyset.io>

--- nom-sql-arbitrary/src/expression/mod.rs
+++ nom-sql-arbitrary/src/expression/mod.rs
@@ -1,4 +1,51 @@
 //! Module that provides proptest [`Strategy`]s for
 //! [`Expr`] AST structs.
+// TODO(fran): Generate more complex `Expr`:
+//   - Arrays
+//   - JSON
+use nom_sql::Expr;
+use proptest::arbitrary::{any, any_with};
+use proptest::strategy::{BoxedStrategy, Just, Strategy};
+
+use crate::expression::expr::{generate_base_expr, ExprSpec};
+
 mod expr;
 mod util;
+
+/// A struct that holds the [`Strategy`]s to generate
+/// [`Expr`] of different kinds.
+// TODO(fran): Expand the kinds of `Expr` being generated
+#[derive(Debug, Clone)]
+pub struct ExprStrategy {
+    pub bool: BoxedStrategy<Expr>,
+    pub string: BoxedStrategy<Expr>,
+    pub integer: BoxedStrategy<Expr>,
+    pub float: BoxedStrategy<Expr>,
+    pub timestamp: BoxedStrategy<Expr>,
+}
+
+/// Produces a [`Strategy`] that generates [`ExprStrategy`] structs, where
+/// the underlying [`Expr`] of said structs get recursively more complex.
+#[allow(dead_code)]
+fn arbitrary_expr_strategy() -> impl Strategy<Value = ExprStrategy> {
+    Just(ExprStrategy {
+        bool: generate_base_expr(ExprSpec::Bool).boxed(),
+        string: generate_base_expr(ExprSpec::String(
+            any_with::<String>("[a-zA-Z0-9]{0,8}".into()).boxed(),
+        ))
+        .boxed(),
+        integer: generate_base_expr(ExprSpec::UnsignedInt(any::<u32>().boxed())).boxed(),
+        float: generate_base_expr(ExprSpec::Float(any::<f32>().boxed())).boxed(),
+        timestamp: generate_base_expr(ExprSpec::Timestamp).boxed(),
+    })
+    .prop_recursive(3, 32, 3, move |inner| {
+        inner.prop_map(move |e| ExprStrategy {
+            // TODO(fran): Replace this with respective recursive function for each type
+            bool: e.bool,
+            string: e.string,
+            integer: e.integer,
+            float: e.float,
+            timestamp: e.timestamp,
+        })
+    })
+}
