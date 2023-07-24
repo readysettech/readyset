96469575b02fdb46e469c91083b21ec121ee9a6b
Nick Marino <nick@readyset.io>
2023-07-24 16:37:38 -0400

    psql-srv: Implement FromSql for PsqlValue
    This is not used yet, but the intent is for this to take the place of
    the current code path that is used for the proxy code where we call
    DfValue::from_sql followed by PsqlValue::try_from (with an intermediate
    step of wrapping in TypedDfValue). As such it's intended to basically be
    a composition of those two functions, but avoiding the intermediate
    DfValue step lets us save a lot of redundant encode/decode work.
    
    Refs: REA-3143
    Change-Id: If465b651034a0a6f93ee6ab10686cccfa283718a
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/5538
    Tested-by: Buildkite CI
    Reviewed-by: Jason Brown <jason.b@readyset.io>
    Reviewed-by: Aspen Smith <aspen@readyset.io>

--- psql-srv/src/value.rs
+++ psql-srv/src/value.rs
@@ -1,7 +1,10 @@
+use std::error::Error;
+
 use bit_vec::BitVec;
 use chrono::{DateTime, FixedOffset, NaiveDate, NaiveDateTime, NaiveTime};
 use cidr::IpInet;
 use eui48::MacAddress;
+use postgres_types::{FromSql, Kind, Type};
 use readyset_data::{Array, Text};
 use rust_decimal::Decimal;
 use uuid::Uuid;
@@ -35,6 +38,74 @@ pub enum PsqlValue {
     Jsonb(serde_json::Value),
     Bit(BitVec),
     VarBit(BitVec),
-    Array(Array, postgres_types::Type),
+    Array(Array, Type),
     PassThrough(readyset_data::PassThrough),
+}
+
+impl<'a> FromSql<'a> for PsqlValue {
+    fn from_sql(ty: &Type, raw: &'a [u8]) -> Result<Self, Box<dyn Error + Sync + Send>> {
+        match ty.kind() {
+            Kind::Array(_) => Array::from_sql(ty, raw).map(|a| PsqlValue::Array(a, ty.clone())),
+            Kind::Enum(_) => Text::try_from(raw)
+                .map(PsqlValue::Text)
+                .map_err(|e| e.into()),
+            _ => match *ty {
+                // Note we have arms here for every PsqlValue variant except OID. We have an OID
+                // variant of PsqlValue defined, but we don't support that type, so we'll handle it
+                // in the catchall passthrough arm at the end of this match block.
+                Type::BOOL => bool::from_sql(ty, raw).map(PsqlValue::Bool),
+                Type::VARCHAR => <&str>::from_sql(ty, raw).map(|s| PsqlValue::VarChar(s.into())),
+                Type::NAME => <&str>::from_sql(ty, raw).map(|s| PsqlValue::Name(s.into())),
+                Type::BPCHAR => <&str>::from_sql(ty, raw).map(|s| PsqlValue::BpChar(s.into())),
+                Type::CHAR => i8::from_sql(ty, raw).map(PsqlValue::Char),
+                Type::INT4 => i32::from_sql(ty, raw).map(PsqlValue::Int),
+                Type::INT8 => i64::from_sql(ty, raw).map(PsqlValue::BigInt),
+                Type::INT2 => i16::from_sql(ty, raw).map(PsqlValue::SmallInt),
+                Type::FLOAT8 => f64::from_sql(ty, raw).map(PsqlValue::Double),
+                Type::FLOAT4 => f32::from_sql(ty, raw).map(PsqlValue::Float),
+                Type::NUMERIC => {
+                    // rust-decimal has a bug whereby it will successfully deserialize from the
+                    // Postgres binary format NUMERIC values with scales in [0, 255], but it will
+                    // panic when serializing them to bincode if they are outside [0, 28].
+                    let d = Decimal::from_sql(ty, raw)?;
+                    if d.scale() > 28 {
+                        Err(format!(
+                            "Unsupported scale {} on NUMERIC value - \
+                             max supported by ReadySet is 28",
+                            d.scale(),
+                        )
+                        .into())
+                    } else {
+                        Ok(PsqlValue::Numeric(d))
+                    }
+                }
+                Type::TEXT => <&str>::from_sql(ty, raw).map(|s| PsqlValue::Text(s.into())),
+                Type::TIMESTAMP => NaiveDateTime::from_sql(ty, raw).map(PsqlValue::Timestamp),
+                Type::TIMESTAMPTZ => <chrono::DateTime<chrono::FixedOffset>>::from_sql(ty, raw)
+                    .map(PsqlValue::TimestampTz),
+                Type::DATE => NaiveDate::from_sql(ty, raw).map(PsqlValue::Date),
+                Type::TIME => NaiveTime::from_sql(ty, raw).map(PsqlValue::Time),
+                Type::BYTEA => <Vec<u8>>::from_sql(ty, raw).map(PsqlValue::ByteArray),
+                Type::MACADDR => MacAddress::from_sql(ty, raw).map(PsqlValue::MacAddress),
+                Type::INET => IpInet::from_sql(ty, raw).map(PsqlValue::Inet),
+                Type::UUID => Uuid::from_sql(ty, raw).map(PsqlValue::Uuid),
+                Type::JSON => serde_json::Value::from_sql(ty, raw).map(PsqlValue::Json),
+                Type::JSONB => serde_json::Value::from_sql(ty, raw).map(PsqlValue::Jsonb),
+                Type::BIT => BitVec::from_sql(ty, raw).map(PsqlValue::Bit),
+                Type::VARBIT => BitVec::from_sql(ty, raw).map(PsqlValue::VarBit),
+                _ => Ok(PsqlValue::PassThrough(readyset_data::PassThrough {
+                    ty: ty.clone(),
+                    data: Box::from(raw),
+                })),
+            },
+        }
+    }
+
+    fn from_sql_null(_: &Type) -> Result<Self, Box<dyn Error + Sync + Send>> {
+        Ok(PsqlValue::Null)
+    }
+
+    fn accepts(_: &Type) -> bool {
+        true // Anything we don't support, we can still wrap in PsqlValue::PassThrough
+    }
 }
