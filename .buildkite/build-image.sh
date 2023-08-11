605da514861dd3d16b0ae2c5ce8e67fa2a1f1496
Ron Hough <ron@readyset.io>
2023-08-11 14:08:41 -0500

    build: Move public build down into public
    To support buildkite CI builds out of the github repo, everything
    necessary for the CI run needs to live in the github repo.  This update
    moves the pipeline and all supporting scripts and files down under the
    public/ tree.  The main top-level pipeline is adjusted to run the
    public/.buildkite/pipeline.yml on changes to public/.
    
    NOTE: This copies a lot of files from outside public into public, which
    means there's a bunch of duplication (build-image.sh, run-tests.sh,
    etc.).  To remove that duplication, all references from outside public
    would need to be changed to point to the copies in public, and the
    non-public files deleted.
    
    Change-Id: I481c35df137d4902857d1b55c90baa014108639c

---
+++ .buildkite/build-image.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This script is used by Buildkite to standardize a few things about how we
+# build Docker containers that are used for other steps in Buildkite.
+# The script ensures the following things:
+# Docker Buildkit is used for better performance and caching
+# Builds are uploaded to our private AWS Elastic Container Registry
+# When builds are done on main, they are tagged as latest to help with caching.
+#
+# To optimize rebuilds of images that rarely change, set the SKIP_IF_UNCHANGED
+# environment variable to a comma-separated lists of repository-relative paths
+# to files - if none of these files have changed, the image won't actually be
+# built, it'll just be re-tagged with the current commit via the AWS API.
+
+
+dockerfile="$1"
+image_name="$2"
+context="$3"
+
+if [ -z "$dockerfile" ] || [ -z "$image_name" ]; then
+    echo "Usage: $0 <dockerfile> <image_name> <context> [<docker-build-args>...]" >&2
+    exit 1
+fi
+shift 3
+
+AWS_ACCOUNT=${AWS_ACCOUNT:-305232526136} # build
+AWS_REGION=${AWS_REGION:-us-east-2}
+VERSION=${VERSION:-$BUILDKITE_COMMIT}
+
+docker_repo="$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com"
+image="$docker_repo/$image_name"
+
+if [ -n "${SKIP_IF_UNCHANGED+1}" ]; then
+    do_skip="1"
+    changed_files="$(git diff --name-only HEAD~ HEAD)"
+    IFS=',' read -ra files <<< "$SKIP_IF_UNCHANGED,$dockerfile"
+    for fname in "${files[@]}"; do
+        if grep "$fname" <<< "$changed_files" > /dev/null; then
+            do_skip=""
+        fi
+    done
+
+    if [ -n "$do_skip" ]; then
+        echo "+++ :docker: Dockerfile unchanged, retagging latest image as ${BUILDKITE_COMMIT}"
+        set +eo pipefail
+
+        manifest=$(
+            aws ecr batch-get-image \
+                --repository-name "$image_name" \
+                --image-ids imageTag=latest \
+                --output json \
+                | jq -r '.images[0].imageManifest')
+
+        # This might fail if the image already exists (eg if we're
+        # rebuilding the same commit), and we don't want to fail if that's
+        # the case - if the operation fails for *another* reason, we'll just
+        # fail in a later build because the image doesn't exist
+        aws ecr put-image \
+            --repository-name "$image_name" \
+            --image-tag "$BUILDKITE_COMMIT" \
+            --image-manifest "$manifest" || true \
+            > /dev/null
+        exit 0
+    fi
+fi
+
+echo "--- :docker: Pulling $image:latest"
+if docker pull "$image:latest"; then
+    cache_from="--cache-from=$image:latest"
+else
+    echo "Failed to pull previous build of image"
+    cache_from=""
+fi
+
+build_cmd_prefix=(
+    "docker" "build" \
+    "-f" "$dockerfile" \
+    "-t" "$image:$VERSION" \
+    "--build-arg" "BUILDKIT_INLINE_CACHE=1" \
+)
+build_cmd_suffix=(
+    "$@" \
+    "$context"
+)
+if [ -n "$cache_from" ]; then
+    build_cmd=("${build_cmd_prefix[@]}" "$cache_from" "${build_cmd_suffix[@]}")
+else
+    build_cmd=("${build_cmd_prefix[@]}" "${build_cmd_suffix[@]}")
+fi
+
+echo "+++ :docker: Building $image:$VERSION"
+DOCKER_BUILDKIT=1 "${build_cmd[@]}"
+tags_to_push=("$image:$VERSION")
+
+if [ "$BUILDKITE_BRANCH" = "refs/heads/main" ]; then
+    docker tag "$image:$VERSION" "$image:latest"
+    tags_to_push+=("$image:latest")
+    docker tag "$image:$VERSION" "$image:release-${BUILDKITE_COMMIT}"
+    tags_to_push+=("$image:release-${BUILDKITE_COMMIT}")
+fi
+
+for tag in "${tags_to_push[@]}"; do
+    echo "--- :docker: Pushing $tag"
+    docker push "$tag"
+done
---
+++ build/docker/monitoring/README.md
@@ -0,0 +1,24 @@
+# Overview
+This docker stack spins up ReadySet's monitoring services locally. It can be used to test Grafana and Prometheus integrations.
+
+This docker stack is provided in order to facilitate testing the experiments with Grafana integration.
+
+## Components
+The following components are present in this stack
+
+| Service | Port | User | Password | Tools configured |
+|------|---------|------|---------|---------|
+| Grafana | 4000 | -- | -- | <ul><li>'Experiments' dashboard</li></ul> |
+| Prometheus | 9090 | | | |
+
+## Configurations
+### Grafana
+If you want to extend the current Grafana configurations, check:
+- `grafana/dashboards`: This folder is where all the dashboard definitions are stored, as separate json files.
+- `grafana/provisioning/dashboards/default.yaml`: This file configures which dashboards should be pre-loaded by Grafana.
+- `grafana/provisioning/datasources/default.yaml`: This file configures which datasources should be pre-loaded by Grafana.
+
+### Prometheus
+Prometheus's config lives in `prometheus/prometheus.yml`.
+By default, prometheus only pulls metrics from "localhost:6033/metrics", the default prometheus endpoint
+for noria-server.
