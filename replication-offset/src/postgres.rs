46a1120eba62ab24c84c079dcdecf1e8447df0dd
Ethan Donowitz <ethan@readyset.io>
2023-08-16 14:51:15 -0400

    Revert "Revert "replicators: Handle out-of-order PGSQL LSNs""
    This reverts commit 5968de2d47239008efda93410c315c3faf1a44c0.
    
    Change-Id: Iae31dc5c71411de6a295559ef635e723f2d83d50
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/5824
    Tested-by: Buildkite CI
    Reviewed-by: Luke Osborne <luke@readyset.io>

--- replication-offset/src/postgres.rs
+++ replication-offset/src/postgres.rs
@@ -1,14 +1,45 @@
 use std::fmt::{self, Formatter};
 
+use readyset_errors::ReadySetError;
 use serde::{Deserialize, Serialize};
 
 use crate::ReplicationOffset;
 
-/// Represents a position within the Postgres write-ahead log
+/// Represents a position within the Postgres write-ahead log.
+///
+/// Postgres reorders WAL events so that we receive all of the events in a given transaction at
+/// once, which means that there is no guarantee that the LSNs of events we receive will
+/// monotonically increase. This means the event LSN on its own is not enough to ensure that we
+/// can define a [`ReplicationOffset`] that is well-ordered. However, there *is* a guarantee that
+/// the LSNs of **COMMIT** events we receive will monotonically increase. We leverage this by
+/// including the LSN of the last COMMIT we saw in our definition of [`PostgresPosition`], which
+/// gives us an ordering key of `(last_commit_lsn, lsn)`.
 #[derive(Debug, PartialEq, PartialOrd, Ord, Eq, Clone, Copy, Default, Serialize, Deserialize)]
 pub struct PostgresPosition {
-    /// Postgres Log Sequence Number
+    /// The LSN of the last COMMIT we saw
+    pub last_commit_lsn: CommitLsn,
+    /// The LSN of the position
     pub lsn: Lsn,
+}
+
+impl PostgresPosition {
+    /// Constructs a [`PostgresPosition`] from a [`CommitLsn`]. This function initializes the
+    /// [`Lsn`] of the [`PostgresPosition`] to zero.
+    pub fn from_commit_lsn(commit_lsn: impl Into<CommitLsn>) -> Self {
+        Self {
+            last_commit_lsn: commit_lsn.into(),
+            lsn: 0.into(),
+        }
+    }
+
+    /// Consumes `self`, constructing a new [`PostgresPosition`] with `self`'s [`CommitLsn`] and the
+    /// given [`Lsn`].
+    pub fn with_lsn(self, lsn: impl Into<Lsn>) -> Self {
+        Self {
+            last_commit_lsn: self.last_commit_lsn,
+            lsn: lsn.into(),
+        }
+    }
 }
 
 impl From<PostgresPosition> for ReplicationOffset {
@@ -17,21 +48,9 @@ impl From<PostgresPosition> for ReplicationOffset {
     }
 }
 
-impl From<i64> for PostgresPosition {
-    fn from(i: i64) -> Self {
-        PostgresPosition { lsn: i.into() }
-    }
-}
-
-impl From<Lsn> for PostgresPosition {
-    fn from(lsn: Lsn) -> Self {
-        PostgresPosition { lsn }
-    }
-}
-
 impl fmt::Display for PostgresPosition {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.lsn)
+        write!(f, "({}, {})", self.last_commit_lsn, self.lsn)
     }
 }
 
@@ -39,26 +58,71 @@ impl fmt::Display for PostgresPosition {
 #[derive(PartialEq, PartialOrd, Ord, Eq, Clone, Copy, Default, Serialize, Deserialize)]
 pub struct Lsn(pub i64);
 
-impl fmt::Display for Lsn {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{:X}/{:X}", self.0 >> 32, self.0 & 0xffffffff)
+impl TryFrom<ReplicationOffset> for Lsn {
+    type Error = ReadySetError;
+
+    fn try_from(offset: ReplicationOffset) -> Result<Self, Self::Error> {
+        Ok(PostgresPosition::try_from(offset)?.lsn)
     }
 }
 
-impl fmt::Debug for Lsn {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self)
+/// This type specifically represents the Postgres "log sequence number" of COMMITs. It is used to
+/// differentiate at compile time between LSNs that can be used to represent any WAL events and LSNs
+/// that can only represent COMMITs.
+#[derive(PartialEq, PartialOrd, Ord, Eq, Clone, Copy, Default, Serialize, Deserialize)]
+pub struct CommitLsn(pub i64);
+
+impl TryFrom<ReplicationOffset> for CommitLsn {
+    type Error = ReadySetError;
+
+    fn try_from(offset: ReplicationOffset) -> Result<Self, Self::Error> {
+        Ok(PostgresPosition::try_from(offset)?.last_commit_lsn)
     }
 }
 
-impl From<i64> for Lsn {
-    fn from(i: i64) -> Self {
-        Self(i)
-    }
+macro_rules! impl_lsn_traits {
+    ($t:ty) => {
+        impl fmt::Display for $t {
+            fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                write!(f, "{:X}/{:X}", self.0 >> 32, self.0 & 0xffffffff)
+            }
+        }
+
+        impl fmt::Debug for $t {
+            fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                write!(f, "{}", self)
+            }
+        }
+
+        impl From<i64> for $t {
+            fn from(i: i64) -> Self {
+                Self(i)
+            }
+        }
+    };
 }
 
-impl From<Lsn> for ReplicationOffset {
-    fn from(lsn: Lsn) -> Self {
-        Self::Postgres(PostgresPosition { lsn })
+impl_lsn_traits!(Lsn);
+impl_lsn_traits!(CommitLsn);
+
+#[cfg(test)]
+mod tests {
+    use super::PostgresPosition;
+
+    // The `PartialOrd` derivation on `PostgresPosition` relies upon the ordering of the members
+    // in the struct: `last_commit_lsn` *must* be listed before `lsn`. This test will fail if
+    // the members of the struct are ever reordered.
+    #[test]
+    fn test_postgres_position_partial_ord() {
+        let pos1 = PostgresPosition {
+            last_commit_lsn: 1.into(),
+            lsn: 0.into(),
+        };
+        let pos2 = PostgresPosition {
+            last_commit_lsn: 0.into(),
+            lsn: 1.into(),
+        };
+
+        assert!(pos1 > pos2);
     }
 }
