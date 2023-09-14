d978215fa50aa08d9e83570e868ee556882cfe86
Ethan Donowitz <ethan@readyset.io>
2023-09-14 11:15:43 -0400

    dataflow: Add min persisted offset RPC
    This commit adds a `/min_persisted_replication_offset` controller RPC
    that queries all the base table domains for their minimum persisted
    replication offsets, computes the minimum of those, and returns the
    result. Eventually, this offset will be used by the replicator to
    determine the greatest offset it can ACK upstream such that the upstream
    database doesn't purge any WAL files it would need to replay events after
    a crash.
    
    Refs: REA-3434
    Change-Id: Id9e5f5dc0a2becd7e5b4f36a9041252dc5be8399

--- readyset-client/src/controller.rs
+++ readyset-client/src/controller.rs
@@ -887,8 +887,12 @@ impl ReadySetHandle {
     );
 
     simple_request!(
-        /// Get a list of all current tables node indexes that are involved in snapshotting.
-        snapshotting_tables() -> Vec<String>
+        /// Gets the minimum replication offset up to which data has been persisted across all the base
+        /// table nodes. If no base tables have unpersisted data, this method returns `None`.
+        ///
+        /// See [the documentation for PersistentState](::readyset_dataflow::state::persistent_state)
+        /// for more information about replication offsets.
+        min_persisted_replication_offset() -> Option<ReplicationOffset>
     );
 
     /// Poll in a loop to wait for all tables to finish compacting
