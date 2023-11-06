820c46d3b13757eccb782ec072b57c282e77d504
Luke Osborne <luke.o@readyset.io>
2023-11-06 15:59:41 -0500

    nom-sql: Check 'create cache' parsing last
    Now that we clone the original string for `create cache` parsing, do it
    last so that we don't incur this expense for non-create cache queries.
    
    Change-Id: Ifc06a371ae7b0b72f0aa9c4823e9fc8498ec75b2

--- nom-sql/src/parser.rs
+++ nom-sql/src/parser.rs
@@ -204,7 +204,6 @@ fn sql_query_part1(
             map(updating(dialect), SqlQuery::Update),
             map(set(dialect), SqlQuery::Set),
             map(view_creation(dialect), SqlQuery::CreateView),
-            map(create_cached_query(dialect), SqlQuery::CreateCache),
             map(drop_cached_query(dialect), SqlQuery::DropCache),
             map(drop_all_caches, SqlQuery::DropAllCaches),
             map(alter_table_statement(dialect), SqlQuery::AlterTable),
@@ -215,6 +214,8 @@ fn sql_query_part1(
             map(use_statement(dialect), SqlQuery::Use),
             map(show(dialect), SqlQuery::Show),
             map(explain_statement(dialect), SqlQuery::Explain),
+            // This does a more expensive clone of `i`, so process it last.
+            map(create_cached_query(dialect), SqlQuery::CreateCache),
         ))(i)
     }
 }
