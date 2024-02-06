de884c0b19d9c2fcf561f803c86515b01177a7f1
Jason Brown <jason.b@readyset.io>
2024-02-05 18:09:20 -0800

    adapter-types: Add crate for shared adapter types
    `readyset-{mysql|psql}` depends on both `readyset-adapter` and
    `{mysql|psql}-srv`. `readyset-adapter` and `{mysql|psql}-srv` are not
    dependent the other. This patch creates a low-level crate to share
    data types between these all of crates to overlapping names/structs
    across them (along with confusing/annoying conversion functions).
    
    Fixes: REA-3983
    Change-Id: Ib40091436cb77c4870376b74a4582ae7ba4cb82b

---
+++ readyset-adapter-types/src/lib.rs
@@ -0,0 +1,58 @@
+/// This module contains types to be shared across adapter-related crates,
+/// such as `readyset-adapter`, `readyset-{mysql|psql}`,
+/// and `{mysql|psql}-srv`.
+use std::fmt::{Display, Formatter, Result};
+
+/// An identifier for a single prepared statement that is to be deallocated, or
+/// all prepared statements.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum DeallocateId {
+    /// A simple numeric id for a single statement.
+    Numeric(u32),
+    /// An alphanumeric name for a statement.
+    Named(String),
+    /// Apply to all statements.
+    All,
+}
+
+impl From<String> for DeallocateId {
+    fn from(s: String) -> Self {
+        if let Ok(i) = s.parse::<u32>() {
+            DeallocateId::Numeric(i)
+        } else {
+            DeallocateId::Named(s)
+        }
+    }
+}
+
+impl From<Option<String>> for DeallocateId {
+    fn from(t: Option<String>) -> Self {
+        match t {
+            Some(id) => {
+                if let Ok(i) = id.parse::<u32>() {
+                    DeallocateId::Numeric(i)
+                } else {
+                    DeallocateId::Named(id)
+                }
+            }
+            None => DeallocateId::All,
+        }
+    }
+}
+
+impl Display for DeallocateId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            DeallocateId::Numeric(id) => write!(f, "{}", id),
+            DeallocateId::Named(name) => write!(f, "{}", name),
+            DeallocateId::All => write!(f, "all"),
+        }
+    }
+}
+
+/// Indicates that after parsing a query, a lower/outer level behavior needs to execute the command.
+#[derive(Clone, Debug)]
+pub enum ParsedCommand {
+    /// A `DEALLOCATE (PREPARE)` SQL command was invoked.
+    Deallocate(DeallocateId),
+}
