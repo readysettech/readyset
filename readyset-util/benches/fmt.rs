4aec2039f38eb09aa8528a5aea81c5035f93f698
Ethan Donowitz <ethan@readyset.io>
2024-04-03 12:03:44 -0400

    util: Add fast timestamp encoding
    This commit adds handwritten algorithms to encode `NaiveDateTime` and
    `DateTime<FixedOffset>` as strings into a given `BytesMut` in the format
    the Postgres uses to display timestamps and timestamps with time zones.
    Eventually, these algorithms will replace our usage of chrono's
    `format()` method.
    
    This commit also adds a benchmark to demonstrate the performance
    improvement between our implementation and chrono's implementation.
    
    Change-Id: I19799931d1c52929a9805c3ba158fd834aba283e

--- readyset-util/benches/fmt.rs
+++ readyset-util/benches/fmt.rs
@@ -1,10 +1,11 @@
 use std::fmt::Write;
 
 use bytes::BytesMut;
+use chrono::{DateTime, FixedOffset, TimeZone};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use pprof::criterion::{Output, PProfProfiler};
 use rand::Rng;
-use readyset_util::fmt::write_padded_u32;
+use readyset_util::fmt::{write_padded_u32, FastEncode};
 
 const NUMBERS_TO_GENERATE: usize = 1_000_000;
 
@@ -44,6 +45,57 @@ fn bench_write_padded_u32(c: &mut Criterion) {
     });
 }
 
+fn bench_write_timestamptz(c: &mut Criterion) {
+    let mut group = c.benchmark_group("write a timestamptz");
+
+    group.bench_function("custom formatter", |b| {
+        let mut bytes = BytesMut::new();
+        let mut iter = 0usize;
+        let mut rng = rand::thread_rng();
+        let numbers = (0..NUMBERS_TO_GENERATE)
+            .map(|_| {
+                FixedOffset::west_opt(18_000)
+                    .unwrap()
+                    .with_ymd_and_hms(2020, rng.gen_range(1..12), rng.gen_range(1..28), 12, 30, 45)
+                    .single()
+                    .unwrap()
+            })
+            .collect::<Vec<DateTime<FixedOffset>>>();
+
+        b.iter(|| {
+            black_box(numbers[iter % NUMBERS_TO_GENERATE]).put(black_box(&mut bytes));
+            iter += 1;
+        })
+    });
+
+    group.bench_function("default formatter", |b| {
+        const TIMESTAMP_TZ_FORMAT: &str = "%Y-%m-%d %H:%M:%S%.f %:z";
+        let mut bytes = BytesMut::new();
+        let mut iter = 0usize;
+        let mut rng = rand::thread_rng();
+        let numbers = (0..NUMBERS_TO_GENERATE)
+            .map(|_| {
+                FixedOffset::west_opt(18_000)
+                    .unwrap()
+                    .with_ymd_and_hms(2020, rng.gen_range(1..12), rng.gen_range(1..28), 12, 30, 45)
+                    .single()
+                    .unwrap()
+            })
+            .collect::<Vec<DateTime<FixedOffset>>>();
+
+        b.iter(|| {
+            let tstz = numbers[iter % NUMBERS_TO_GENERATE];
+            write!(
+                black_box(&mut bytes),
+                "{}",
+                tstz.format(TIMESTAMP_TZ_FORMAT)
+            )
+            .unwrap();
+            iter += 1;
+        })
+    });
+}
+
 fn flamegraphs_profiler() -> Criterion {
     return Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
 }
@@ -51,6 +103,6 @@ fn flamegraphs_profiler() -> Criterion {
 criterion_group!(
     name = benches;
     config = flamegraphs_profiler();
-    targets = bench_write_padded_u32
+    targets = bench_write_padded_u32, bench_write_timestamptz
 );
 criterion_main!(benches);
