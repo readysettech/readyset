65669d6d72abe2f72191983c12d3ecf03d8ca8de
Sidney Cammeresi <sac@readyset.io>
2024-08-01 08:07:26 -0700

    readyset-psql: Remove box_patterns (2/7)
    Fortunately, this is a simple one because the box contains a simple enum.
    
    Change-Id: Ib9d6d86cd4c70efbe6045b955933a414a20721ce

--- readyset-psql/src/schema.rs
+++ readyset-psql/src/schema.rs
@@ -111,72 +111,76 @@ pub fn type_to_pgsql(col_type: &DfType) -> Result<pgsql::types::Type, Error> {
         DfType::Uuid => Ok(Type::UUID),
         DfType::Bit(_) => Ok(Type::BIT),
         DfType::VarBit(_) => Ok(Type::VARBIT),
-        DfType::Array(box DfType::Unknown) => {
-            // The default type for "unknown" in pgsql is TEXT
-            Ok(Type::TEXT)
+        DfType::Array(elem) => {
+            match elem.as_ref() {
+                DfType::Unknown => {
+                    // The default type for "unknown" in pgsql is TEXT
+                    Ok(Type::TEXT)
+                }
+                DfType::Bool => Ok(Type::BOOL_ARRAY),
+                DfType::Char(..) => Ok(Type::BPCHAR_ARRAY),
+                DfType::VarChar(_, Collation::Utf8) => Ok(Type::VARCHAR_ARRAY),
+                DfType::VarChar(_, Collation::Citext) => {
+                    // TODO: use the right CITEXT type
+                    Ok(Type::VARCHAR_ARRAY)
+                }
+                DfType::Int => Ok(Type::INT4_ARRAY),
+                DfType::BigInt => Ok(Type::INT8_ARRAY),
+                DfType::SmallInt => Ok(Type::INT2_ARRAY),
+                DfType::Float => Ok(Type::FLOAT4_ARRAY),
+                DfType::Double => Ok(Type::FLOAT8_ARRAY),
+                DfType::Text(Collation::Utf8) => Ok(Type::TEXT_ARRAY),
+                DfType::Text(Collation::Citext) => {
+                    // TODO: use the right CITEXT_ARRAY type
+                    Ok(Type::TEXT_ARRAY)
+                }
+                DfType::Timestamp { .. } => Ok(Type::TIMESTAMP_ARRAY),
+                DfType::TimestampTz { .. } => Ok(Type::TIMESTAMPTZ_ARRAY),
+                DfType::Json => Ok(Type::JSON_ARRAY),
+                DfType::Jsonb => Ok(Type::JSONB_ARRAY),
+                DfType::Date => Ok(Type::DATE_ARRAY),
+                DfType::Time { .. } => Ok(Type::TIME_ARRAY),
+                DfType::UnsignedInt => unsupported_type!(),
+                DfType::UnsignedBigInt => unsupported_type!(),
+                DfType::TinyInt => Ok(Type::CHAR_ARRAY),
+                DfType::UnsignedTinyInt => unsupported_type!(),
+                DfType::UnsignedSmallInt => unsupported_type!(),
+                DfType::MediumInt => unsupported_type!(),
+                DfType::UnsignedMediumInt => unsupported_type!(),
+                DfType::Blob => unsupported_type!(),
+                DfType::DateTime { .. } => unsupported_type!(),
+                DfType::Binary(_) => unsupported_type!(),
+                DfType::VarBinary(_) => unsupported_type!(),
+                DfType::Enum {
+                    metadata:
+                        Some(PgEnumMetadata {
+                            name,
+                            schema,
+                            oid,
+                            array_oid,
+                        }),
+                    variants,
+                    ..
+                } => Ok(Type::new(
+                    format!("_{name}"),
+                    *array_oid,
+                    Kind::Array(Type::new(
+                        name.into(),
+                        *oid,
+                        Kind::Enum(variants.to_vec()),
+                        schema.into(),
+                    )),
+                    schema.into(),
+                )),
+                DfType::Enum { metadata: None, .. } => unsupported_type!(),
+                DfType::Numeric { .. } => Ok(Type::NUMERIC_ARRAY),
+                DfType::MacAddr => Ok(Type::MACADDR_ARRAY),
+                DfType::Inet => Ok(Type::INET_ARRAY),
+                DfType::Uuid => Ok(Type::UUID_ARRAY),
+                DfType::Bit(_) => Ok(Type::BIT_ARRAY),
+                DfType::VarBit(_) => Ok(Type::VARBIT_ARRAY),
+                DfType::Array(_) => unsupported_type!(),
+            }
         }
-        DfType::Array(box DfType::Bool) => Ok(Type::BOOL_ARRAY),
-        DfType::Array(box DfType::Char(..)) => Ok(Type::BPCHAR_ARRAY),
-        DfType::Array(box DfType::VarChar(_, Collation::Utf8)) => Ok(Type::VARCHAR_ARRAY),
-        DfType::Array(box DfType::VarChar(_, Collation::Citext)) => {
-            // TODO: use the right CITEXT type
-            Ok(Type::VARCHAR_ARRAY)
-        }
-        DfType::Array(box DfType::Int) => Ok(Type::INT4_ARRAY),
-        DfType::Array(box DfType::BigInt) => Ok(Type::INT8_ARRAY),
-        DfType::Array(box DfType::SmallInt) => Ok(Type::INT2_ARRAY),
-        DfType::Array(box DfType::Float) => Ok(Type::FLOAT4_ARRAY),
-        DfType::Array(box DfType::Double) => Ok(Type::FLOAT8_ARRAY),
-        DfType::Array(box DfType::Text(Collation::Utf8)) => Ok(Type::TEXT_ARRAY),
-        DfType::Array(box DfType::Text(Collation::Citext)) => {
-            // TODO: use the right CITEXT_ARRAY type
-            Ok(Type::TEXT_ARRAY)
-        }
-        DfType::Array(box DfType::Timestamp { .. }) => Ok(Type::TIMESTAMP_ARRAY),
-        DfType::Array(box DfType::TimestampTz { .. }) => Ok(Type::TIMESTAMPTZ_ARRAY),
-        DfType::Array(box DfType::Json) => Ok(Type::JSON_ARRAY),
-        DfType::Array(box DfType::Jsonb) => Ok(Type::JSONB_ARRAY),
-        DfType::Array(box DfType::Date) => Ok(Type::DATE_ARRAY),
-        DfType::Array(box DfType::Time { .. }) => Ok(Type::TIME_ARRAY),
-        DfType::Array(box DfType::UnsignedInt) => unsupported_type!(),
-        DfType::Array(box DfType::UnsignedBigInt) => unsupported_type!(),
-        DfType::Array(box DfType::TinyInt) => Ok(Type::CHAR_ARRAY),
-        DfType::Array(box DfType::UnsignedTinyInt) => unsupported_type!(),
-        DfType::Array(box DfType::UnsignedSmallInt) => unsupported_type!(),
-        DfType::Array(box DfType::MediumInt) => unsupported_type!(),
-        DfType::Array(box DfType::UnsignedMediumInt) => unsupported_type!(),
-        DfType::Array(box DfType::Blob) => unsupported_type!(),
-        DfType::Array(box DfType::DateTime { .. }) => unsupported_type!(),
-        DfType::Array(box DfType::Binary(_)) => unsupported_type!(),
-        DfType::Array(box DfType::VarBinary(_)) => unsupported_type!(),
-        DfType::Array(box DfType::Enum {
-            metadata:
-                Some(PgEnumMetadata {
-                    name,
-                    schema,
-                    oid,
-                    array_oid,
-                }),
-            variants,
-            ..
-        }) => Ok(Type::new(
-            format!("_{name}"),
-            *array_oid,
-            Kind::Array(Type::new(
-                name.into(),
-                *oid,
-                Kind::Enum(variants.to_vec()),
-                schema.into(),
-            )),
-            schema.into(),
-        )),
-        DfType::Array(box DfType::Enum { metadata: None, .. }) => unsupported_type!(),
-        DfType::Array(box DfType::Numeric { .. }) => Ok(Type::NUMERIC_ARRAY),
-        DfType::Array(box DfType::MacAddr) => Ok(Type::MACADDR_ARRAY),
-        DfType::Array(box DfType::Inet) => Ok(Type::INET_ARRAY),
-        DfType::Array(box DfType::Uuid) => Ok(Type::UUID_ARRAY),
-        DfType::Array(box DfType::Bit(_)) => Ok(Type::BIT_ARRAY),
-        DfType::Array(box DfType::VarBit(_)) => Ok(Type::VARBIT_ARRAY),
-        DfType::Array(box DfType::Array(_)) => unsupported_type!(),
     }
 }
