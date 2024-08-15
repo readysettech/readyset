8e31cac1066f300b263931c3b3bdfb451cafa17e
Sandaruwan Gunathilake <sandaruwan@readyset.io>
2024-08-15 15:47:58 +0530

    grafana: New dashboards and config options
    This commit upgrades Grafana to the latest version and
    fixes the broken dashboards. The new metric names with
    appended time units are used in the dashboards.
    
    Grafana was configured to extract some config variables from
    UPSTREAM_DB_URL and others from RS_* type variables. This
    commit removes UPSTREAM_DB_URL and moves all variables RS_*
    type variables.
    
    RS_GRAFANA_PORT is renamed to GRAFANA_HTTP_PORT.
    
    The variable changes are backwards compatible.
    
    Change-Id: I732b3a405d5f07863a58e48c3bcf66cbf072ea4e
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/7831
    Tested-by: Buildkite CI
    Reviewed-by: Marcelo Altmann <marcelo@readyset.io>
    Reviewed-by: Jason Brown <jason.b@readyset.io>

--- build/docker/grafana/config-then-run.sh
+++ build/docker/grafana/config-then-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -exo pipefail
 
 # Parse the upstream database URL
 RE='(.+):\/\/([^:@]*)(:([^@]*))?@([^:\/]+)(:[0-9]+)?(\/(.+))?'
@@ -16,11 +16,21 @@ RE='(.+):\/\/([^:@]*)(:([^@]*))?@([^:\/]+)(:[0-9]+)?(\/(.+))?'
 #   |  |└---> "://" Separator [required]
 #   └--> Protocol [required] (e.g., postgresql, mysql)
 
-[[ $UPSTREAM_DB_URL =~ $RE ]]
-RS_DB_TYPE=${BASH_REMATCH[1]}
-RS_USER=${BASH_REMATCH[2]}
-RS_PASS=${BASH_REMATCH[4]}
-RS_DB_NAME=${BASH_REMATCH[8]}
+# Only parse if UPSTREAM_DB_URL is not empty.
+# Otherwise, these parameters must be passed down from the environment.
+# TODO: Remove UPSTREAM_DB_URL completely when new docker images are in ECR
+if [[! -z "${UPSTREAM_DB_URL}" ]; then
+    [[ $UPSTREAM_DB_URL =~ $RE ]]
+    RS_DB_TYPE=${BASH_REMATCH[1]}
+    RS_USER=${BASH_REMATCH[2]}
+    RS_PASS=${BASH_REMATCH[4]}
+    RS_DB_NAME=${BASH_REMATCH[8]}
+fi
+
+# Rename RS_GRAFANA_PORT to GRAFANA_HTTP_PORT
+if [ -z "${GRAFANA_HTTP_PORT}" ]; then
+    GRAFANA_HTTP_PORT="$RS_GRAFANA_PORT"
+fi
 
 if [[ $RS_DB_TYPE = "mysql" ]]; then
     CONFIG_FILE=/etc/grafana/provisioning/datasources/default.template.mysql.yml
