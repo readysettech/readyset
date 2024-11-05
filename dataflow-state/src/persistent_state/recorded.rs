6bd05f4ea6be5db5a57bde885b0a71d2d43053b7
Sandaruwan Gunathilake <sandaruwan@readyset.io>
2024-11-05 22:10:12 +0530

    metrics: Rename rocksdb metrics
    Append the unit names(_bytes) to the rocksdb metrics. This would
    make the metric names consistent with the other metric names.
    
    Change-Id: Id23428831bef3eb7b2d8ab1e75f273f7a2535fdf
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/8221
    Tested-by: Buildkite CI
    Reviewed-by: Jason Brown <jason.b@readyset.io>

--- dataflow-state/src/persistent_state/recorded.rs
+++ dataflow-state/src/persistent_state/recorded.rs
@@ -1,14 +1,14 @@
 /// Gauge: Approximate memory usage of all the mem-tables.
-pub const MEM_TABLE_TOTAL: &str = "readyset_rocksdb_mem_table_total";
+pub const MEM_TABLE_TOTAL: &str = "readyset_rocksdb_mem_table_total_bytes";
 
 /// Gauge: Approximate memory usage of un-flushed mem-tables.
-pub const MEM_TABLE_UNFLUSHED: &str = "readyset_rocksdb_mem_table_unflushed";
+pub const MEM_TABLE_UNFLUSHED: &str = "readyset_rocksdb_mem_table_unflushed_bytes";
 
 /// Gauge: Approximate memory usage of all the table readers.
-pub const READERS_TOTAL: &str = "readyset_rocksdb_readers_total";
+pub const READERS_TOTAL: &str = "readyset_rocksdb_readers_total_bytes";
 
 /// Gauge: Approximate memory usage by cache.
-pub const CACHE_TOTAL: &str = "readyset_rocksdb_cache_total";
+pub const CACHE_TOTAL: &str = "readyset_rocksdb_cache_total_bytes";
 
 /// Gauge: Total memory used by block index and filter, as loaded from each sstable.
-pub const BLOCK_INDEXES_FILTERS_TOTAL: &str = "readyset_rocksdb_block_index_filters_total";
+pub const BLOCK_INDEXES_FILTERS_TOTAL: &str = "readyset_rocksdb_block_index_filters_total_bytes";
