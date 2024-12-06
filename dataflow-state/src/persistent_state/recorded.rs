1de382a01f753772ee2fde056ac634fbd15b5512
Jason Brown <jason.b@readyset.io>
2024-12-06 07:13:08 -0800

    persistent-state: Extend rocksdb metrics
    Collect more rocksdb metrics for compaction, sstables, and memtable
    flushes. Also, the previous mechanism to collect block-cache metrics,
    from the `get_memory_usage_stats()` function, was incorrect as we
    needed a reference to the `Cache` objects (and pass them to the
    function) in order to get the value. Instead, we'll just query the
    standard "get property" function for them, like we do for most other
    metrics.
    
    Release-Note-Core: Add and correct many of the reported rocksdb
      metrics.
    Change-Id: I6dc0cf3872343fbeb1e27b23fd9c563a5d649abe
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/8414
    Reviewed-by: Johnathan Davis <jcd@readyset.io>
    Tested-by: Buildkite CI

--- dataflow-state/src/persistent_state/recorded.rs
+++ dataflow-state/src/persistent_state/recorded.rs
@@ -7,8 +7,33 @@ pub const MEM_TABLE_UNFLUSHED: &str = "readyset_rocksdb_mem_table_unflushed_byte
 /// Gauge: Approximate memory usage of all the table readers.
 pub const READERS_TOTAL: &str = "readyset_rocksdb_readers_total_bytes";
 
-/// Gauge: Approximate memory usage by cache.
-pub const CACHE_TOTAL: &str = "readyset_rocksdb_cache_total_bytes";
-
 /// Gauge: Total memory used by block index and filter, as loaded from each sstable.
 pub const BLOCK_INDEXES_FILTERS_TOTAL: &str = "readyset_rocksdb_block_index_filters_total_bytes";
+
+/// Gauge: Number of compactions pending.
+pub const COMPACTION_PENDING: &str = "readyset_rocksdb_compaction_pending";
+
+/// Gauge: Estimate of pending compaction bytes.
+pub const ESTIMATE_PENDING_COMPACTION_BYTES: &str =
+    "readyset_rocksdb_estimate_pending_compaction_bytes";
+
+/// Gauge: Number of running compactions.
+pub const NUM_RUNNING_COMPACTIONS: &str = "readyset_rocksdb_num_running_compactions";
+
+/// Gauge: Number of running flushes.
+pub const NUM_RUNNING_FLUSHES: &str = "readyset_rocksdb_num_running_flushes";
+
+/// Gauge: Total size of all SST files.
+pub const TOTAL_SST_FILES_SIZE: &str = "readyset_rocksdb_total_sst_files_size";
+
+/// Gauge: Live size of all SST files.
+pub const LIVE_SST_FILES_SIZE: &str = "readyset_rocksdb_live_sst_files_size";
+
+/// Gauge: Capacity of the block cache.
+pub const BLOCK_CACHE_CAPACITY: &str = "readyset_rocksdb_block_cache_capacity";
+
+/// Gauge: Usage of the block cache.
+pub const BLOCK_CACHE_USAGE: &str = "readyset_rocksdb_block_cache_usage";
+
+/// Gauge: Pinned usage of the block cache.
+pub const BLOCK_CACHE_PINNED_USAGE: &str = "readyset_rocksdb_block_cache_pinned_usage";
