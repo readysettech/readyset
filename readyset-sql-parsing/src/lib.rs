9fbb4f9c82e073377d0e250fa4cb235bbf3c8662
Michael Victor Zink <michael.z@readyset.io>
2025-01-30 15:29:52 -0800

    readyset-sql-parsing: Parse DROP ALL CACHES
    This does not yet parse any of our other custom DROP statements.
    
    Change-Id: I12d9615cfcbfe5f587cc157cf014d651baacc748

--- readyset-sql-parsing/src/lib.rs
+++ readyset-sql-parsing/src/lib.rs
@@ -31,6 +31,7 @@ fn sqlparser_dialect_from_readyset_dialect(
 #[cfg(feature = "sqlparser")]
 #[expect(clippy::upper_case_acronyms, reason = "SQL keywords are capitalized")]
 enum ReadysetKeyword {
+    CACHES,
     READYSET,
     SIMPLIFIED,
 }
@@ -39,6 +40,7 @@ enum ReadysetKeyword {
 impl ReadysetKeyword {
     fn as_str(&self) -> &str {
         match self {
+            Self::CACHES => "CACHES",
             Self::READYSET => "READYSET",
             Self::SIMPLIFIED => "SIMPLIFIED",
         }
@@ -166,6 +168,21 @@ fn parse_show(parser: &mut Parser) -> Result<SqlQuery, ReadysetParsingError> {
     }
 }
 
+/// Expects `DROP` was already parsed. Attempts to parse a Readyset-specific drop statement,
+/// otherwise falls back to [`Parser::parse_drop`].
+#[cfg(feature = "sqlparser")]
+fn parse_drop(parser: &mut Parser) -> Result<SqlQuery, ReadysetParsingError> {
+    if parser.parse_keyword(Keyword::ALL) && parse_readyset_keyword(parser, ReadysetKeyword::CACHES)
+    {
+        Ok(SqlQuery::DropAllCaches(
+            readyset_sql::ast::DropAllCachesStatement {},
+        ))
+    } else {
+        Ok(parser.parse_drop()?.try_into()?)
+    }
+}
+
+/// Attempts to parse a Readyset-specific statement, and falls back to [`Parser::parse_statement`] if it fails.
 #[cfg(feature = "sqlparser")]
 fn parse_readyset_query(
     parser: &mut Parser,
@@ -177,6 +194,8 @@ fn parse_readyset_query(
         parse_explain(parser)
     } else if parser.parse_keyword(Keyword::SHOW) {
         parse_show(parser)
+    } else if parser.parse_keyword(Keyword::DROP) {
+        parse_drop(parser)
     } else {
         Ok(parser.parse_statement()?.try_into()?)
     }
