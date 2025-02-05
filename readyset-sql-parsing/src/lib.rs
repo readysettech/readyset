7c1774a70e7250c544cf86aed4861f407af42498
Michael Victor Zink <michael.z@readyset.io>
2025-02-04 16:45:22 -0800

    readyset-sql-parsing: Parse SHOW READYSET MIGRATION STATUS
    Yay, another custom Readyset keyword.
    
    Change-Id: Ieaca6e700023b839d0428b0f77745dcd5bd01dd5

--- readyset-sql-parsing/src/lib.rs
+++ readyset-sql-parsing/src/lib.rs
@@ -16,6 +16,8 @@ pub enum ReadysetParsingError {
     SqlparserError(#[from] sqlparser::parser::ParserError),
     #[error("AST conversion error: {0}")]
     AstConversionError(#[from] readyset_sql::AstConversionError),
+    #[error("readyset parsing error: {0}")]
+    ReadysetParsingError(String),
 }
 
 #[cfg(feature = "sqlparser")]
@@ -32,6 +34,7 @@ fn sqlparser_dialect_from_readyset_dialect(
 #[expect(clippy::upper_case_acronyms, reason = "SQL keywords are capitalized")]
 enum ReadysetKeyword {
     CACHES,
+    MIGRATION,
     READYSET,
     SIMPLIFIED,
 }
@@ -41,6 +44,7 @@ impl ReadysetKeyword {
     fn as_str(&self) -> &str {
         match self {
             Self::CACHES => "CACHES",
+            Self::MIGRATION => "MIGRATION",
             Self::READYSET => "READYSET",
             Self::SIMPLIFIED => "SIMPLIFIED",
         }
@@ -157,12 +161,23 @@ fn parse_explain(parser: &mut Parser) -> Result<SqlQuery, ReadysetParsingError> 
 /// SHOW READYSET VERSION
 #[cfg(feature = "sqlparser")]
 fn parse_show(parser: &mut Parser) -> Result<SqlQuery, ReadysetParsingError> {
-    if parse_readyset_keyword(parser, ReadysetKeyword::READYSET)
-        && parser.parse_keyword(Keyword::VERSION)
-    {
-        Ok(SqlQuery::Show(
-            readyset_sql::ast::ShowStatement::ReadySetVersion,
-        ))
+    if parse_readyset_keyword(parser, ReadysetKeyword::READYSET) {
+        if parser.parse_keyword(Keyword::VERSION) {
+            Ok(SqlQuery::Show(
+                readyset_sql::ast::ShowStatement::ReadySetVersion,
+            ))
+        } else if parse_readyset_keyword(parser, ReadysetKeyword::MIGRATION)
+            && parser.parse_keyword(Keyword::STATUS)
+        {
+            let id = parser.parse_literal_uint()?;
+            Ok(SqlQuery::Show(
+                readyset_sql::ast::ShowStatement::ReadySetMigrationStatus(id),
+            ))
+        } else {
+            Err(ReadysetParsingError::ReadysetParsingError(
+                "expected SHOW READYSET VERSION or SHOW READYSET MIGRATION STATUS".into(),
+            ))
+        }
     } else {
         Ok(parser.parse_show()?.try_into()?)
     }
