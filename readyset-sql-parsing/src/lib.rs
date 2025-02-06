5eab23dda88af8702e703584ae55c8f4142353d2
Michael Victor Zink <michael.z@readyset.io>
2025-02-06 10:33:42 -0800

    readyset-sql-parsing: Parse SHOW READYSET STATUS
    Just adding these piecemeal as I come across them.
    
    Change-Id: Ie21daf917fc9494f751685ff8e1d6676719c8576

--- readyset-sql-parsing/src/lib.rs
+++ readyset-sql-parsing/src/lib.rs
@@ -197,6 +197,10 @@ fn parse_show(parser: &mut Parser) -> Result<SqlQuery, ReadysetParsingError> {
             Ok(SqlQuery::Show(
                 readyset_sql::ast::ShowStatement::ReadySetMigrationStatus(id),
             ))
+        } else if parser.parse_keyword(Keyword::STATUS) {
+            Ok(SqlQuery::Show(
+                readyset_sql::ast::ShowStatement::ReadySetStatus,
+            ))
         } else {
             Err(ReadysetParsingError::ReadysetParsingError(
                 "expected SHOW READYSET VERSION or SHOW READYSET MIGRATION STATUS".into(),
