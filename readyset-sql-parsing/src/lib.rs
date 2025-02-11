fa3d3997cf1fe56c5038b1b726080f1d28baf983
Michael Victor Zink <michael.z@readyset.io>
2025-02-11 13:39:29 -0800

    readyset-sql-parsing: Add feature for erroring out on sqlparser-rs failure
    This is just to make development and testing easier, while with default
    features we will continue to only error if nom-sql does.
    
    Use like:
    
    ```sh
    cargo test --features ast-conversion-errors
    ```
    
    Change-Id: Ie933a0a45504c9830da3f4943473e19f64b4cb2c

--- readyset-sql-parsing/src/lib.rs
+++ readyset-sql-parsing/src/lib.rs
@@ -1,7 +1,6 @@
-use pretty_assertions::Comparison;
 use readyset_sql::{ast::SqlQuery, Dialect};
-use tracing::warn;
 
+#[cfg(feature = "sqlparser")]
 fn sqlparser_dialect_from_readyset_dialect(
     dialect: Dialect,
 ) -> Box<dyn sqlparser::dialect::Dialect> {
@@ -11,34 +10,47 @@ fn sqlparser_dialect_from_readyset_dialect(
     }
 }
 
-/// Parse SQL using the specified parser implementation and dialect
+#[cfg(feature = "sqlparser")]
 pub fn parse_query(dialect: Dialect, input: impl AsRef<str>) -> Result<SqlQuery, String> {
     let nom_result = nom_sql::parse_query(dialect, input.as_ref());
     let sqlparser_dialect = sqlparser_dialect_from_readyset_dialect(dialect);
-    let sqlparser_result = sqlparser::parser::Parser::new(sqlparser_dialect.as_ref())
-        .try_with_sql(input.as_ref())
-        .and_then(|mut p| p.parse_statement())
-        .map_err(|e| format!("failed to parse: {e}"))
-        .and_then(|q| {
-            q.try_into()
-                .map_err(|e| format!("failed to convert AST: {e}"))
-        });
-    match (&nom_result, &sqlparser_result) {
+    let sqlparser_result: Result<SqlQuery, _> =
+        sqlparser::parser::Parser::new(sqlparser_dialect.as_ref())
+            .try_with_sql(input.as_ref())
+            .and_then(|mut p| p.parse_statement())
+            .map_err(|e| format!("failed to parse: {e}"))
+            .and_then(|q| {
+                q.try_into()
+                    .map_err(|e| format!("failed to convert AST: {e}"))
+            });
+    match (&nom_result, sqlparser_result) {
         (Ok(nom_ast), Ok(sqlparser_ast)) => {
-            if nom_ast != sqlparser_ast {
-                let comparison = Comparison::new(nom_ast, sqlparser_ast);
-                warn!("nom-sql AST differs from sqlparser-rs AST:\n{comparison}");
-            }
+            pretty_assertions::assert_eq!(
+                nom_ast,
+                &sqlparser_ast,
+                "nom-sql AST differs from sqlparser-rs AST. input: {:?}",
+                input.as_ref()
+            );
         }
         (Ok(nom_ast), Err(sqlparser_error)) => {
-            warn!(%sqlparser_error, ?nom_ast, "nom-sql succeeded but sqlparser-rs failed");
+            panic!(
+                "nom-sql succeeded but sqlparser-rs failed: {}\ninput: {}\nnom_ast: {:?}",
+                sqlparser_error,
+                input.as_ref(),
+                nom_ast
+            )
         }
         (Err(nom_error), Ok(sqlparser_ast)) => {
-            warn!(%nom_error, ?sqlparser_ast, "sqlparser-rs succeeded but nom-sql failed")
+            tracing::warn!(%nom_error, ?sqlparser_ast, "sqlparser-rs succeeded but nom-sql failed")
         }
         (Err(nom_error), Err(sqlparser_error)) => {
-            warn!(%nom_error, %sqlparser_error, "both nom-sql and sqlparser-rs failed");
+            tracing::warn!(%nom_error, %sqlparser_error, "both nom-sql and sqlparser-rs failed");
         }
     }
     nom_result
+}
+
+#[cfg(not(feature = "sqlparser"))]
+pub fn parse_query(dialect: Dialect, input: impl AsRef<str>) -> Result<SqlQuery, String> {
+    nom_sql::parse_query(dialect, input.as_ref())
 }
