b1c23be848884f9d7c78fec7d136c0bbaf522546
Michael Victor Zink <michael.z@readyset.io>
2025-03-26 08:25:29 -0700

    readyset-sql: Upgrade sqlparser to support correct variable scopes
    Thanks to upstream work by @MohamedAbdeen21, we can now enable some
    preexisting tests for variable scopes.
    
    Change-Id: Ibf2a397817d1a7bee0ead246f875293771546e72

--- readyset-sql/src/ast/set.rs
+++ readyset-sql/src/ast/set.rs
@@ -25,18 +25,14 @@ impl TryFromDialect<sqlparser::ast::Set> for SetStatement {
         use sqlparser::ast::Set;
         match value {
             Set::SingleAssignment {
-                local,
+                scope,
                 hivevar: _,
                 variable,
                 values,
             } => match dialect {
                 Dialect::PostgreSQL => {
-                    let scope = if local {
-                        Some(PostgresParameterScope::Local)
-                    } else {
-                        Some(PostgresParameterScope::Session)
-                    };
-                    let name = variable
+                    let scope = scope.map(TryInto::try_into).transpose()?;
+                    let variable = variable
                         .0
                         .into_iter()
                         .exactly_one()
@@ -47,12 +43,15 @@ impl TryFromDialect<sqlparser::ast::Set> for SetStatement {
                     let value = values.try_into_dialect(dialect)?;
                     Ok(Self::PostgresParameter(SetPostgresParameter {
                         scope,
-                        name,
+                        name: variable,
                         value,
                     }))
                 }
                 Dialect::MySQL => {
-                    let name = variable.try_into()?;
+                    let mut variable: Variable = variable.try_into_dialect(dialect)?;
+                    if let Some(scope) = scope {
+                        variable.scope = scope.into();
+                    }
                     let value = values
                         .into_iter()
                         .exactly_one()
@@ -61,17 +60,20 @@ impl TryFromDialect<sqlparser::ast::Set> for SetStatement {
                         })?
                         .try_into_dialect(dialect)?;
                     Ok(Self::Variable(SetVariables {
-                        variables: vec![(name, value)],
+                        variables: vec![(variable, value)],
                     }))
                 }
             },
             Set::MultipleAssignments { assignments } => {
                 let variables = assignments
                     .into_iter()
-                    .map(|set_assignment| {
-                        let name = set_assignment.name.try_into()?;
-                        let value = set_assignment.value.try_into_dialect(dialect)?;
-                        Ok((name, value))
+                    .map(|sqlparser::ast::SetAssignment { scope, name, value }| {
+                        let mut variable: Variable = name.try_into_dialect(dialect)?;
+                        if let Some(scope) = scope {
+                            variable.scope = scope.into();
+                        }
+                        let value = value.try_into_dialect(dialect)?;
+                        Ok((variable, value))
                     })
                     .try_collect()?;
                 Ok(Self::Variable(SetVariables { variables }))
@@ -118,6 +120,18 @@ impl SetStatement {
 pub enum PostgresParameterScope {
     Session,
     Local,
+}
+
+impl TryFrom<sqlparser::ast::ContextModifier> for PostgresParameterScope {
+    type Error = AstConversionError;
+
+    fn try_from(scope: sqlparser::ast::ContextModifier) -> Result<Self, Self::Error> {
+        match scope {
+            sqlparser::ast::ContextModifier::Session => Ok(PostgresParameterScope::Session),
+            sqlparser::ast::ContextModifier::Local => Ok(PostgresParameterScope::Local),
+            _ => unsupported!("Postgres only has LOCAL and SESSION scope, found {scope}"),
+        }
+    }
 }
 
 impl fmt::Display for PostgresParameterScope {
@@ -292,6 +306,16 @@ impl From<sqlparser::ast::ObjectNamePart> for VariableScope {
     }
 }
 
+impl From<sqlparser::ast::ContextModifier> for VariableScope {
+    fn from(value: sqlparser::ast::ContextModifier) -> Self {
+        match value {
+            sqlparser::ast::ContextModifier::Session => Self::Session,
+            sqlparser::ast::ContextModifier::Global => Self::Global,
+            sqlparser::ast::ContextModifier::Local => Self::Local,
+        }
+    }
+}
+
 impl fmt::Display for VariableScope {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -309,54 +333,58 @@ pub struct Variable {
     pub name: SqlIdentifier,
 }
 
-impl From<String> for Variable {
-    fn from(value: String) -> Self {
-        let lowered = value[..(10.min(value.len()))].to_lowercase();
-        if lowered.starts_with("@@local.") {
-            Self {
-                scope: VariableScope::Local,
-                name: value[8..].into(),
+fn split_variable(value: String) -> (Option<VariableScope>, SqlIdentifier) {
+    let lowered = value[..(10.min(value.len()))].to_lowercase();
+    if lowered.starts_with("@@local.") {
+        (Some(VariableScope::Local), value[8..].into())
+    } else if lowered.starts_with("@@global.") {
+        (Some(VariableScope::Global), value[9..].into())
+    } else if lowered.starts_with("@@session.") {
+        (Some(VariableScope::Session), value[10..].into())
+    } else if lowered.starts_with("@@") {
+        (Some(VariableScope::Session), value[2..].into())
+    } else if lowered.starts_with("@") {
+        (Some(VariableScope::User), value[1..].into())
+    } else {
+        (None, value.into())
+    }
+}
+
+impl TryFromDialect<String> for Variable {
+    fn try_from_dialect(value: String, dialect: Dialect) -> Result<Self, AstConversionError> {
+        match dialect {
+            Dialect::PostgreSQL => {
+                failed!("Attempting to convert Postgres parameter to MySQL variable")
             }
-        } else if lowered.starts_with("@@global.") {
-            Self {
-                scope: VariableScope::Global,
-                name: value[9..].into(),
-            }
-        } else if lowered.starts_with("@@session.") {
-            Self {
-                scope: VariableScope::Session,
-                name: value[10..].into(),
-            }
-        } else if lowered.starts_with("@@") {
-            Self {
-                scope: VariableScope::Session,
-                name: value[2..].into(),
-            }
-        } else if lowered.starts_with("@") {
-            Self {
-                scope: VariableScope::User,
-                name: value[1..].into(),
-            }
-        } else {
-            Self {
-                scope: VariableScope::Local,
-                name: value.into(),
+            Dialect::MySQL => {
+                let (scope, name) = split_variable(value);
+                Ok(Variable {
+                    scope: scope.unwrap_or(VariableScope::Local),
+                    name,
+                })
             }
         }
     }
 }
 
-impl From<sqlparser::ast::Ident> for Variable {
-    fn from(value: sqlparser::ast::Ident) -> Self {
+impl TryFromDialect<sqlparser::ast::Ident> for Variable {
+    fn try_from_dialect(
+        value: sqlparser::ast::Ident,
+        dialect: Dialect,
+    ) -> Result<Self, AstConversionError> {
         // XXX(mvzink): We lowercase across the board (even ignoring dialect) just to match nom-sql
-        value.value.to_lowercase().into()
+        value.value.to_lowercase().try_into_dialect(dialect)
     }
 }
 
-impl TryFrom<Vec<sqlparser::ast::Ident>> for Variable {
-    type Error = AstConversionError;
-
-    fn try_from(mut value: Vec<sqlparser::ast::Ident>) -> Result<Self, Self::Error> {
+impl TryFromDialect<Vec<sqlparser::ast::Ident>> for Variable {
+    fn try_from_dialect(
+        mut value: Vec<sqlparser::ast::Ident>,
+        dialect: Dialect,
+    ) -> Result<Self, AstConversionError> {
+        if dialect == Dialect::PostgreSQL {
+            return failed!("Attempt to convert Postgres parameter to MySQL variable");
+        }
         // XXX(mvzink): We lowercase across the board (even ignoring dialect) just to match nom-sql
         let name = value
             .pop()
@@ -364,7 +392,7 @@ impl TryFrom<Vec<sqlparser::ast::Ident>> for Variable {
             .value
             .to_lowercase();
         if value.is_empty() {
-            Ok(name.into())
+            Ok(name.try_into_dialect(dialect)?)
         } else if value.len() == 1 {
             let scope = value.pop().unwrap().value.as_str().into();
             Ok(Self {
@@ -377,10 +405,11 @@ impl TryFrom<Vec<sqlparser::ast::Ident>> for Variable {
     }
 }
 
-impl TryFrom<sqlparser::ast::ObjectName> for Variable {
-    type Error = AstConversionError;
-
-    fn try_from(value: sqlparser::ast::ObjectName) -> Result<Self, Self::Error> {
+impl TryFromDialect<sqlparser::ast::ObjectName> for Variable {
+    fn try_from_dialect(
+        value: sqlparser::ast::ObjectName,
+        dialect: Dialect,
+    ) -> Result<Self, AstConversionError> {
         value
             .0
             .into_iter()
@@ -388,7 +417,7 @@ impl TryFrom<sqlparser::ast::ObjectName> for Variable {
                 sqlparser::ast::ObjectNamePart::Identifier(ident) => ident,
             })
             .collect::<Vec<_>>()
-            .try_into()
+            .try_into_dialect(dialect)
     }
 }
 
