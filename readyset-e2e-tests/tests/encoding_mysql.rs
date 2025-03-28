7a1db01e3ae246977409943ee4c08c3f1a4012e4
Michael Victor Zink <michael.z@readyset.io>
2025-03-28 11:26:54 -0700

    encoding_mysql: Refactor test to support variable byte widths
    This will be used in the future for testing non-ASCII characters with
    utf8 encodings.
    
    Change-Id: I3cd115cfaa258e707f578e0d86c246e1c2022dc9
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/9126
    Reviewed-by: Sidney Cammeresi <sac@readyset.io>
    Tested-by: Buildkite CI

--- readyset-e2e-tests/tests/encoding_mysql.rs
+++ readyset-e2e-tests/tests/encoding_mysql.rs
@@ -1,11 +1,19 @@
+use itertools::Itertools;
 use mysql_async::prelude::Queryable;
 use pretty_assertions::assert_eq;
 use readyset_client_test_helpers::{
     mysql_helpers::{self, MySQLAdapter},
     sleep, TestBuilder,
 };
-use std::fmt::{Display, UpperHex};
 use test_utils::{serial, slow};
+
+macro_rules! check_rows {
+    ($my_rows:expr, $rs_rows:expr) => {
+        for row in $my_rows.iter().zip($rs_rows.iter()) {
+            assert_eq!(row.0, row.1, "mysql (left) differed from readyset (right)");
+        }
+    };
+}
 
 /// At present, this tests that snapshotting and streaming replication of a varchar column with the
 /// specified character set results in the same utf8 encoded version of the data in Readyset. This
@@ -19,20 +27,15 @@ use test_utils::{serial, slow};
 /// and requesting the original character set results in it being re-encoded from utf8 to the
 /// original before being returned to the client.
 #[cfg(test)]
-async fn test_encoding_replication_inner<T>(
-    charset: &str,
-    hex_len: usize,
-    range: impl IntoIterator<Item = T>,
-) where
-    T: UpperHex + Display,
+async fn test_encoding_replication_inner<I>(charset: &str, range: I)
+where
+    I: IntoIterator<Item = (u32, String)>,
 {
     readyset_tracing::init_test_logging();
 
-    println!("starting");
-
-    let upstream_opts = mysql_helpers::upstream_config().db_name(Some("encoding_test"));
     mysql_helpers::recreate_database("encoding_test").await;
 
+    let upstream_opts = mysql_helpers::upstream_config().db_name(Some("encoding_test"));
     let mut upstream_conn = mysql_async::Conn::new(upstream_opts).await.unwrap();
 
     let create_snapshot_table = format!(
@@ -41,6 +44,7 @@ async fn test_encoding_replication_inner<T>(
             DROP TABLE IF EXISTS encoding_snapshot;
             CREATE TABLE encoding_snapshot (
                 id INT NOT NULL PRIMARY KEY,
+                hex VARCHAR(255) CHARACTER SET utf8mb4,
                 text VARCHAR(255) CHARACTER SET {}
             );
         "#,
@@ -51,31 +55,26 @@ async fn test_encoding_replication_inner<T>(
         .await
         .unwrap();
 
-    println!("created table");
+    let values: Vec<_> = range.into_iter().collect();
 
-    // Generate all latin1 characters (0x00-0xFF); characters 128-255 aren't valid UTF-8
-    let insert_values: String = range
-        .into_iter()
-        .map(|i| format!("({i}, UNHEX('{i:0width$X}'))", width = hex_len))
-        .collect::<Vec<String>>()
-        .join(",");
-    upstream_conn
-        .query_drop(format!(
-            "INSERT INTO encoding_snapshot (id, text) VALUES {insert_values}"
-        ))
-        .await
-        .unwrap();
-
-    println!("inserted data");
+    for chunk in values.iter().chunks(1000).into_iter() {
+        let insert_values: String = chunk
+            .map(|(i, h)| format!("({i}, '{h}', UNHEX('{h}'))"))
+            .collect::<Vec<String>>()
+            .join(",");
+        upstream_conn
+            .query_drop(format!(
+                "INSERT INTO encoding_snapshot (id, hex, text) VALUES {insert_values}"
+            ))
+            .await
+            .unwrap();
+    }
 
     // Verify the data was inserted correctly
-    let my_rows: Vec<(i64, Vec<u8>)> = upstream_conn
-        .query("SELECT id, text FROM encoding_snapshot ORDER BY id")
+    let my_rows: Vec<(i64, String, Vec<u8>)> = upstream_conn
+        .query("SELECT id, hex, text FROM encoding_snapshot ORDER BY id")
         .await
         .unwrap();
-    dbg!(&my_rows);
-
-    println!("starting readyset");
 
     // Test snapshot replication
     let (rs_opts, _handle, shutdown_tx) = TestBuilder::default()
@@ -84,27 +83,16 @@ async fn test_encoding_replication_inner<T>(
         .build::<MySQLAdapter>()
         .await;
 
-    println!("started readyset");
-
     sleep().await;
 
-    println!("slept");
+    let mut rs_conn = mysql_async::Conn::new(rs_opts).await.unwrap();
 
-    // Query the snapshotted table to verify all 255 characters were replicated correctly
-    let mut rs_conn = mysql_async::Conn::new(mysql_async::OptsBuilder::from_opts(rs_opts))
+    let rs_snapshot_rows: Vec<(i64, String, Vec<u8>)> = rs_conn
+        .query("SELECT id, hex, text FROM encoding_snapshot ORDER BY id")
         .await
         .unwrap();
 
-    println!("connected");
-
-    let rs_snapshot_rows: Vec<(i64, Vec<u8>)> = rs_conn
-        .query("SELECT id, text FROM encoding_snapshot ORDER BY id")
-        .await
-        .unwrap();
-
-    dbg!(&rs_snapshot_rows);
-
-    assert_eq!(my_rows, rs_snapshot_rows);
+    check_rows!(my_rows, rs_snapshot_rows);
 
     // Test streaming replication
     let create_streaming_table = format!(
@@ -112,6 +100,7 @@ async fn test_encoding_replication_inner<T>(
             DROP TABLE IF EXISTS encoding_streaming;
             CREATE TABLE encoding_streaming (
                 id INT NOT NULL PRIMARY KEY,
+                hex VARCHAR(255) CHARACTER SET utf8mb4,
                 text VARCHAR(255) CHARACTER SET {}
             );
         "#,
@@ -122,49 +111,50 @@ async fn test_encoding_replication_inner<T>(
         .await
         .unwrap();
 
-    println!("created streaming table");
+    for chunk in values.iter().chunks(1000).into_iter() {
+        let insert_values: String = chunk
+            .map(|(i, h)| format!("({i}, '{h}', UNHEX('{h}'))"))
+            .collect::<Vec<String>>()
+            .join(",");
+        upstream_conn
+            .query_drop(format!(
+                "INSERT INTO encoding_streaming (id, hex, text) VALUES {insert_values}"
+            ))
+            .await
+            .unwrap();
+    }
 
-    upstream_conn
-        .query_drop(format!(
-            "INSERT INTO encoding_streaming (id, text) VALUES {insert_values}"
-        ))
+    let rs_streaming_rows: Vec<(i64, String, Vec<u8>)> = rs_conn
+        .query("SELECT id, hex, text FROM encoding_streaming ORDER BY id")
         .await
         .unwrap();
 
-    println!("inserted streaming data");
-
-    sleep().await;
-
-    println!("slept");
-
-    let rs_streaming_rows: Vec<(i64, Vec<u8>)> = rs_conn
-        .query("SELECT id, text FROM encoding_streaming ORDER BY id")
-        .await
-        .unwrap();
-
-    dbg!(&rs_streaming_rows);
-
-    assert_eq!(my_rows, rs_streaming_rows);
-
-    println!("shutting down");
+    check_rows!(my_rows, rs_streaming_rows);
 
     shutdown_tx.shutdown().await;
-
-    println!("shutdown complete");
 }
 
 macro_rules! test_encoding_replication {
-    ($name:ident, $charset:expr, $hex_len:expr, $range:expr) => {
+    ($name:ident, $charset:expr, $range:expr) => {
         #[tokio::test]
         #[serial(mysql)]
         #[slow]
         async fn $name() {
-            test_encoding_replication_inner($charset, $hex_len, $range).await;
+            test_encoding_replication_inner($charset, $range).await;
         }
     };
 }
 
-test_encoding_replication!(ascii, "ascii", 2, 0..=127);
-test_encoding_replication!(test_latin1, "latin1", 2, 0..=255);
-test_encoding_replication!(test_utf8mb4, "utf8mb4", 2, 0..=127);
-test_encoding_replication!(test_utf8mb3, "utf8mb3", 2, 0..=127);
+fn format_u32s<I>(width: usize, range: I) -> impl Iterator<Item = (u32, String)>
+where
+    I: IntoIterator<Item = u32>,
+{
+    range
+        .into_iter()
+        .map(move |value| (value, format!("{value:0width$X}", width = width)))
+}
+
+test_encoding_replication!(ascii, "ascii", format_u32s(2, 0..=127));
+test_encoding_replication!(test_latin1, "latin1", format_u32s(2, 0..=255));
+test_encoding_replication!(test_utf8mb4, "utf8mb4", format_u32s(2, 0..=127));
+test_encoding_replication!(test_utf8mb3, "utf8mb3", format_u32s(2, 0..=127));
