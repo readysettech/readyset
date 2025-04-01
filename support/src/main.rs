b939f025473b9b7effbb9a4211d5d3a37e7c264d
Marcelo Altmann <marcelo@readyset.io>
2025-04-01 12:08:03 -0300

    support: add support tool
    Added a support tool to help troubleshoot.
    
    At the moment, the tool dumps data from a rocksdb instance.
    Different from the existing rocksdb dump tool, this tool will convert
    the data from "default" column family to JSON format and all other
    column families to a Vec<DfValue>.
    
    ```
    $ ./target/debug/support \
    --rocksdb-datadir ./readyset.db/readyset.db-test-tb1-0.db \
    --operation list-column-families
    Available column families:
      - default
      - 0
    ```
    
    ```
    $ ./target/debug/support \
    --rocksdb-datadir ./readyset.db/readyset.db-test-tb1-0.db \
    --operation dump-data --column-family default
    Dumping data from column family: default
    Key: [109, 101, 116, 97], Value: {
      "epoch": 1,
      "indices": [
        {
          "columns": [
            0
          ],
          "index_type": "HashMap"
        }
      ],
      "replication_offset": {
        "MySql": {
          "binlog_file_base_name": "binlog",
          "binlog_file_suffix": 19,
          "binlog_file_suffix_length": 6,
          "position": 2088
        }
      },
      "serde_version": 2
    }
    Dumped 1 rows
    ```
    
    ```
    $ ./target/debug/support \
    --rocksdb-datadir ./readyset.db/readyset.db-test-tb1-0.db \
    --operation dump-data --column-family 0
    Dumping data from column family: 0
    Key: [2, 1, 2], Value: [Int(1), TinyText("Marcelo" (Citext))]
    Key: [2, 1, 4], Value: [Int(2), TinyText("Someone Else" (Citext))]
    Dumped 2 rows
    ```
    
    Change-Id: If0d0a3c889dd96ea8236d477ccc2feba36a1d6a8
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/9138
    Reviewed-by: Jason Brown <jason.b@readyset.io>
    Tested-by: Buildkite CI

---
+++ support/src/main.rs
@@ -0,0 +1,100 @@
+use bincode::Options as BincodeOptions;
+use clap::{Parser, ValueEnum, command};
+use readyset_data::DfValue;
+use serde_json::Value;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(name = "readyset-support", about = "ReadySet support tools")]
+struct Options {
+    /// Path to RocksDB data directory
+    #[arg(long, env = "READYSET_ROCKSDB_DATADIR")]
+    rocksdb_datadir: PathBuf,
+
+    /// Operation to perform
+    #[arg(long)]
+    operation: Operation,
+
+    /// Name of the column family to dump
+    #[arg(long)]
+    column_family: Option<String>,
+}
+
+#[derive(Parser, Clone, ValueEnum)]
+enum Operation {
+    ListColumnFamilies,
+    DumpData,
+}
+
+// todo: make persistent_State public
+
+fn deserialize_row<T: AsRef<[u8]>>(bytes: T) -> Vec<DfValue> {
+    bincode::options()
+        .deserialize(bytes.as_ref())
+        .expect("Deserializing from rocksdb")
+}
+
+fn main() -> anyhow::Result<()> {
+    let options = Options::parse();
+    // Now you can use options.myrocks_datadir
+    // ...
+    let column_families =
+        rocksdb::DB::list_cf(&rocksdb::Options::default(), &options.rocksdb_datadir)?;
+
+    match options.operation {
+        Operation::ListColumnFamilies => {
+            println!("Available column families:");
+            for cf in &column_families {
+                println!("  - {}", cf);
+            }
+        }
+        Operation::DumpData => {
+            if let Some(cf) = options.column_family {
+                println!("Dumping data from column family: {}", cf);
+                let db = rocksdb::DB::open_cf(
+                    &rocksdb::Options::default(),
+                    options.rocksdb_datadir,
+                    column_families,
+                )
+                .unwrap();
+                //dump all data from the supplied column family.
+                if let Some(column_family) = db.cf_handle(&cf) {
+                    let iter = db.iterator_cf(column_family, rocksdb::IteratorMode::Start);
+                    let mut rows = 0;
+                    for item in iter {
+                        rows += 1;
+                        match item {
+                            Ok((key, value)) => {
+                                if cf == "default" {
+                                    println!(
+                                        "Key: {:?}, Value: {}",
+                                        (key),
+                                        serde_json::from_slice::<Value>(&value)
+                                            .map(|v| serde_json::to_string_pretty(&v).unwrap())
+                                            .unwrap_or_else(
+                                                |_| String::from_utf8_lossy(&value).to_string()
+                                            )
+                                    );
+                                } else {
+                                    println!(
+                                        "Key: {:?}, Value: {:?}",
+                                        (key),
+                                        deserialize_row(value)
+                                    );
+                                }
+                            }
+                            Err(e) => println!("Error reading key-value pair: {}", e),
+                        }
+                    }
+                    println!("Dumped {} rows", rows);
+                } else {
+                    return Err(anyhow::anyhow!("Column family '{}' not found", cf));
+                }
+            } else {
+                return Err(anyhow::anyhow!("Please specify a column family to dump"));
+            }
+        }
+    }
+
+    Ok(())
+}
