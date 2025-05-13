843b7251524cf07e3fd3e618b9f8d91228851598
Ron Hough <ron@readyset.io>
2025-05-13 18:42:02 -0500

    build: (test) Convert rust-tests from parallelism to matrix
    Using buildkite's "matrix" feature is more appropriate to the rust tests
    than "parallelism", since we actually run different tests for the
    parallel jobs.  Using "matrix" also allows the printing of the type of
    test in the label, which "parallelism" doesn't allow.
    
    Also, switching the rust-test agent queue from c6a-8xlarge to
    c7a-2xlarge results in roughly the same amount of step duration, but for
    30% of the cost.  This is counter-intuitive, since the c7a-2xlarge
    instances have 8vcpu vs 32vcpu on the c6a-8xlarge.  However, the c7a
    processors are faster, albeit fewer.
    
    Change-Id: Ibf13f7370264def9d78cbdbc510d03cd24dfb137
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/9405
    Reviewed-by: Michael Zink <michael.z@readyset.io>
    Tested-by: Buildkite CI

--- .buildkite/run-tests.sh
+++ .buildkite/run-tests.sh
@@ -12,25 +12,24 @@ export DISABLE_TELEMETRY=true
 export PROPTEST_MAX_SHRINK_TIME=1800000
 export CARGO_TERM_PROGRESS_WHEN=never
 
-# If we aren't actually running run-tests.sh as a parallel job, just follow the
-# "job 0" path.
-: "${BUILDKITE_PARALLEL_JOB:=0}"
+# Default to "nextest" run:
+: "${MATRIX_VALUE:=nextest}"
 
-if [[ "$BUILDKITE_PARALLEL_JOB" == "0" ]]; then
+if [[ "$MATRIX_VALUE" == "nextest" ]]; then
     # Run nextest
     echo "+++ :rust: Run tests (nextest)"
     cargo --locked nextest run --profile ci --hide-progress-bar \
         --workspace --features failure_injection \
         --exclude readyset-clustertest --exclude benchmarks \
         || upload_artifacts
-elif [[ "$BUILDKITE_PARALLEL_JOB" == "1" ]]; then
+elif [[ "$MATRIX_VALUE" == "doctest" ]]; then
     # Run doctests, because at this time nextest does not support doctests
     echo "+++ :rust: Run tests (doctest)"
     cargo --locked test --doc \
         --workspace --features failure_injection \
         --exclude readyset-clustertest --exclude benchmarks \
         || upload_artifacts
-elif [[ "$BUILDKITE_PARALLEL_JOB" == "2" ]]; then
+elif [[ "$MATRIX_VALUE" == "MRBR" ]]; then
     echo "+++ :rust: Run tests (mysql minimal row-based replication)"
 
     # Need a mysql-client to set binlog_row_image:
@@ -66,6 +65,6 @@ elif [[ "$BUILDKITE_PARALLEL_JOB" == "2" ]]; then
         --ignore-default-filter -E 'test(~serial_mysql)' \
         || upload_artifacts
 else
-    echo "No command defined for this parallel job."
+    echo "No test defined for MATRIX_VALUE=${MATRIX_VALUE}."
     exit 1
 fi
