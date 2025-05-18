fbbb551ca9cac4dd0268cd73f3b537502261c9de
Michael Victor Zink <michael.z@readyset.io>
2025-05-17 18:03:45 -0700

    build: Run nightly tests against more upstreams
    This should get us more coverage today, and make it easier when
    introducing support for newer versions of Postgres and MySQL.
    
    Fixes: REA-5726
    
    Change-Id: I252d87b0ad6f52653cf4e844f329f4a16bb986c4
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/9434
    Reviewed-by: Ron Hough <ron@readyset.io>
    Tested-by: Buildkite CI

--- .buildkite/run-tests.sh
+++ .buildkite/run-tests.sh
@@ -8,29 +8,8 @@ upload_artifacts() {
     exit 1
 }
 
-export DISABLE_TELEMETRY=true
-export PROPTEST_MAX_SHRINK_TIME=1800000
-export CARGO_TERM_PROGRESS_WHEN=never
-
-# Default to "nextest" run:
-: "${MATRIX_VALUE:=nextest}"
-
-if [[ "$MATRIX_VALUE" == "nextest" ]]; then
-    # Run nextest
-    echo "+++ :rust: Run tests (nextest)"
-    cargo --locked nextest run --profile ci --hide-progress-bar \
-        --workspace --features failure_injection \
-        --exclude readyset-clustertest --exclude benchmarks \
-        || upload_artifacts
-elif [[ "$MATRIX_VALUE" == "doctest" ]]; then
-    # Run doctests, because at this time nextest does not support doctests
-    echo "+++ :rust: Run tests (doctest)"
-    cargo --locked test --doc \
-        --workspace --features failure_injection \
-        --exclude readyset-clustertest --exclude benchmarks \
-        || upload_artifacts
-elif [[ "$MATRIX_VALUE" == "MRBR" ]]; then
-    echo "+++ :rust: Run tests (mysql minimal row-based replication)"
+enable_mrbr() {
+    echo "+++ :mysql: Configure minimal row based replication"
 
     # Need a mysql-client to set binlog_row_image:
     if ! command -v mysql &> /dev/null; then
@@ -58,13 +37,67 @@ elif [[ "$MATRIX_VALUE" == "MRBR" ]]; then
         echo "mysql 'SET GLOBAL binlog_row_image=MINIMAL' failed after $max_attempts attempts."
         exit 1
     fi
+}
+
+export DISABLE_TELEMETRY=true
+export PROPTEST_MAX_SHRINK_TIME=1800000
+export CARGO_TERM_PROGRESS_WHEN=never
+
+: "${TEST_CATEGORY:=nextest}"
+: "${UPSTREAM_CONFIG:=default}"
+: "${MYSQL_MRBR:=off}"
+
+# We use $UPSTREAM_CONFIG to determine which tests to run; the appropriate docker containers and
+# images should already have been set up by the pipeline step. The `default` config has mysql8,
+# postgres15, postgres13, as well as consul and redis which are used for some non-upstream tests. We
+# shouldn't need to filter down to a subset of the tests in that case. For specific upstreams, we
+# run only the tests that run against that particular upstream.
+#
+# The filter sets should generally match the groups defined in .config/nextest.toml, but we can't
+# just identify a group to run. Besides, down the line these tests may become parallelizable and we
+# won't have need for nextest mutual exclusion groups (for all tests); for this reason, we don't
+# here use the ':serial:` tag, even though it's used in the groups.
+case "$UPSTREAM_CONFIG" in
+  "none")
+    NEXTEST_ARGS=(--ignore-default-filter -E "not test(/:(\w+)_upstream:/)") ;;
+  mysql8*)
+    # If we ever add a `mysql57_upstream` test, this will filter it out, and it can be added in the next case.
+    NEXTEST_ARGS=(--ignore-default-filter -E "test(/:mysql8\d*_upstream:/) or test(/:mysql_upstream:/)") ;;
+  "mysql57")
+    NEXTEST_ARGS=(--ignore-default-filter -E "test(/:mysql_upstream:/)") ;;
+  "postgres15")
+    NEXTEST_ARGS=(--ignore-default-filter -E "test(/:postgres\d*_upstream:/)") ;;
+  "postgres13")
+    NEXTEST_ARGS=(--ignore-default-filter -E "test(/:postgres_upstream:/)") ;;
+  *)
+    NEXTEST_ARGS=() ;;
+esac
+
+
+if [[ "$TEST_CATEGORY" == "nextest" ]]; then
+    if [[ "$MYSQL_MRBR" == "on" ]]; then
+        if [[ "$UPSTREAM_CONFIG" != "mysql"* ]]; then
+            echo "MRBR configured for non-MySQL upstream"
+            exit 1
+        fi
+        enable_mrbr
+    fi
+
+    echo "+++ :rust: Run tests (nextest $UPSTREAM_CONFIG, MRBR $MYSQL_MRBR)"
 
     cargo --locked nextest run --profile ci --hide-progress-bar \
         --workspace --features failure_injection \
         --exclude readyset-clustertest --exclude benchmarks \
-        --ignore-default-filter -E 'test(/:mysql_upstream:/)' \
+        "${NEXTEST_ARGS[@]}" \
+        || upload_artifacts
+elif [[ "$TEST_CATEGORY" == "doctest" ]]; then
+    # Run doctests, because at this time nextest does not support doctests
+    echo "+++ :rust: Run tests (doctest)"
+    cargo --locked test --doc \
+        --workspace --features failure_injection \
+        --exclude readyset-clustertest --exclude benchmarks \
         || upload_artifacts
 else
-    echo "No test defined for MATRIX_VALUE=${MATRIX_VALUE}."
+    echo "No test defined for TEST_CATEGORY=${TEST_CATEGORY}."
     exit 1
 fi
