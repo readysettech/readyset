3852c4bfcbec7cd6cfe96b3a5a3f72fc8449922a
Ron Hough <ron@readyset.io>
2025-06-04 17:38:12 -0500

    buildkite: Update build image creation script for ARM support
    This update makes changes to the build-image.sh script necessary for
    supporting multi-architecture image builds.  Changes include:
    - switching to `docker buildx` for image building, which provides
      multi-arch support.
    - when building multi-arch images, they must be tagged with an
      architecture identifier so that the later manifest generation step can
      reference them.
    
    The support for the SKIP_IF_UNCHANGED feature was removed.  It's not
    being used anywhere else in the pipeline, and the docker layer caching
    makes it largely irrelevant since if nothing has changed the docker
    build step uses cached layers and completes very quickly.
    
    Change-Id: I62c8389693f82ff3fe85fa31d75d5aa0e8376463
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/9584
    Reviewed-by: Michael Zink <michael.z@readyset.io>
    Tested-by: Buildkite CI

--- .buildkite/build-image.sh
+++ .buildkite/build-image.sh
@@ -7,12 +7,6 @@ set -euo pipefail
 # Docker Buildkit is used for better performance and caching
 # Builds are uploaded to our private AWS Elastic Container Registry
 # When builds are done on main, they are tagged as latest to help with caching.
-#
-# To optimize rebuilds of images that rarely change, set the SKIP_IF_UNCHANGED
-# environment variable to a comma-separated lists of repository-relative paths
-# to files - if none of these files have changed, the image won't actually be
-# built, it'll just be re-tagged with the current commit via the AWS API.
-
 
 dockerfile="$1"
 image_name="$2"
@@ -28,43 +22,30 @@ AWS_ACCOUNT=${AWS_ACCOUNT:-305232526136} # build
 AWS_REGION=${AWS_REGION:-us-east-2}
 VERSION=${VERSION:-$BUILDKITE_COMMIT}
 
+# Standardize detected arch variants to preferred docker style:
+arch=$(arch | sed 's/x86_64/amd64/;s/aarch64/arm64/')
+
+if [[ "$arch" != "arm64" && "$arch" != "amd64" ]]; then
+    echo "Error: Unsupported architecture: $arch" >&2
+    exit 1
+fi
+
+platform="linux/$arch"
+
+# If $MULTIARCH is defined, this image is part of a multi-arch docker manifest. Append the
+# architecture to image tags.  The manifest is created in a different step and will reference these
+# -$arch tags.
+if [ -n "${MULTIARCH-}" ]; then
+    tag_version=$VERSION-$arch
+else
+    tag_version=$VERSION
+fi
+
 docker_repo="$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com"
 image="$docker_repo/$image_name"
 
-if [ -n "${SKIP_IF_UNCHANGED+1}" ]; then
-    do_skip="1"
-    changed_files="$(git diff --name-only HEAD~ HEAD)"
-    IFS=',' read -ra files <<< "$SKIP_IF_UNCHANGED,$dockerfile"
-    for fname in "${files[@]}"; do
-        if grep "$fname" <<< "$changed_files" > /dev/null; then
-            do_skip=""
-        fi
-    done
-
-    if [ -n "$do_skip" ]; then
-        echo "+++ :docker: Dockerfile unchanged, retagging latest image as ${BUILDKITE_COMMIT}"
-        set +eo pipefail
-
-        manifest=$(
-            aws ecr batch-get-image \
-                --repository-name "$image_name" \
-                --image-ids imageTag=latest \
-                --output json \
-                | jq -r '.images[0].imageManifest')
-
-        # This might fail if the image already exists (eg if we're
-        # rebuilding the same commit), and we don't want to fail if that's
-        # the case - if the operation fails for *another* reason, we'll just
-        # fail in a later build because the image doesn't exist
-        aws ecr put-image \
-            --repository-name "$image_name" \
-            --image-tag "$BUILDKITE_COMMIT" \
-            --image-manifest "$manifest" || true \
-            > /dev/null
-        exit 0
-    fi
-fi
-
+# Pull the latest image to populate the cache layers.  This can make the container build go much
+# faster.
 echo "--- :docker: Pulling $image:latest"
 if docker pull --quiet "$image:latest"; then
     cache_from="--cache-from=$image:latest"
@@ -74,10 +55,12 @@ else
 fi
 
 build_cmd_prefix=(
-    "docker" "build" \
-    "-f" "$dockerfile" \
-    "-t" "$image:$VERSION" \
+    "docker" "buildx" "build" \
+    "--platform" "$platform" \
+    "--file" "$dockerfile" \
+    "--tag" "$image:$tag_version" \
     "--progress" "plain" \
+    "--load" \
     "--build-arg" "BUILDKIT_INLINE_CACHE=1" \
 )
 build_cmd_suffix=(
@@ -90,18 +73,28 @@ else
     build_cmd=("${build_cmd_prefix[@]}" "${build_cmd_suffix[@]}")
 fi
 
-echo "+++ :docker: Building $image:$VERSION"
-DOCKER_BUILDKIT=1 "${build_cmd[@]}"
-tags_to_push=("$image:$VERSION")
+echo "+++ :docker: Building $image:$tag_version"
 
-if [ "$BUILDKITE_BRANCH" = "refs/heads/main" ]; then
-    docker tag "$image:$VERSION" "$image:latest"
+# Must initialize buildx builder for multi-arch use:
+docker buildx create \
+  --name container-builder \
+  --driver docker-container \
+  --bootstrap --use
+
+"${build_cmd[@]}"
+tags_to_push=("$image:$tag_version")
+
+if [[ "${BUILDKITE_BRANCH:-}" == "refs/heads/main" && -n "${MULTIARCH:-}" ]]; then
+    # If this is main and we're not building multi-arch image, then we need to push "latest".
+    # Multi-arch images require a later manifest creation step which will be pushed as "latest".
+    docker tag "$image:$tag_version" "$image:latest"
     tags_to_push+=("$image:latest")
-    docker tag "$image:$VERSION" "$image:release-${BUILDKITE_COMMIT}"
-    tags_to_push+=("$image:release-${BUILDKITE_COMMIT}")
 fi
 
-for tag in "${tags_to_push[@]}"; do
-    echo "--- :docker: Pushing $tag"
-    docker push "$tag"
-done
+# Push the tags, unless SKIP_PUSH is defined:
+if [ -z "${SKIP_PUSH+x}" ]; then
+  for tag in "${tags_to_push[@]}"; do
+      echo "--- :docker: Pushing $tag"
+      docker push "$tag"
+  done
+fi
