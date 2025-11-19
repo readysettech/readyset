cb23574e4e482234efd9a89440e066be3360d926
Vassili Zarouba <vassili@readyset.io>
2025-11-18 18:08:42 -0600

    readyset-sql-passes: Use aggregates as functions arguments
      Fix issue with detecting aggregates used as built-in
      functions arguments. Prior to the fix, only function
      `round(x)` was handled to discover the aggregates.
      The fix now introduced an explicit set of built-in
      functions, which are allowed to be used in the context
      of aggregated expressions. The list is force updatable,
      in a sense that if new built-ins are added, the compiler
      will force the developer to update the set.
    
    Fixes: REA-6189
    
    Release-Note-Core: Allow to use aggregates in all supported
      row level built-in functions, except for `jsonb_insert` and
      `jsonb_set`. For ex.:
      select coalesce(max(qty), 100), sn from qa.spj group by sn;
    
    BuildDistroPkg: aggsinfuncs
    Change-Id: I450f4fba9719b55e3ac8d34becf67016adae2581
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/10987
    Reviewed-by: Michael Zink <michael.z@readyset.io>
    Tested-by: Buildkite CI

--- readyset-sql-passes/src/util.rs
+++ readyset-sql-passes/src/util.rs
@@ -1,6 +1,3 @@
-use std::collections::{HashMap, HashSet};
-use std::iter;
-
 use itertools::Either;
 use readyset_errors::{ReadySetResult, unsupported_err};
 use readyset_sql::DialectDisplay;
@@ -10,6 +7,9 @@ use readyset_sql::ast::{
     FunctionExpr, InValue, JoinClause, JoinRightSide, Relation, SelectStatement, SqlIdentifier,
     TableExpr, TableExprInner,
 };
+use std::collections::{HashMap, HashSet};
+use std::iter;
+use std::sync::OnceLock;
 
 pub(crate) fn join_clause_tables(join: &JoinClause) -> impl Iterator<Item = &TableExpr> {
     match &join.right {
@@ -118,6 +118,83 @@ pub(crate) fn subquery_schemas<'a>(
         .collect()
 }
 
+// Built-in function names that are allowed in expressions containing aggregates.
+// All allowed functions must be deterministic, formatting-only, and must not affect result cardinality.
+static BUILTIN_FUNCTIONS_ALLOWED_IN_AGG_CONTEXT: OnceLock<HashSet<&'static str>> = OnceLock::new();
+
+fn is_builtin_allowed_in_aggregate_context(name: &str) -> bool {
+    BUILTIN_FUNCTIONS_ALLOWED_IN_AGG_CONTEXT
+        .get_or_init(builtin_functions_allowed_in_agg_context)
+        .contains(name.to_ascii_lowercase().as_str())
+}
+
+fn builtin_functions_allowed_in_agg_context() -> HashSet<&'static str> {
+    use dataflow_expression::BuiltinFunctionDiscriminants;
+    use strum::IntoEnumIterator;
+    // Always add explicit arms to this match, do not use default arm here `_ =>`.
+    // We have to make sure, any newly added built-ins will be explicitly added here.
+    // To exclude a function from the list, the corresponding arm should return empty `!vec[]`.
+    BuiltinFunctionDiscriminants::iter()
+        .flat_map(|bf| match bf {
+            BuiltinFunctionDiscriminants::ConvertTZ => vec!["convert_tz"],
+            BuiltinFunctionDiscriminants::DayOfWeek => vec!["dayofweek"],
+            BuiltinFunctionDiscriminants::IfNull => vec!["ifnull"],
+            BuiltinFunctionDiscriminants::Month => vec!["month"],
+            BuiltinFunctionDiscriminants::Timediff => vec!["timediff"],
+            BuiltinFunctionDiscriminants::Addtime => vec!["addtime"],
+            BuiltinFunctionDiscriminants::DateFormat => vec!["date_format"],
+            BuiltinFunctionDiscriminants::Round => vec!["round"],
+            BuiltinFunctionDiscriminants::JsonDepth => vec!["json_depth"],
+            BuiltinFunctionDiscriminants::JsonValid => vec!["json_valid"],
+            BuiltinFunctionDiscriminants::JsonQuote => vec!["json_quote"],
+            BuiltinFunctionDiscriminants::JsonOverlaps => vec!["json_overlaps"],
+            BuiltinFunctionDiscriminants::JsonTypeof => vec!["json_typeof", "jsonb_typeof"],
+            BuiltinFunctionDiscriminants::JsonObject => vec!["json_object"],
+            BuiltinFunctionDiscriminants::JsonBuildObject => {
+                vec!["json_build_object", "jsonb_build_object"]
+            }
+            BuiltinFunctionDiscriminants::JsonArrayLength => {
+                vec!["json_array_length", "jsonb_array_length"]
+            }
+            BuiltinFunctionDiscriminants::JsonStripNulls => {
+                vec!["json_strip_nulls", "jsonb_strip_nulls"]
+            }
+            BuiltinFunctionDiscriminants::JsonExtractPath => vec![
+                "json_extract_path",
+                "jsonb_extract_path",
+                "json_extract_path_text",
+                "jsonb_extract_path_text",
+            ],
+            BuiltinFunctionDiscriminants::JsonbInsert => vec![],
+            BuiltinFunctionDiscriminants::JsonbSet => vec![],
+            BuiltinFunctionDiscriminants::JsonbPretty => vec!["jsonb_pretty"],
+            BuiltinFunctionDiscriminants::Coalesce => vec!["coalesce"],
+            BuiltinFunctionDiscriminants::Concat => vec!["concat"],
+            BuiltinFunctionDiscriminants::ConcatWs => vec!["concat_ws"],
+            BuiltinFunctionDiscriminants::Substring => vec!["substring", "substr"],
+            BuiltinFunctionDiscriminants::SplitPart => vec!["split_part"],
+            BuiltinFunctionDiscriminants::Greatest => vec!["greatest"],
+            BuiltinFunctionDiscriminants::Least => vec!["least"],
+            BuiltinFunctionDiscriminants::ArrayToString => vec!["array_to_string"],
+            BuiltinFunctionDiscriminants::DateTrunc => vec!["date_trunc"],
+            BuiltinFunctionDiscriminants::Extract => vec!["extract"],
+            BuiltinFunctionDiscriminants::Length => {
+                vec!["length", "octet_length", "char_length", "character_length"]
+            }
+            BuiltinFunctionDiscriminants::Ascii => vec!["ascii"],
+            BuiltinFunctionDiscriminants::Lower => vec!["lower"],
+            BuiltinFunctionDiscriminants::Upper => vec!["upper"],
+            BuiltinFunctionDiscriminants::Hex => vec!["hex"],
+            BuiltinFunctionDiscriminants::SpatialAsText => {
+                vec!["st_astext", "st_aswkt"]
+            }
+            BuiltinFunctionDiscriminants::SpatialAsEWKT => vec!["st_asewkt"],
+            BuiltinFunctionDiscriminants::Bucket => vec!["bucket"],
+        })
+        .filter(|t| !t.is_empty())
+        .collect::<HashSet<_>>()
+}
+
 #[must_use]
 pub fn map_aggregates(
     expr: &mut Expr,
@@ -143,14 +220,20 @@ pub fn map_aggregates(
                 ret.append(&mut map_aggregates(else_expr, dialect));
             }
         }
+        Expr::Call(
+            FunctionExpr::Lower { expr, .. }
+            | FunctionExpr::Upper { expr, .. }
+            | FunctionExpr::Extract { expr, .. }
+            | FunctionExpr::Substring { string: expr, .. }
+            | FunctionExpr::Bucket { expr, .. },
+        ) => {
+            ret.append(&mut map_aggregates(expr, dialect));
+        }
         Expr::Call(FunctionExpr::Call {
             name,
             arguments: Some(exprs),
-        }) if matches!(name.as_str(), "round") => {
-            let expr = exprs
-                .first_mut()
-                .expect("round should have at least one argument");
-            ret.append(&mut map_aggregates(expr, dialect));
+        }) if is_builtin_allowed_in_aggregate_context(name.as_str()) => {
+            ret.extend(exprs.iter_mut().flat_map(|e| map_aggregates(e, dialect)));
         }
         Expr::Call(_) | Expr::Literal(_) | Expr::Column(_) | Expr::Variable(_) => {}
         Expr::BinaryOp { lhs, rhs, .. }
