f2887cd40c97e6a3f429b173370a64dcf2ee0e33
Ron Hough <ron@readyset.io>
2025-12-22 17:09:46 -0600

    build: Use multi-arch release image in nightly
    Make the nightly build create a multi-arch release image.  The test-demo
    and framework-testing pipelines uploaded by the nightly pipeline also
    build release images, so this change additionally sets a
    `SKIP_UPLOADED_RELEASE_IMAGE_BUILD` flag that (as its name implies) will
    notify uploaded pipelines to skip their own release image build steps.
    
    The `push_docker_image_manifest.sh` script has been modified to take
    additional arguments to apply as tags to the image being pushed.  The
    logic for pushing `latest` based on whether or not the current branch is
    `main` is really best left to the build step definition.  This way, any
    image can be tagged with any tag as determined by the context in which
    it is being invoked.  Build steps which push images built on `main`
    should tag the images with latest.
    
    Change-Id: I917ef1e1defc84b1f176ca5c6847ef5c6a6a6964
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/11202
    Tested-by: Buildkite CI
    Reviewed-by: Michael Zink <michael.z@readyset.io>

--- .buildkite/push_docker_image_manifest.sh
+++ .buildkite/push_docker_image_manifest.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Create and push manifests for multi-architecture Docker images to an AWS ECR registry. Given an
-# image name (i.e. "readyset-rust-ubuntu2204-builder-base"), create a multi-arch manifest tagged
-# with $VERSION that references the architecture-specific images for that image name (i.e.
-# "readyset-rust-ubuntu2204-builder-base:$VERSION-amd64").  If $BUILDKITE_BRANCH is defined and
-# equals "refs/heads/main", also push the manifest with tag "latest".
+# image name (i.e. "readyset-rust-ubuntu2204-builder-base") and optional tags, create a multi-arch
+# manifest that references the architecture-specific images for that image name (i.e.
+# "readyset-rust-ubuntu2204-builder-base:$VERSION-amd64"). If no tags are provided, the manifest
+# is tagged with $VERSION. If tags are provided as arguments, the manifest is created and pushed
+# for each specified tag.
 #
 # Environment variables used:
 #   AWS_ACCOUNT      - AWS account ID (default: 305232526136, i.e. "readyset-build-default")
 #   AWS_REGION       - AWS region (default: us-east-2)
 #   VERSION          - Image version tag (default: $BUILDKITE_COMMIT)
-#   BUILDKITE_BRANCH - CI branch name (used to determine if 'latest' tag is pushed)
 
 set -o errexit
 set -o nounset
@@ -19,11 +19,13 @@ set -o pipefail
 ME=$(basename "$0")
 
 if [[ $# -lt 1 ]]; then
-  echo "Usage: $ME <image_name>"
+  echo "Usage: $ME <image_name> [<tags>...]"
   exit 1
 fi
 
 image_name=$1
+shift 1
+
 archs=("amd64" "arm64")
 
 AWS_ACCOUNT=${AWS_ACCOUNT:-305232526136}
@@ -50,8 +52,10 @@ echo "+++ :docker: Creating multi-arch manifest"
 echo "Images to link:"
 printf '  %s\n' "${arch_images[@]}"
 
-create_and_push_manifest "$VERSION"
-
-if [[ "${BUILDKITE_BRANCH:-}" = "refs/heads/main" ]]; then
-  create_and_push_manifest "latest"
+if [ $# -eq 0 ]; then
+  create_and_push_manifest "$VERSION"
+else
+  for param in "$@"; do
+    create_and_push_manifest "$param"
+  done
 fi
