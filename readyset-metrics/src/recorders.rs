99c538f96a5188c172ddef504b4646615128e809
Johnathan Davis <jcd@readyset.io>
2026-05-15 09:33:11 -0700

    readyset-metrics: Always include Duration
    A vertical test from nightly demonstrated that Duration is referenced
    even without the push-gateway feature.
    
    Change-Id: I4258d1383c3ff328ac80addfe438be7caff7a922
    Reviewed-on: https://gerrit.readyset.name/c/readyset/+/13179
    Tested-by: Buildkite CI
    Reviewed-by: Jason Brown <jason.b@readyset.io>

--- readyset-metrics/src/recorders.rs
+++ readyset-metrics/src/recorders.rs
@@ -3,7 +3,6 @@ use std::sync::Arc;
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering;
 use std::sync::{PoisonError, RwLock};
-#[cfg(feature = "push-gateway")]
 use std::time::Duration;
 
 use indexmap::IndexMap;
